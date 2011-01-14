86db286bb15a474645be601661625b6ce6088c22
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-01-14 16:02:28 +0100

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 UnipeptWeb::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+end
