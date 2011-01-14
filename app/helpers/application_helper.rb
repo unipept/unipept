0e8d07b4a5b268d018c3e25549d9e38e48533989
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-01-14 23:28:16 +0100

    cleaned up title code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Unipept"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
