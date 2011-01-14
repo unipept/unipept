bb7257c7e9a7ad6ce19dd40f000f29e3dbf4d18f
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-01-14 11:37:09 +0100

    changed capistrano settings

--- Gemfile
+++ Gemfile
@@ -5,13 +5,13 @@ gem 'rails', '3.0.3'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3-ruby', :require => 'sqlite3'
+# gem 'sqlite3-ruby', :require => 'sqlite3'
 
 # Use unicorn as the web server
 # gem 'unicorn'
 
 # Deploy with Capistrano
-# gem 'capistrano'
+gem 'capistrano'
 
 # To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
 # gem 'ruby-debug'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,27 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "unipept-web"
+set :repository,  "ssh://gitosis@zeus.ugent.be:2222/unipept-web.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+#own stuff
+set :deploy_to, "/home/bmesuere/rails"
+set :user, "bmesuere"
+set :use_sudo, false
+set :port, 4840
+
+role :web, "buruli.ugent.be"                          # Your HTTP server, Apache/etc
+role :app, "buruli.ugent.be"                          # This may be the same as your `Web` server
+role :db,  "buruli.ugent.be", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
