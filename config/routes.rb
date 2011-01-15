16a592f2a7eac957d318cb7d601e221b6b069a9c
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-01-15 23:21:55 +0100

    added some nicer routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 UnipeptWeb::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
