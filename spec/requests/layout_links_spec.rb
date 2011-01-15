84efcd83c6ae2ab20ca3bceea4591fbf1518aac9
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-01-15 23:34:26 +0100

    added links to contact and about

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -16,4 +16,14 @@ describe "LayoutLinks" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end
+  
+  it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      response.should have_selector('title', :content => "About")
+      click_link "Contact"
+      response.should have_selector('title', :content => "Contact")
+      click_link "Home"
+      response.should have_selector('title', :content => "Home")
+    end
 end
