da9fe71896eac9e2f2880412a254dc7caefcc91f
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-01-15 23:18:08 +0100

    added integration tests

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should have an About page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "About")
+  end
+end
