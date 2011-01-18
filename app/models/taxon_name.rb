4f8bdc8f18f743d9fc3708bc1b921a0c14c4638f
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-01-18 15:15:40 +0100

    removed nameClass

--- app/models/taxon_name.rb
+++ app/models/taxon_name.rb
@@ -5,7 +5,6 @@
 #  id        :integer(4)      not null, primary key
 #  tax_id    :integer(4)      not null
 #  name      :string(256)
-#  nameClass :string(64)
 #
 
 class TaxonName < ActiveRecord::Base
