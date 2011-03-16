ba8a4781089a47766b63ca32f1fb914cd7083788
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-03-16 14:04:16 +0100

    fixed the view organism

--- app/controllers/organisms_controller.rb
+++ app/controllers/organisms_controller.rb
@@ -1,12 +1,14 @@
 class OrganismsController < ApplicationController
 
+  #show the information about an organism
   def show
     @organism = Taxon.find_by_id(params[:id])
     if @organism.nil?
       flash[:error] = "No matches for #{params[:id]}"
       redirect_to organisms_path
     else
-      @genbank_files = GenbankFile.where("taxon_id = ?", params[:id])
+      @genbank_files = GenbankFile.where("taxon_id = ?", params[:id]).group("project_id")
+      @aantal = @genbank_files.size.size # first size gives a count per project_id
       @title = @organism.name
     
       respond_to do |format|
