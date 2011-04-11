39597b7764b190e15990817bedd081ef054e70ed
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-04-11 09:57:21 +0200

    made loadTaxon executable

--- scripts/loadTaxon.sh
+++ scripts/loadTaxon.sh
@@ -1,4 +1,7 @@
-wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip -O data/taxon/taxon.zip
-unzip data/taxon/taxon.zip
+cd data/taxon
+rm *
+wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip
+unzip taxdmp.zip
+cd ../..
 java -cp ".:bin:bin/:lib/bytecode.jar:lib/patricia.jar:lib/mysql.jar:lib/biojava.jar" -Xmx512m tools/commandline/TaxonLoader data/taxon/nodes.dmp data/taxon/names.dmp
 java -cp ".:bin:bin/:lib/bytecode.jar:lib/patricia.jar:lib/mysql.jar:lib/biojava.jar" -Xmx512m tools/commandline/PeptideLoader load
