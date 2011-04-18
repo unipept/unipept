fd204444e4b4fe4156217a10d82dac4c44d90a28
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-04-18 22:13:50 +0200

    capistrano now has dev en production deployment + removed debug log from production

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,16 +5,40 @@ set :repository,  "ssh://git@zoidberg.ugent.be:4840/unipept/unipept-web.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-#own stuff
-set :deploy_to, "/home/bmesuere/rails"
-set :user, "bmesuere"
-set :use_sudo, false
-set :port, 4840
-set :deploy_via, :remote_cache
+task :dev do
+  set :deploy_to, "/home/bmesuere/rails"
+  set :user, "bmesuere"
+  set :use_sudo, false
+  set :port, 4840
+  set :deploy_via, :remote_cache
 
-role :web, "buruli.ugent.be"                          # Your HTTP server, Apache/etc
-role :app, "buruli.ugent.be"                          # This may be the same as your `Web` server
-role :db,  "buruli.ugent.be", :primary => true # This is where Rails migrations will run
+  role :web, "buruli.ugent.be"                          # Your HTTP server, Apache/etc
+  role :app, "buruli.ugent.be"                          # This may be the same as your `Web` server
+  role :db,  "buruli.ugent.be", :primary => true # This is where Rails migrations will run
+end
+
+task :prod do
+  set :deploy_to, "/home/bmesuere/rails"
+  set :user, "bmesuere"
+  set :use_sudo, false
+  set :port, 4840
+  set :deploy_via, :remote_cache
+
+  role :web, "buruli.ugent.be"                          # Your HTTP server, Apache/etc
+  role :app, "buruli.ugent.be"                          # This may be the same as your `Web` server
+  role :db,  "buruli.ugent.be", :primary => true # This is where Rails migrations will run
+end
+
+#own stuff
+#set :deploy_to, "/home/bmesuere/rails"
+#set :user, "bmesuere"
+#set :use_sudo, false
+#set :port, 4840
+#set :deploy_via, :remote_cache
+
+#role :web, "buruli.ugent.be"                          # Your HTTP server, Apache/etc
+#role :app, "buruli.ugent.be"                          # This may be the same as your `Web` server
+#role :db,  "buruli.ugent.be", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
