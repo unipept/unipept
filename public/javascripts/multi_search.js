868ab0780b263df488e499db578fdc1a32989155
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-04-19 09:51:23 +0200

    code cleanup

--- public/javascripts/multi_search.js
+++ public/javascripts/multi_search.js
@@ -1,46 +1,49 @@
-var labelType, useGradients, nativeTextSupport, animate;
+var labelType,
+useGradients,
+nativeTextSupport,
+animate;
 
-(function() {
-  var ua = navigator.userAgent,
-      iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
-      typeOfCanvas = typeof HTMLCanvasElement,
-      nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
-      textSupport = nativeCanvasSupport 
-        && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
-  //I'm setting this based on the fact that ExCanvas provides text support for IE
-  //and that as of today iPhone/iPad current text support is lame
-  labelType = (!nativeCanvasSupport || (textSupport && !iStuff))? 'Native' : 'HTML';
-  nativeTextSupport = labelType == 'Native';
-  useGradients = nativeCanvasSupport;
-  animate = !(iStuff || !nativeCanvasSupport);
+ (function() {
+    var ua = navigator.userAgent,
+    iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
+    typeOfCanvas = typeof HTMLCanvasElement,
+    nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
+    textSupport = nativeCanvasSupport
+    && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
+    //I'm setting this based on the fact that ExCanvas provides text support for IE
+    //and that as of today iPhone/iPad current text support is lame
+    labelType = (!nativeCanvasSupport || (textSupport && !iStuff)) ? 'Native': 'HTML';
+    nativeTextSupport = labelType == 'Native';
+    useGradients = nativeCanvasSupport;
+    animate = !(iStuff || !nativeCanvasSupport);
 })();
 
-function init(data){
-  //init TreeMap
-  var tm = new $jit.TM.Squarified({
-    //where to inject the visualization
-    injectInto: 'treeMap',
-    //parent box title heights
-    titleHeight: 15,
-    //enable animations
-    animate: animate,
-    //box offsets
-    offset: 1,
-	constrained: true,
-	levelsToShow: 1,
-    //Attach left and right click events
-    Events: {
-      enable: true,
-      onClick: function(node) {
-        if(node) tm.enter(node);
-      },
-      onRightClick: function() {
-        tm.out();
-      }
-    },
-    duration: 1000,
-    //Enable tips
-    /*Tips: {
+function init(data) {
+    //init TreeMap
+    var tm = new $jit.TM.Squarified({
+        //where to inject the visualization
+        injectInto: 'treeMap',
+        //parent box title heights
+        titleHeight: 15,
+        //enable animations
+        animate: animate,
+        //box offsets
+        offset: 1,
+        //constrained: true,
+        //levelsToShow: 1,
+        //Attach left and right click events
+        Events: {
+            enable: true,
+            onClick: function(node) {
+                if (node) tm.enter(node);
+            },
+            onRightClick: function() {
+                tm.out();
+            }
+        },
+        duration: 1000,
+        //Enable tips
+        /*Tips: {
       enable: true,
       //add positioning offsets
       offsetX: 20,
@@ -61,22 +64,22 @@ function init(data){
         tip.innerHTML =  html; 
       }  
     },*/
-    //Add the name of the node in the correponding label
-    //This method is called once, on label creation.
-    onCreateLabel: function(domElement, node){
-        domElement.innerHTML = node.name + " (" + node.data.count + ")";
-        var style = domElement.style;
-        style.display = '';
-        style.border = '2px solid transparent';
-        domElement.onmouseover = function() {
-          style.border = '2px solid #9FD4FF';
-        };
-        domElement.onmouseout = function() {
-          style.border = '2px solid transparent';
-        };
-    }
-  });
-  tm.loadJSON(data);
-  tm.refresh();
-  //end
+        //Add the name of the node in the correponding label
+        //This method is called once, on label creation.
+        onCreateLabel: function(domElement, node) {
+            domElement.innerHTML = node.name + " (" + node.data.count + ")";
+            var style = domElement.style;
+            style.display = '';
+            style.border = '2px solid transparent';
+            domElement.onmouseover = function() {
+                style.border = '2px solid #9FD4FF';
+            };
+            domElement.onmouseout = function() {
+                style.border = '2px solid transparent';
+            };
+        }
+    });
+    tm.loadJSON(data);
+    tm.refresh();
+    //end
 }
--- public/javascripts/sequence_show.js
+++ public/javascripts/sequence_show.js
@@ -1,75 +1,83 @@
-var labelType, useGradients, nativeTextSupport, animate;
+var labelType,
+useGradients,
+nativeTextSupport,
+animate;
 
-(function() {
-  var ua = navigator.userAgent,
-      iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
-      typeOfCanvas = typeof HTMLCanvasElement,
-      nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
-      textSupport = nativeCanvasSupport 
-        && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
-  //I'm setting this based on the fact that ExCanvas provides text support for IE
-  //and that as of today iPhone/iPad current text support is lame
-  labelType = (!nativeCanvasSupport || (textSupport && !iStuff))? 'Native' : 'HTML';
-  nativeTextSupport = labelType == 'Native';
-  useGradients = nativeCanvasSupport;
-  animate = !(iStuff || !nativeCanvasSupport);
+ (function() {
+    var ua = navigator.userAgent,
+    iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
+    typeOfCanvas = typeof HTMLCanvasElement,
+    nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
+    textSupport = nativeCanvasSupport
+    && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
+    //I'm setting this based on the fact that ExCanvas provides text support for IE
+    //and that as of today iPhone/iPad current text support is lame
+    labelType = (!nativeCanvasSupport || (textSupport && !iStuff)) ? 'Native': 'HTML';
+    nativeTextSupport = labelType == 'Native';
+    useGradients = nativeCanvasSupport;
+    animate = !(iStuff || !nativeCanvasSupport);
 })();
 
-function init(data){
+function init(data) {
     //Create a new SpaceTree instance
     var st = new $jit.ST({
-        injectInto: 'lineageTree',				//id of viz container element
-        duration: 800,							//set duration for the animation
-        transition: $jit.Trans.Quart.easeInOut,	//set animation transition type
-        levelDistance: 50,						//set distance between node and its children
-		levelsToShow: 3,
-		//offsetY: 170,
-		//orientation: 'top',
-		offsetX: 130,
-		
+        injectInto: 'lineageTree',
+        //id of viz container element
+        duration: 800,
+        //set duration for the animation
+        transition: $jit.Trans.Quart.easeInOut,
+        //set animation transition type
+        levelDistance: 50,
+        //set distance between node and its children
+        levelsToShow: 3,
+        //offsetY: 170,
+        //orientation: 'top',
+        offsetX: 130,
+
         //enable panning
         Navigation: {
-          enable:true,
-          panning:true
+            enable: true,
+            panning: true
         },
 
         //set node and edge styles
         Node: {
             autoHeight: true,
             //autoWidth: true,
-            width: 100,           //also change the CSS .node property if you change this!
-			type: 'rectangle',
+            width: 100,
+            //also change the CSS .node property if you change this!
+            type: 'rectangle',
             color: '#DCDFE4',
             overridable: true,
-			align:'center'
+            align: 'center'
         },
-        
+
         Edge: {
             type: 'bezier',
             overridable: true
         },
-        
+
         //This method is called on DOM label creation.
         //Use this method to add event handlers and styles to
         //your node.
-        onCreateLabel: function(label, node){
-            label.id = node.id;            
+        onCreateLabel: function(label, node) {
+            label.id = node.id;
             label.innerHTML = node.name;
-            label.onclick = function(){
-           		st.onClick(node.id);
-            	//st.setRoot(node.id, 'animate');
+            label.onclick = function() {
+                st.onClick(node.id);
+                //st.setRoot(node.id, 'animate');
             };
             //set label styles => TODO: fix the labels with these settings instead of CSS
             var style = label.style;
             style.width = 60 + 'px';
-            style.height = 17 + 'px';            
+            style.height = 17 + 'px';
             style.cursor = 'pointer';
             style.color = '#333';
             style.fontSize = '0.8em';
-            style.textAlign= 'center';
+            style.textAlign = 'center';
             style.paddingTop = '3px';
         },
-        
+
         //This method is called right before plotting
         //a node. It's useful for changing an individual node
         //style properties before plotting it.
@@ -94,13 +102,13 @@ function init(data){
                 }
             }
         },*/
-        
+
         //This method is called right before plotting
         //an edge. It's useful for changing an individual edge
         //style properties before plotting it.
         //Edge data proprties prefixed with a dollar sign will
         //override the Edge global style properties.
-        onBeforePlotLine: function(adj){
+        onBeforePlotLine: function(adj) {
             if (adj.nodeFrom.selected && adj.nodeTo.selected) {
                 adj.data.$color = "#eed";
                 adj.data.$lineWidth = 3;
@@ -110,9 +118,13 @@ function init(data){
                 delete adj.data.$lineWidth;
             }
         }
-    });    
-    st.loadJSON(data);										//load json data
-    st.compute();											//compute node positions and layout
-    st.geom.translate(new $jit.Complex(-200, 0), "current");//optional: make a translation of the tree
-    st.onClick(st.root);									//emulate a click on the root node.
+    });
+    st.loadJSON(data);
+    //load json data
+    st.compute();
+    //compute node positions and layout
+    st.geom.translate(new $jit.Complex( - 200, 0), "current");
+    //optional: make a translation of the tree
+    st.onClick(st.root);
+    //emulate a click on the root node.
 }
