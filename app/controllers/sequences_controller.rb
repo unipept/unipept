62aa5ae5af7ec1798c31067c1c493d2aa001411c
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-05-27 11:37:22 +0200

    removed all old multi_search code

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -82,45 +82,8 @@ class SequencesController < ApplicationController
     redirect_to "#{sequences_path}/#{params[:q]}"
   end
   
-  
   # processes a list of sequences
   def multi_search
-    @title = "Results"
-    
-    # remove duplicates, split missed cleavages, substitute I by L
-    data = params[:p][0].gsub(/([KR])([^P\r])/,"\\1\n\\2").gsub(/I/,'L').lines.map(&:strip).to_a.uniq
-    
-    @number_searched_for = data.length
-    @number_found = 0
-    @number_unique_found = 0
-    
-    # build the resultset
-    @matches = Hash.new
-    @species = Array.new
-    data.each do |s|
-      sequence = Sequence.find_by_sequence(s)
-      unless sequence.nil? || (!params[:drafts] && sequence.peptides.map(&:genbank_file).map(&:draft).count("\x00") == 0)
-        @number_found += 1
-        resultset = sequence.occurrences(params[:drafts])
-        if resultset.num_rows == 1
-          @number_unique_found += 1
-          hash = resultset.fetch_hash
-          hash["sequence"] = sequence
-          s = @matches[hash["species"]]
-          if s.nil?
-            @species << hash["species"]
-            @matches[hash["species"]] = [hash]
-          else
-            @matches[hash["species"]] << hash
-          end
-        end
-      end
-    end
-    @species.sort!
-  end
-  
-  # processes a list of sequences
-  def multi_search2
     @title = "Results"
     
     # remove duplicates, split missed cleavages, substitute I by L
