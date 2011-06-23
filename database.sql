9be622bb09582d63795b865811ccc42a1a4d6cd1
Bart Mesuere <Bart.Mesuere@UGent.be>
2011-06-23 09:51:42 +0200

    new database scheme

--- database.sql
+++ database.sql
@@ -1,80 +1,151 @@
-delimiter $$
-CREATE DATABASE `unipept` /*!40100 DEFAULT CHARACTER SET utf8 */$$
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-delimiter $$
-CREATE TABLE `genbank_files` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `project_id` varchar(32) NOT NULL,
-  `accession_number` varchar(32) NOT NULL,
-  `draft` bit(1) NOT NULL DEFAULT b'0',
-  `taxon_id` int(11) NOT NULL,
-  `hash` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uidx_id` (`id`),
-  KEY `idx_taxon_id` (`taxon_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+CREATE SCHEMA IF NOT EXISTS `unipept` DEFAULT CHARACTER SET utf8 ;
+USE `unipept` ;
 
-delimiter $$
-CREATE TABLE `peptides` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `sequence_id` int(11) NOT NULL,
-  `genbank_file_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uidx_id` (`id`),
-  KEY `idx_sequence_id` (`sequence_id`),
-  KEY `idx_genbank_file_id` (`genbank_file_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+-- -----------------------------------------------------
+-- Table `unipept`.`taxons`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `unipept`.`taxons` (
+  `id` MEDIUMINT UNSIGNED NOT NULL ,
+  `name` VARCHAR(256) NOT NULL ,
+  `rank` ENUM('no rank', 'superkingdom', 'kingdom', 'superphylum', 'phylum', 'subphylum', 'superclass', 'class', 'subclass', 'infraclass', 'superorder', 'order', 'suborder', 'infraorder', 'parvorder', 'superfamily', 'family', 'subfamily', 'tribe', 'subtribe', 'genus', 'subgenus', 'species_group', 'species_subgroup', 'species', 'subspecies', 'varietas', 'forma' ) NULL DEFAULT NULL ,
+  `parent_id` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_taxon_taxon` (`parent_id` ASC) ,
+  CONSTRAINT `fk_taxon_taxon`
+    FOREIGN KEY (`parent_id` )
+    REFERENCES `unipept`.`taxons` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-delimiter $$
-CREATE TABLE `sequences` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `sequence` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uidx_sequence` (`sequence`),
-  UNIQUE KEY `uidx_id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36293784 DEFAULT CHARSET=utf8$$
 
-delimiter $$
-CREATE TABLE `taxons` (
-  `id` int(11) NOT NULL,
-  `name` varchar(256) NOT NULL,
-  `rank` varchar(16) DEFAULT NULL,
-  `parent_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+-- -----------------------------------------------------
+-- Table `unipept`.`uniprot_entries`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `unipept`.`uniprot_entries` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `uniprot_accession_number` VARCHAR(8) NOT NULL ,
+  `version` SMALLINT UNSIGNED NOT NULL ,
+  `taxon_id` MEDIUMINT UNSIGNED NOT NULL ,
+  `type` ENUM('swissprot', 'trembl') NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_uniprot_entries_taxons` (`taxon_id` ASC) ,
+  CONSTRAINT `fk_uniprot_entries_taxons`
+    FOREIGN KEY (`taxon_id` )
+    REFERENCES `unipept`.`taxons` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 1136
+DEFAULT CHARACTER SET = utf8;
 
-delimiter $$
-CREATE TABLE `lineages` (
-  `taxon_id` int(11) unsigned NOT NULL,
-  `superkingdom` int(11) unsigned DEFAULT NULL,
-  `kingdom` int(11) unsigned DEFAULT NULL,
-  `superphylum` int(11) unsigned DEFAULT NULL,
-  `phylum` int(11) unsigned DEFAULT NULL,
-  `subphylum` int(11) unsigned DEFAULT NULL,
-  `superclass` int(11) unsigned DEFAULT NULL,
-  `class` int(11) unsigned DEFAULT NULL,
-  `subclass` int(11) unsigned DEFAULT NULL,
-  `infraclass` int(11) unsigned DEFAULT NULL,
-  `superorder` int(11) unsigned DEFAULT NULL,
-  `order` int(11) unsigned DEFAULT NULL,
-  `suborder` int(11) unsigned DEFAULT NULL,
-  `infraorder` int(11) unsigned DEFAULT NULL,
-  `parvorder` int(11) unsigned DEFAULT NULL,
-  `superfamily` int(11) unsigned DEFAULT NULL,
-  `family` int(11) unsigned DEFAULT NULL,
-  `subfamily` int(11) unsigned DEFAULT NULL,
-  `tribe` int(11) unsigned DEFAULT NULL,
-  `subtribe` int(11) unsigned DEFAULT NULL,
-  `genus` int(11) unsigned DEFAULT NULL,
-  `subgenus` int(11) unsigned DEFAULT NULL,
-  `species_group` int(11) unsigned DEFAULT NULL,
-  `species_subgroup` int(11) unsigned DEFAULT NULL,
-  `species` int(11) unsigned DEFAULT NULL,
-  `subspecies` int(11) unsigned DEFAULT NULL,
-  `varietas` int(11) unsigned DEFAULT NULL,
-  `forma` int(11) unsigned DEFAULT NULL,
-  PRIMARY KEY (`taxon_id`),
-  UNIQUE KEY `id_UNIQUE` (`taxon_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
+-- -----------------------------------------------------
+-- Table `unipept`.`lineages`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `unipept`.`lineages` (
+  `taxon_id` MEDIUMINT UNSIGNED NOT NULL ,
+  `superkingdom` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `kingdom` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `superphylum` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `phylum` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `subphylum` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `superclass` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `class` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `subclass` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `infraclass` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `superorder` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `order` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `suborder` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `infraorder` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `parvorder` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `superfamily` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `family` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `subfamily` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `tribe` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `subtribe` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `genus` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `subgenus` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `species_group` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `species_subgroup` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `species` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `subspecies` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `varietas` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  `forma` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
+  PRIMARY KEY (`taxon_id`) ,
+  INDEX `fk_lineages_taxons` (`taxon_id` ASC) ,
+  CONSTRAINT `fk_lineages_taxons`
+    FOREIGN KEY (`taxon_id` )
+    REFERENCES `unipept`.`taxons` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `unipept`.`sequences`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `unipept`.`sequences` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `sequence` VARCHAR(50) NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `uidx_sequence` (`sequence` ASC) )
+ENGINE = InnoDB
+AUTO_INCREMENT = 17232835
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `unipept`.`peptides`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `unipept`.`peptides` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `sequence_id` INT UNSIGNED NOT NULL ,
+  `uniprot_entry_id` INT UNSIGNED NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_peptides_sequences` (`sequence_id` ASC) ,
+  INDEX `fk_peptides_uniprot_entries` (`uniprot_entry_id` ASC) ,
+  CONSTRAINT `fk_peptides_sequences`
+    FOREIGN KEY (`sequence_id` )
+    REFERENCES `unipept`.`sequences` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_peptides_uniprot_entries`
+    FOREIGN KEY (`uniprot_entry_id` )
+    REFERENCES `unipept`.`uniprot_entries` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 26402948
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `unipept`.`uniprot_cross_references`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `unipept`.`uniprot_cross_references` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `uniprot_entry_id` INT UNSIGNED NOT NULL ,
+  `sequence_id` VARCHAR(15) NULL ,
+  `protein_id` VARCHAR(15) NULL ,
+  `type` ENUM('refseq', 'EMBL') NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_uniprot_cross_reference_uniprot_entries` (`uniprot_entry_id` ASC) ,
+  CONSTRAINT `fk_uniprot_cross_reference_uniprot_entries`
+    FOREIGN KEY (`uniprot_entry_id` )
+    REFERENCES `unipept`.`uniprot_entries` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
