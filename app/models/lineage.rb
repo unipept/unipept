1c284707ce93a15447b174affedc8443588a10e8
Bart Mesuere <Bart.Mesuere@UGent.be>
2012-03-19 09:47:56 +0100

    add suborder to lineages. This fixes \!7

--- app/models/lineage.rb
+++ app/models/lineage.rb
@@ -50,6 +50,7 @@ class Lineage < ActiveRecord::Base
   belongs_to :infraclass_t,       :foreign_key  => "infraclass",    :primary_key  => "id",  :class_name   => 'Taxon'
   belongs_to :superorder_t,       :foreign_key  => "superorder",    :primary_key  => "id",  :class_name   => 'Taxon'
   belongs_to :order_t,            :foreign_key  => "order",         :primary_key  => "id",  :class_name   => 'Taxon'
+  belongs_to :suborder_t,         :foreign_key  => "suborder",      :primary_key  => "id",  :class_name   => 'Taxon'
   belongs_to :infraorder_t,       :foreign_key  => "infraorder",    :primary_key  => "id",  :class_name   => 'Taxon'
   belongs_to :parvorder_t,        :foreign_key  => "parvorder",     :primary_key  => "id",  :class_name   => 'Taxon'
   belongs_to :superfamily_t,      :foreign_key  => "superfamily",   :primary_key  => "id",  :class_name   => 'Taxon'
@@ -67,14 +68,14 @@ class Lineage < ActiveRecord::Base
   belongs_to :forma_t,            :foreign_key  => "forma",         :primary_key  => "id",  :class_name   => 'Taxon'
                                   
   ORDER = [:superkingdom, :kingdom, :subkingdom, :superphylum, :phylum, :subphylum, 
-            :superclass, :class_, :subclass, :infraclass, :superorder, :order, 
+            :superclass, :class_, :subclass, :infraclass, :superorder, :order, :suborder,
             :infraorder, :parvorder, :superfamily, :family, :subfamily, :tribe, 
             :subtribe, :genus, :subgenus, :species_group, :species_subgroup, 
             :species, :subspecies, :varietas, :forma]
             
   ORDER_T = [:superkingdom_t, :kingdom_t, :subkingdom_t, :superphylum_t, :phylum_t, 
             :subphylum_t, :superclass_t, :class_t, :subclass_t, :infraclass_t, 
-            :superorder_t, :order_t, :infraorder_t, :parvorder_t, :superfamily_t, 
+            :superorder_t, :order_t, :suborder, :infraorder_t, :parvorder_t, :superfamily_t, 
             :family_t, :subfamily_t, :tribe_t, :subtribe_t, :genus_t, :subgenus_t, 
             :species_group_t, :species_subgroup_t, :species_t, :subspecies_t, 
             :varietas_t, :forma_t] 
