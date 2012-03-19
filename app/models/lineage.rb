8adc1174ecf408b680ca79c26306b1f089678313
Bart Mesuere <Bart.Mesuere@UGent.be>
2012-03-19 15:35:03 +0100

    typo

--- app/models/lineage.rb
+++ app/models/lineage.rb
@@ -75,7 +75,7 @@ class Lineage < ActiveRecord::Base
             
   ORDER_T = [:superkingdom_t, :kingdom_t, :subkingdom_t, :superphylum_t, :phylum_t, 
             :subphylum_t, :superclass_t, :class_t, :subclass_t, :infraclass_t, 
-            :superorder_t, :order_t, :suborder, :infraorder_t, :parvorder_t, :superfamily_t, 
+            :superorder_t, :order_t, :suborder_t, :infraorder_t, :parvorder_t, :superfamily_t, 
             :family_t, :subfamily_t, :tribe_t, :subtribe_t, :genus_t, :subgenus_t, 
             :species_group_t, :species_subgroup_t, :species_t, :subspecies_t, 
             :varietas_t, :forma_t] 
