bf1d25c964e103f2e686f48eb75e7adc5c8108a2
Bart Mesuere <Bart.Mesuere@UGent.be>
2012-04-30 14:56:48 +0200

    added deploy setting for sunburst branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :scm, :git
 
 task :dev do
   set :deploy_to, "/home/bmesuere/rails"
-  set :branch, "0.4"
+  set :branch, "feature/sunburst"
   set :user, "bmesuere"
   set :use_sudo, false
   set :port, 4840
