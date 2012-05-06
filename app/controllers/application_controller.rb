77f90938297075031716bea2aaf947b9a9ffcec6
Bart Mesuere <Bart.Mesuere@UGent.be>
2012-05-06 17:34:57 +0200

    add motd support. Fixes #17

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_motd
+  
+  def set_motd
+    file = Rails.root.join("public", "motd")
+    @motd = File.read(file) if FileTest.exists?(file)
+  end
 end
