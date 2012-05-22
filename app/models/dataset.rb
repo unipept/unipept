d2c2fa31a07c0a77454de8e4e80c9cc7d8735572
Bart Mesuere <Bart.Mesuere@UGent.be>
2012-05-22 13:14:56 +0200

    added database annotations

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: datasets
+#
+#  id              :integer(4)      not null, primary key
+#  environment     :string(160)
+#  reference       :string(500)
+#  url             :string(200)
+#  project_website :string(200)
+#
+
 class Dataset < ActiveRecord::Base
   has_many :dataset_items
   
--- app/models/dataset_item.rb
+++ app/models/dataset_item.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: dataset_items
+#
+#  id         :integer(4)      not null, primary key
+#  dataset_id :integer(4)
+#  name       :string(160)
+#  path       :string(160)
+#  order      :integer(4)
+#
+
 class DatasetItem < ActiveRecord::Base
   belongs_to :dataset
   
