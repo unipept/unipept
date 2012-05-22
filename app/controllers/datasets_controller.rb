803cb3ba4e87c46ab5829a413d2b16de2b1272d5
Bart Mesuere <Bart.Mesuere@UGent.be>
2012-05-22 15:12:27 +0200

    cleaned up the default views

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -44,7 +44,7 @@ class DatasetsController < ApplicationController
 
     respond_to do |format|
       if @dataset.save
-        format.html { redirect_to(@dataset, :notice => 'Dataset was successfully created.') }
+        format.html { redirect_to(@dataset, :flash => { :info => 'Dataset was successfully created.' }) }
         format.xml  { render :xml => @dataset, :status => :created, :location => @dataset }
       else
         format.html { render :action => "new" }
@@ -60,7 +60,7 @@ class DatasetsController < ApplicationController
 
     respond_to do |format|
       if @dataset.update_attributes(params[:dataset])
-        format.html { redirect_to(@dataset, :notice => 'Dataset was successfully updated.') }
+        format.html { redirect_to(@dataset, :flash => { :info => 'Dataset was successfully updated.' }) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
