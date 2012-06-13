efe8413129483e4463c4598efef555bc42d01c63
Bart Mesuere <Bart.Mesuere@UGent.be>
2012-06-13 19:53:37 +0200

    try loading LCA first

--- app/assets/javascripts/sequence_show.js
+++ app/assets/javascripts/sequence_show.js
@@ -128,7 +128,11 @@ function init_sequence_show(data, lcaId) {
     st.geom.translate(new $jit.Complex(-200, 0), "current");
 
 	//emulate a click on the root node.
-    st.onClick(st.root);
-	st.onClick(lcaId);
+	try{ 
+    	st.onClick(lcaId);
+	}
+	catch(err){
+        st.onClick(st.root);
+    }
 
 }
