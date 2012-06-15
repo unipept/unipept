dfaa2af6d027482bfc3a1bb87137b9a32502d014
Bart Mesuere <Bart.Mesuere@UGent.be>
2012-06-15 17:07:19 +0200

    add news to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     @title = "Home"
     @datasets = Dataset.includes(:dataset_items).all
+    @post = Post.order("id DESC").limit(1)
   end
 
   def about
