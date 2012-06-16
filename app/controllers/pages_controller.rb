3768c0895057d557f18762c5c718ab06f5f8fc06
Bart Mesuere <Bart.Mesuere@UGent.be>
2012-06-16 23:04:14 +0200

    rails 3 shortcut

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def home
     @title = "Home"
     @datasets = Dataset.includes(:dataset_items).all
-    @post = Post.order("id DESC").limit(1)
+    @post = Post.last
   end
 
   def about
