1a8449583769f3ef1f7a1dddec9e6aabcdb53731
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-01-23 17:34:59 +0100

    another test

--- app/controllers/pancoreproteome_controller.rb
+++ app/controllers/pancoreproteome_controller.rb
@@ -5,7 +5,7 @@ class PancoreproteomeController < ApplicationController
     @genomes = ["NC_003909.8", "NC_004722.1", "NC_006274.1", "NC_011658.1", "NC_011725.1", "NC_011772.1", "NC_011773.1", "NC_011969.1", "NC_012472.1", "NC_014335.1", "NC_016771.1", "NC_016779.1"]
     @cores = Array.new
     @pans = Array.new
-    pan = Array.new
+    pan = Set.new
     core = nil
     @genomes.each do|g|
       start = Time.now
