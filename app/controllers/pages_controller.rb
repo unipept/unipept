18835af4ffbdf599a89f8625545ec9b0d4126792
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-03-27 16:59:14 +0100

    add a progress bar to the admin page. Closes #111

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,8 +14,17 @@ class PagesController < ApplicationController
   def contact
     @title = "Contact"
   end
-  
+
   def admin
     @title = "Admin"
+
+    # progressbar stuff
+    file = Rails.root.join("public", "progress")
+    if FileTest.exists?(file)
+      content = File.read(file)
+      content = content.gsub(/#/,"\n").lines.to_a
+      @progress_name = content[0]
+      @progress_percentage = content[1]
+    end
   end
 end
