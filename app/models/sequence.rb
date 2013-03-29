33f376e9dbae063080834e0e226c942a13cd458d
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-03-29 16:41:53 +0100

    less white space, more comments

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -9,23 +9,23 @@
 #
 
 class Sequence < ActiveRecord::Base
-  
+
   has_many :peptides
   has_many :original_peptides, :foreign_key  => "original_sequence_id", :primary_key  => "id", :class_name   => 'Peptide'
-  
+
   belongs_to :lca_t, :foreign_key  => "lca", :primary_key  => "id",  :class_name   => 'Taxon'
   belongs_to :lca_il_t, :foreign_key  => "lca_il", :primary_key  => "id",  :class_name   => 'Taxon'
-  
+
   # SELECT DISTINCT lineages.* FROM unipept.peptides INNER JOIN unipept.uniprot_entries ON (uniprot_entries.id = peptides.uniprot_entry_id) INNER JOIN unipept.lineages ON (uniprot_entries.taxon_id = lineages.taxon_id) WHERE peptides.sequence_id = #{id}
   def lineages(equate_il = true, eager = false)
-    if equate_il  
+    if equate_il
       if eager
-        l = Lineage.select("lineages.*").joins(:uniprot_entries => :peptides).where("peptides.sequence_id = ?", id).uniq.includes(:name,  
-                  :superkingdom_t, :kingdom_t, :subkingdom_t, :superphylum_t, :phylum_t, 
-                  :subphylum_t, :superclass_t, :class_t, :subclass_t, :infraclass_t, 
-                  :superorder_t, :order_t, :suborder_t, :infraorder_t, :parvorder_t, :superfamily_t, 
-                  :family_t, :subfamily_t, :tribe_t, :subtribe_t, :genus_t, :subgenus_t, 
-                  :species_group_t, :species_subgroup_t, :species_t, :subspecies_t, 
+        l = Lineage.select("lineages.*").joins(:uniprot_entries => :peptides).where("peptides.sequence_id = ?", id).uniq.includes(:name,
+                  :superkingdom_t, :kingdom_t, :subkingdom_t, :superphylum_t, :phylum_t,
+                  :subphylum_t, :superclass_t, :class_t, :subclass_t, :infraclass_t,
+                  :superorder_t, :order_t, :suborder_t, :infraorder_t, :parvorder_t, :superfamily_t,
+                  :family_t, :subfamily_t, :tribe_t, :subtribe_t, :genus_t, :subgenus_t,
+                  :species_group_t, :species_subgroup_t, :species_t, :subspecies_t,
                   :varietas_t, :forma_t)
       else
         l = Lineage.select("lineages.*").joins(:uniprot_entries => :peptides).where("peptides.sequence_id = ?", id).uniq
@@ -39,7 +39,8 @@ class Sequence < ActiveRecord::Base
     end
     return l
   end
-  
+
+  # Calculates the lowest common ancestor for this sequence
   def calculate_lca(equate_il = true, return_taxon = false)
     if equate_il
       if lca_il.nil?
@@ -48,7 +49,7 @@ class Sequence < ActiveRecord::Base
         save
       end
       return lca_il_t if return_taxon
-      return lca_il 
+      return lca_il
     else
       if lca.nil?
         temp = Lineage.calculate_lca(lineages(false))
@@ -59,16 +60,17 @@ class Sequence < ActiveRecord::Base
       return lca
     end
   end
-      
+
+  # Processes the input file and writes the results in csv format to the output file
   def self.batch_process(input, output = "output.csv")
     file = File.open(input, 'r')
-    slice_size = 1
+    slice_size = 1000
     data = file.readlines.each_slice(slice_size).to_a
     num_of_slices = data.size / slice_size
     current_slice = 0
-    
+
     File.open(output, 'w') { |file| file.write(CSV.generate_line ["peptide"].concat(Lineage.ranks)) }
-    
+
     for slice in data do
       csv_string = ""
       File.open("public/progress", 'w') { |file| file.write("batch process#" + (current_slice * 100 / num_of_slices).to_s) }
@@ -99,5 +101,5 @@ class Sequence < ActiveRecord::Base
       current_slice += 1
     end
     File.open("public/progress", 'w') { |file| file.write("batch process#100") }
-  end                  
+  end
 end
