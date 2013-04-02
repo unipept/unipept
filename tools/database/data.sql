c492794eec9308e98779ec7bc3de85bbb7ef9524
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-04-02 15:15:42 +0200

    split data and structure

---
+++ tools/database/data.sql
@@ -0,0 +1,36 @@
+-- -----------------------------------------------------
+-- Data for table `unipept`.`counters`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `unipept`;
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('sequence_id', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('superkingdom', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('kingdom', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('subkingdom', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('superphylum', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('phylum', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('subphylum', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('superclass', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('class', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('subclass', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('infraclass', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('superorder', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('order', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('suborder', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('infraorder', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('parvorder', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('superfamily', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('family', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('subfamily', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('tribe', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('subtribe', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('genus', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('subgenus', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('species group', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('species subgroup', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('species', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('subspecies', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('varietas', 0);
+INSERT INTO `unipept`.`counters` (`name`, `value`) VALUES ('forma', 0);
+
+COMMIT;
