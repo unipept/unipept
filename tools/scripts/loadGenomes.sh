34b5ae98189326197a11a03f81229ca870787270
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-04-04 10:22:47 +0200

    add new datadir to loadGenomes script

--- tools/scripts/loadGenomes.sh
+++ tools/scripts/loadGenomes.sh
@@ -1,15 +1,22 @@
-datadir="data"
+#!/bin/bash
+# Downloads the prokaryotes.txt file and loads them into the genomes table
+
+datadir="../data"
+currentdir=`pwd`
+
 cd "${datadir}"
-rm prokaryotes.txt
-rm prokaryotes.sql
+rm -f prokaryotes.txt
+rm -f prokaryotes.sql
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/prokaryotes.txt
 touch prokaryotes.sql
 IFS='	'
 cat prokaryotes.txt | cut -f1 -f3 -f8 -f18 | egrep -v -- "-|#" | sed "s/'//g" |
-while read -a line; do 
+while read -a line; do
     IFS=',' read -a sequences <<< "${line[2]}"
     for element in "${sequences[@]}"
     do
         echo "INSERT INTO genomes (name, bioproject_id, refseq_id, status) VALUES ('${line[0]}', '${line[1]}', '$element', '${line[3]}');" >> prokaryotes.sql
     done
 done
+
+cd "${currentdir}"
