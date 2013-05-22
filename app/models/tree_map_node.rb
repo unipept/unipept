6d0f53b062716ef92a1e71c91ce57734a75f9fab
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-05-22 15:31:11 +0200

    clean up the treeNode class a bit

--- app/models/tree_map_node.rb
+++ app/models/tree_map_node.rb
@@ -1,58 +1,80 @@
+# Overview of the data in a TreeMapNode
+# - id
+# - name
+# - children
+# - data
+#   - title
+#   - $area
+#   - $color
+#   - count
+#   - self_count
+#   - level
+#   - piecharturl
+#   - rank
+# - metadata
+#   - id
+#   - all_sequences
+#   - own_sequences
+# - attr
+#   - title
+# - state
+# - nodes
 class TreeMapNode < Node
   #:metadata contains the sequences
   attr_accessor :state, :metadata, :attr
-  
+
+  # Gradients for the treemap
   GRADIENT = Hash["no rank", "#242a42", "superkingdom", "#26314a", "kingdom", "#283851", "subkingdom", "#294059", "superphylum", "#2b4760", "phylum", "#2d4f67", "subphylum", "#2f576d", "superclass", "#315e74", "class", "#35667a", "subclass", "#386e7f", "infraclass", "#3d7685", "superorder", "#427e8a", "order", "#48868e", "suborder", "#4f8e93", "infraorder", "#579697", "parvorder", "#5f9e9a", "superfamily", "#68a69e", "family", "#72aea1", "subfamily", "#7db6a5", "tribe", "#88bea8", "subtribe", "#93c5ab", "genus", "#9fcdae", "subgenus", "#abd4b2", "species group", "#b8dcb5", "species subgroup", "#c5e3b9", "species", "#d3eabd", "subspecies", "#e1f1c2", "varietas", "#eff8c7", "forma", "#fdffcc"]
-  
+
   def initialize(id, name, rank="root")
     super(id, name)
-    
+
     #area
     @data["$area"] = 0
     @data["count"] = 0
-    
+
     @metadata = Hash.new
     @metadata["all_sequences"] = Array.new
-    
+    @metadata["id"] = id
+
     @attr = Hash.new
     @attr["title"] = rank
     @data["rank"] = rank
-    @metadata["id"] = id
-    
+
     #color
     @data["level"] = 0
     @data["$color"] = GRADIENT[rank]
-    
+
     fix_title_and_state
   end
-  
+
   # adds a child to this node and updates the color of the child
   # returns the added child
   def add_child(child, root)
-    child.data["level"] = @data["level"]+1 
+    child.data["level"] = @data["level"]+1
     super(child, root)
   end
-  
+
   # adds a number to the count variable and recalculates the area
   def add_sequences(sequences)
-    @metadata["all_sequences"].concat(sequences) 
-    
+    @metadata["all_sequences"].concat(sequences)
+
     @data["count"] += sequences.length
     @data["$area"] = Math.log10(@data["count"]+1)/Math.log10(2)
   end
-  
+
   def add_own_sequences(sequences)
     @data["self_count"] = sequences.length
     @metadata["own_sequences"] = sequences
-    
+
   end
-  
+
   def fix_title_and_state
     @data["title"] = @name
-    @data["title"] += " (" + (@data["self_count"].nil? ? "0" : @data["self_count"].to_s) + "/" + @data["count"].to_s + ")"       
+    @data["title"] += " (" + (@data["self_count"].nil? ? "0" : @data["self_count"].to_s) + "/" + @data["count"].to_s + ")"
     @state = @data["level"] <= 3 ? "open" : "closed"
   end
-  
+
   # Adds a URL to every node linking to a piechart of their children
   # also fixes the title, this is somewhat of hack
   def add_piechart_data
@@ -67,7 +89,7 @@ class TreeMapNode < Node
       @children.map{|c| c.add_piechart_data}
     end
   end
-  
+
   # Sorts the peptides lists and children alphabetically
   def sort_peptides_and_children
     @children.sort_by!(&:name) unless @children.empty?
@@ -89,7 +111,7 @@ class TreeMapNode < Node
     hash["data"].delete("$area")
     hash["data"].delete("rank")
   end
-  
+
   # cleans a hash of redundant data for sunburst
   def self.clean_treemap!(hash)
     hash.delete("nodes")
