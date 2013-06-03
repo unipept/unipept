01dc15e6ae67a0ab3d90334d2e27ba68880958ec
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-06-03 18:22:57 +0200

    double the svg to png resolution. #146

--- app/controllers/imagemagick_controller.rb
+++ app/controllers/imagemagick_controller.rb
@@ -3,10 +3,10 @@ class ImagemagickController < ApplicationController
   
   # converts an svg to a png
   def convert
-     img = Magick::Image.from_blob(params[:image]){ self.format = 'SVG' }.first
-     img.format = "png"
-     image = Base64.encode64(img.to_blob);
-     render :text => "data:image/png;base64,#{image}"
+    img = Magick::Image.from_blob(params[:image]){ self.density = '144.0x144.0'; self.format = 'SVG'; }.first
+    img.format = "png"
+    image = Base64.encode64(img.to_blob);
+    render :text => "data:image/png;base64,#{image}"
   end
   
 end
