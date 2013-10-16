067406990624676a090abe13f4550e82e089c652
Toon Willems <willemstoon@gmail.com>
2013-10-16 15:56:58 +0200

    basic matrix implementation

--- app/assets/javascripts/pancore.js
+++ app/assets/javascripts/pancore.js
@@ -272,6 +272,9 @@ function init_pancore() {
         case 'sequencesDownloaded':
             returnPopoverSequences(data.msg.sequences, data.msg.type);
             break;
+        case 'sim_matrix':
+            showMatrix(data.msg.genomes, data.msg.sim_matrix, data.msg.order);
+            break;
         default:
             console.log(data.msg);
         }
@@ -399,6 +402,18 @@ function init_pancore() {
     // Sends a command and message to the worker
     function sendToWorker(command, message) {
         worker.postMessage({'cmd': command, 'msg': message});
+    }
+
+    // setup similarity matrix buttons etc
+    $("#sim_matrix_buttons").prepend("<button id='calculate-matrix-btn' class='btn btn-mini'><i class='icon-refresh'></i>Calculate Similarity Matrix</button>");
+    $("#calculate-matrix-btn").click(function () {
+        sendToWorker('calculateSimilarity', '');
+    });
+
+    function showMatrix(genomes, data, order) {
+        $('#sim_matrix').empty();
+        init_sim_matrix(genomes, data, order);
+
     }
 
     // Initial method for adding genomes to the graph
