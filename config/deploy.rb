7db980165e79a7326554406c1eb75ae71d7289a1
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-10-18 15:47:04 +0200

    deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,19 @@ set :repository,  "ssh://git@github.ugent.be/bmesuere/unipept.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+task :feat do
+  set :deploy_to, "/home/bmesuere/rails"
+  set :branch, "feature/griet"
+  set :user, "bmesuere"
+  set :use_sudo, false
+  set :port, 4840
+  set :deploy_via, :remote_cache
+
+  role :web, "sherlock.ugent.be"                          # Your HTTP server, Apache/etc
+  role :app, "sherlock.ugent.be"                          # This may be the same as your `Web` server
+  role :db,  "sherlock.ugent.be", :primary => true # This is where Rails migrations will run
+end
+
 task :dev do
   set :deploy_to, "/home/bmesuere/rails"
   set :branch, "develop"
@@ -32,9 +45,9 @@ task :prod do
   role :web, "nibbler.ugent.be"                          # Your HTTP server, Apache/etc
   role :app, "nibbler.ugent.be"                          # This may be the same as your `Web` server
   role :db,  "nibbler.ugent.be", :primary => true # This is where Rails migrations will run
-  
+
   # We need to run this after our collector mongrels are up and running
-  # This goes out even if the deploy fails, sadly 
+  # This goes out even if the deploy fails, sadly
   after "deploy:update", "newrelic:notice_deployment"
 end
 
