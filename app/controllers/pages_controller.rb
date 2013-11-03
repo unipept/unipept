402099ef9562acb15ed3f5db14a66324cd255250
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-11-03 10:09:31 +0100

    don't load the datasets for the homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,8 @@
 class PagesController < ApplicationController
   before_filter :authorize, :only => [:admin]
-  
+
   def home
     @title = "Home"
-    @datasets = Dataset.includes(:dataset_items).all
     @post = Post.last
   end
 
