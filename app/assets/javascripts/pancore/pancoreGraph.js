7c22afe590cf55dcf0f9f6bfb6f2fa9df8b19ec9
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-11-23 17:20:53 +0100

    move redraw and update to object

--- app/assets/javascripts/pancore/pancoreGraph.js
+++ app/assets/javascripts/pancore/pancoreGraph.js
@@ -27,6 +27,7 @@ var constructPancoreGraph = function constructPancoreGraph(args) {
 
     // Data
     var genomes = {},
+        graphData = {},
         legendData;
 
     // Drag and click
@@ -55,8 +56,8 @@ var constructPancoreGraph = function constructPancoreGraph(args) {
         graphArea,
         tooltip,
         // Scales
-        pancoreX,
-        pancoreY,
+        xScale,
+        yScale,
         // Axes
         xAxis,
         yAxis,
@@ -68,7 +69,7 @@ var constructPancoreGraph = function constructPancoreGraph(args) {
     /*************** Private methods ***************/
 
     /**
-     * Initializes...
+     * Initializes the graph
      */
     function init() {
         legendData = [{"name": "genome size", "color": genomeColor, "toggle": "showGenome"},
@@ -76,32 +77,370 @@ var constructPancoreGraph = function constructPancoreGraph(args) {
             {"name": "core peptidome", "color": coreColor, "toggle": "showCore"},
             {"name": "unique peptides", "color": unicoreColor, "toggle": "showUnicore"}];
 
-        pancoreX = d3.scale.ordinal().rangePoints([0, width], 1);
-        pancoreY = d3.scale.linear().range([height, 0]);
+        xScale = d3.scale.ordinal().rangePoints([0, width], 1);
+        yScale = d3.scale.linear().range([height, 0]);
 
         xAxis = d3.svg.axis()
-            .scale(pancoreX)
+            .scale(xScale)
             .tickFormat(function (d) { return genomes[d].name || ""; })
             .orient("bottom");
         yAxis = d3.svg.axis()
-            .scale(pancoreY)
+            .scale(yScale)
             .orient("left");
 
         panLine = d3.svg.line()
             .interpolate("linear")
-            .x(function (d) { return pancoreX(d.bioproject_id); })
-            .y(function (d) { return pancoreY(d.pan); });
+            .x(function (d) { return xScale(d.bioproject_id); })
+            .y(function (d) { return yScale(d.pan); });
         coreLine = d3.svg.line()
             .interpolate("linear")
-            .x(function (d) { return pancoreX(d.bioproject_id); })
-            .y(function (d) { return pancoreY(d.core); });
+            .x(function (d) { return xScale(d.bioproject_id); })
+            .y(function (d) { return yScale(d.core); });
         unicoreLine = d3.svg.line()
             .interpolate("linear")
-            .x(function (d) { return pancoreX(d.bioproject_id); })
-            .y(function (d) { return pancoreY(d.unicore); });
+            .x(function (d) { return xScale(d.bioproject_id); })
+            .y(function (d) { return yScale(d.unicore); });
     }
 
     /*************** Public methods ***************/
+
+    /**
+     * Redraws the entire pancore graph
+     */
+    that.redraw = function redraw() {
+        // erase everything
+        $("#pancore_graph svg").remove();
+        $("#pancore_graph div.tip").remove();
+
+        // create the svg
+        svg = d3.select("#pancore_graph")
+          .append("svg")
+            .attr("version", "1.1")
+            .attr("xmlns", "http://www.w3.org/2000/svg")
+            .attr("viewBox", "0 0 " + fullWidth + " " + fullHeight)
+            .attr("width", fullWidth)
+            .attr("height", fullHeight)
+            .attr("overflow", "hidden")
+            .style("font-family", "'Helvetica Neue', Helvetica, Arial, sans-serif")
+          //.on("click", removePopoversAndHighlights)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // create the tooltip
+        tooltip = d3.select("#pancore_graph")
+          .append("div")
+            .attr("class", "tip")
+            .style("position", "absolute")
+            .style("top", "0px")
+            .style("left", "0px")
+            .style("z-index", "10")
+            .style("visibility", "hidden");
+
+        // create the dropshadow filter
+        var temp = svg.append("svg:defs")
+            .append("svg:filter")
+                .attr("id", "dropshadow");
+        temp.append("svg:feGaussianBlur")
+            .attr("in", "SourceAlpha")
+            .attr("stdDeviation", 1);
+        temp = temp.append("svg:feMerge");
+        temp.append("svg:feMergeNode");
+        temp.append("svg:feMergeNode")
+            .attr("in", "SourceGraphic");
+
+        // add the x-axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        // add the y-axis
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+          .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Number of peptides");
+
+        svg.selectAll(".axis line, .axis path")
+            .style("fill", "none")
+            .style("stroke", "#000")
+            .style("shape-rendering", "crispEdges");
+
+        // container used for dots, paths and bars
+        graphArea = svg.append("g").attr("class", "graphArea");
+
+        // add legend
+        var legend = svg.selectAll(".legend")
+            .data(legendData)
+          .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; })
+            //.on("click", legendClick);
+        legend.append("rect")
+            .attr("x", 30)
+            .attr("width", 8)
+            .attr("height", 8)
+            .style("shape-rendering", "crispEdges")
+            .style("fill", function (d) { return d.color; });
+        legend.append("text")
+            .attr("x", 40)
+            .attr("y", 8)
+            .style("text-anchor", "start")
+            .text(function (d) { return d.name; });
+        $(".legend").disableSelection();
+
+        // draw the lines
+        graphArea.append("path")
+            .datum(graphData)
+            .attr("class", "line pan")
+            .style("stroke", panColor)
+            .attr("d", panLine);
+        graphArea.append("path")
+            .datum(graphData)
+            .attr("class", "line core")
+            .style("stroke", coreColor)
+            .attr("d", coreLine);
+        graphArea.append("path")
+            .datum(graphData)
+            .attr("class", "line unicore")
+            .style("stroke", unicoreColor)
+            .attr("d", unicoreLine);
+        graphArea.selectAll("path.line")
+            .style("stroke-width", 2)
+            .style("fill", "none");
+
+        // add axis marks
+        svg.insert("line")
+            .attr("class", "axisline genome")
+            .attr("stroke", genomeColor);
+        svg.insert("line")
+            .attr("class", "axisline pan")
+            .attr("stroke", panColor);
+        svg.insert("line")
+            .attr("class", "axisline core")
+            .attr("stroke", coreColor);
+        svg.insert("line")
+            .attr("class", "axisline unicore")
+            .attr("stroke", unicoreColor);
+        svg.selectAll("line.axisline")
+            .attr("stroke-width", "2")
+            .attr("x1", "6")
+            .attr("x2", "-6")
+            .attr("shape-rendering", "crispEdges")
+            .style("visibility", "hidden");
+
+        // trash bin
+        var trash = svg.insert("g")
+            .attr("id", "trash")
+            .attr("fill", "#cccccc")
+            .style("opacity", "0")
+            //.on("mouseover", trashMouseOver)
+            //.on("mouseout", trashMouseOut)
+        .insert("g")
+            .attr("transform", "translate(" + (fullWidth + 30) + " " + (height - 46) / 2 + ")");
+        trash.append("circle")
+            .attr("r", 30)
+            .attr("stroke", "#cccccc")
+            .attr("stroke-width", 3)
+            .attr("fill", "white")
+            .attr("cx", -40)
+            .attr("cy", 23);
+        trash.append("rect")
+            .attr("height", height)
+            .attr("width", 32)
+            .attr("y", -182)
+            .attr("x", -40)
+            .style("fill-opacity", 0);
+        trash = trash.insert("g")
+            .attr("transform", "translate(-430 -597)")
+            .style("pointer-events", "none");
+        trash.insert("path").attr("d", "M408.302,604.218h-7.95c0.002-0.055,0.009-0.109,0.009-0.166v-1.6c0-2.91-2.384-5.293-5.294-5.293h-8.824    c-2.912,0-5.294,2.383-5.294,5.293v1.6c0,0.057,0.007,0.111,0.009,0.166h-7.95c-1.456,0-2.646,1.191-2.646,2.646v2.646    c0,1.457,1.19,2.648,2.646,2.648h0.883v-1.766h33.529v1.766h0.883c1.455,0,2.646-1.191,2.646-2.648v-2.646    C410.948,605.409,409.757,604.218,408.302,604.218z M385.333,601.571h10.588v2.646h-10.588V601.571z");
+        trash.insert("path").attr("d", "M375.654,613.042v26.469c0,1.457,1.19,2.648,2.647,2.648h24.705c1.456,0,2.647-1.191,2.647-2.648v-26.469    H375.654z M384.478,636.423c0,0.486-0.397,0.883-0.882,0.883h-1.765c-0.486,0-0.883-0.396-0.883-0.883v-17.646    c0-0.484,0.396-0.883,0.883-0.883h1.765c0.484,0,0.882,0.398,0.882,0.883V636.423z M392.419,636.423    c0,0.486-0.398,0.883-0.882,0.883h-1.766c-0.485,0-0.882-0.396-0.882-0.883v-17.646c0-0.484,0.396-0.883,0.882-0.883h1.766    c0.483,0,0.882,0.398,0.882,0.883V636.423z M400.36,636.423c0,0.486-0.398,0.883-0.883,0.883h-1.765    c-0.485,0-0.882-0.396-0.882-0.883v-17.646c0-0.484,0.396-0.883,0.882-0.883h1.765c0.484,0,0.883,0.398,0.883,0.883V636.423z");
+    };
+
+    /**
+     * Updates the pancore graph
+     */
+    that.update = function update() {
+        // Prepare for line transition
+        var oldPanDatum = svg.select(".line.pan").datum(),
+            oldCoreDatum = svg.select(".line.core").datum();
+        if (oldPanDatum.length < graphData.length && oldPanDatum.length > 0) {
+            var i,
+                diff = graphData.length - oldPanDatum.length;
+            for (i = 0; i < diff; i++) {
+                oldPanDatum.push(oldPanDatum[oldPanDatum.length - 1]);
+                oldCoreDatum.push(oldCoreDatum[oldCoreDatum.length - 1]);
+            }
+            svg.select(".line.pan").attr("d", panLine);
+            svg.select(".line.core").attr("d", coreLine);
+        }
+
+        // set the domains
+        xScale.domain(graphData.map(function (d) { return d.bioproject_id; }));
+        yScale.domain([0, getMaxVisibleDatapoint()]);
+
+        // update the axes
+        svg.select(".x.axis").transition().duration(transitionDuration).call(xAxis);
+        svg.select(".y.axis").transition().duration(transitionDuration).call(yAxis);
+        svg.selectAll(".axis line, .axis path")
+            .style("fill", "none")
+            .style("stroke", "#000")
+            .style("shape-rendering", "crispEdges");
+        // rotate the x-axis labels
+        svg.selectAll(".x.axis text")
+            .style("text-anchor", "end")
+            .transition()
+                .duration(transitionDuration)
+                .attr("transform", "translate(-5,0)rotate(-45)");
+        svg.selectAll(".tick").attr("class", function (d) { return "tick major _" + d; });
+
+        // draw the dots
+        var genomeDots = graphArea.selectAll(".dot.genome")
+            .data(graphData, function (d) { return d.bioproject_id; });
+        genomeDots.enter().append("circle")
+            .attr("class", function (d) { return "dot genome _" + d.bioproject_id; })
+            .attr("r", 5)
+            .attr("fill", genomeColor)
+            .attr("cx", width);
+        genomeDots.transition()
+            .duration(transitionDuration)
+            .attr("cx", function (d) { return xScale(d.bioproject_id); })
+            .attr("cy", function (d) { return yScale(d.peptides); })
+            .attr("opacity", toggles.showGenome ? 1 : 0);
+        genomeDots.exit()
+            .transition()
+                .attr("cy", height / 2)
+                .attr("cx", width)
+            .remove();
+        var panDots = graphArea.selectAll(".dot.pan")
+            .data(graphData, function (d) { return d.bioproject_id; });
+        panDots.enter().append("circle")
+            .attr("class", function (d) { return "dot pan _" + d.bioproject_id; })
+            .attr("r", 5)
+            .attr("fill", panColor)
+            .attr("cx", width);
+        panDots.transition()
+            .duration(transitionDuration)
+            .attr("cx", function (d) { return xScale(d.bioproject_id); })
+            .attr("cy", function (d) { return yScale(d.pan); })
+            .attr("opacity", toggles.showPan ? 1 : 0);
+        panDots.exit()
+            .transition()
+                .attr("cy", height / 2)
+                .attr("cx", width)
+            .remove();
+        var coreDots = graphArea.selectAll(".dot.core")
+            .data(graphData, function (d) {return d.bioproject_id; });
+        coreDots.enter().append("circle")
+            .attr("class", function (d) { return "dot core _" + d.bioproject_id; })
+            .attr("r", 5)
+            .attr("fill", coreColor)
+            .attr("cx", width)
+            .attr("cy", yScale(0));
+        coreDots.transition()
+            .duration(transitionDuration)
+            .attr("cx", function (d) { return xScale(d.bioproject_id); })
+            .attr("cy", function (d) { return yScale(d.core); })
+            .attr("opacity", toggles.showCore ? 1 : 0);
+        coreDots.exit()
+            .transition()
+                .attr("cy", height / 2)
+                .attr("cx", width)
+            .remove();
+        var unicoreDots = graphArea.selectAll(".dot.unicore")
+            .data(graphData.filter(function (entry) {
+                return entry.unicore !== undefined;
+            }), function (d) {return d.bioproject_id; });
+        unicoreDots.enter().append("circle")
+            .attr("class", function (d) { return "dot unicore _" + d.bioproject_id; })
+            .attr("r", 5)
+            .attr("fill", unicoreColor)
+            .attr("cx", width)
+            .attr("cy", yScale(0));
+        unicoreDots.transition()
+            .duration(transitionDuration)
+            .attr("cx", function (d) { return xScale(d.bioproject_id); })
+            .attr("cy", function (d) { return yScale(d.unicore); })
+            .attr("opacity", toggles.showUnicore ? 1 : 0);
+        unicoreDots.exit()
+            .transition()
+                .attr("cy", height / 2)
+                .attr("cx", width)
+            .remove();
+
+        // update the lines
+        var dataCopy = graphData.slice(0);
+        if (graphData.length > 0) {
+            graphArea.select(".line.pan").datum(dataCopy)
+                .style("visibility", "visible")
+                .transition()
+                    .duration(transitionDuration)
+                    .style("stroke", panColor)
+                    .attr("opacity", toggles.showPan ? 1 : 0)
+                    .attr("d", panLine);
+            graphArea.select(".line.core").datum(dataCopy)
+                .style("visibility", "visible")
+                .transition()
+                    .duration(transitionDuration)
+                    .style("stroke", coreColor)
+                    .attr("opacity", toggles.showCore ? 1 : 0)
+                    .attr("d", coreLine);
+        } else {
+            // hide the lines when there's no data
+            // drawing them with no data results in JS errors
+            graphArea.select(".line.pan").style("visibility", "hidden");
+            graphArea.select(".line.core").style("visibility", "hidden");
+        }
+        if (graphData.length > 0 && graphData[0].unicore !== undefined) {
+            graphArea.select(".line.unicore").datum(dataCopy)
+                .style("visibility", "visible")
+                .transition()
+                    .duration(transitionDuration)
+                    .style("stroke", unicoreColor)
+                    .attr("opacity", toggles.showUnicore ? 1 : 0)
+                    .attr("d", unicoreLine);
+        } else {
+            graphArea.select(".line.unicore").style("visibility", "hidden");
+        }
+
+        // update the mouseover rects
+        mouseOverWidth = (width / graphData.length) / 1.5;
+        var bars = graphArea.selectAll(".bar")
+            .data(graphData, function (d) {return d.bioproject_id; });
+
+        bars.enter().append("polygon")
+            .attr("class", "bar")
+            .style("fill-opacity", 0)
+            /*.on("mouseover", mouseOver)
+            .on("mouseout", mouseOut)
+            .on("mousemove", mouseMove)
+            .on("click", mouseClick)
+            .call(d3.behavior.drag()
+                .on("dragstart", dragStart)
+                .on("drag", drag)
+                .on("dragend", dragEnd))*/;
+        bars.transition()
+            .duration(transitionDuration)
+            .attr("points", function (d) {
+                var ret = "";
+                ret += (xScale(d.bioproject_id) - mouseOverWidth / 2) + ", " + (height + 15) + " ";
+                ret += (xScale(d.bioproject_id) - mouseOverWidth / 2) + ", 0 ";
+                ret += (xScale(d.bioproject_id) + mouseOverWidth / 2) + ", 0 ";
+                ret += (xScale(d.bioproject_id) + mouseOverWidth / 2) + ", " + (height + 15) + " ";
+                ret += (xScale(d.bioproject_id) + mouseOverWidth / 2 - (margin.bottom - 15)) + ", " + (height + margin.bottom) + " ";
+                ret += (xScale(d.bioproject_id) - mouseOverWidth / 2 - (margin.bottom - 15)) + ", " + (height + margin.bottom) + " ";
+                return ret;
+            });
+        bars.exit().remove();
+
+        // Put the handlebars on top
+        $(".bar").parent().append($(".bar"));
+    };
 
 
     // initialize the object
