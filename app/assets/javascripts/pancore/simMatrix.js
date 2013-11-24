c2849bcfe7a412ae5bb01d6dcea3ad511dbc652b
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-11-24 18:10:54 +0100

    prepare for matrix object

---
+++ app/assets/javascripts/pancore/simMatrix.js
@@ -0,0 +1,27 @@
+/**
+ * Blabla
+ *
+ * @param <Array> args.bla blabla
+ */
+var constructSimMatrix = function constructSimMatrix(args) {
+    /*************** Private variables ***************/
+
+    var that = {};
+
+    /*************** Private methods ***************/
+
+    /**
+     * Initializes pan core
+     */
+    function init() {
+
+    }
+
+    /*************** Public methods ***************/
+
+
+    // initialize the object
+    init();
+
+    return that;
+};
