5710f56fa6d3208752eeb12e262d7e5262fe89db
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-11-24 12:19:31 +0100

    prepare for genome table object

---
+++ app/assets/javascripts/pancore/genomeTable.js
@@ -0,0 +1,30 @@
+/**
+ * creates a genomeTable object representing the table showing all genomes
+ * currently in the visualisation. features: reorder by dragging, autosort,
+ * remove individual genomes, remove all genome.
+ *
+ * @param <Hash> args blablabla
+ */
+var constructGenomeTable = function constructGenomeTable(args) {
+    /*************** Private variables ***************/
+
+    var that = {};
+
+    /*************** Private methods ***************/
+
+    /**
+     * Initializes
+     */
+    function init() {
+
+    }
+
+    /*************** Public methods ***************/
+
+
+
+    // initialize the object
+    init();
+
+    return that;
+};
