d276c40939f1bdd9f125c511bf3497acafd9658a
Bart Mesuere <Bart.Mesuere@UGent.be>
2013-12-01 16:10:59 +0100

    fix creating a new dataset

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -10,6 +10,7 @@
 #
 
 class Dataset < ActiveRecord::Base
+  attr_accessible :environment, :reference, :url, :project_website, :dataset_items_attributes
   has_many :dataset_items, :dependent => :destroy
   accepts_nested_attributes_for :dataset_items, :allow_destroy => true
 end
