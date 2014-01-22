44d553adf7489ec4f1766020e2907a1c087d888e
Toon Willems <willemstoon@gmail.com>
2014-01-22 14:39:55 +0100

    use #pluck instead of select_values

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -100,7 +100,6 @@ class Sequence < ActiveRecord::Base
       data = data.gsub(/([KR])([^P])/,"\\1\n\\2").gsub(/([KR])([^P])/,"\\1\n\\2") unless handle_missed
       data = data.lines.map(&:strip).to_a.select{|l| l.size >= 5}
       data_counts = Hash[data.group_by{|k| k}.map{|k,v| [k, v.length]}]
-      number_searched_for = data.length
       data = data_counts.keys
 
       # build the resultset
@@ -199,12 +198,12 @@ class Sequence < ActiveRecord::Base
 
   # Returns an array of sequences strings based on a list of sequence id's
   def self.list_sequences(ids)
-    connection.select_values(Sequence.select(:sequence).where(:id => ids).to_sql).to_a
+    Sequence.where(id: ids).pluck(:sequence)
   end
 
   # Filters a list of sequences for a given lca
   def self.filter_unique_uniprot_peptides(sequences, lca)
-    connection.select_values(Sequence.select(:id).where(:id => sequences, :lca => lca).to_sql).to_a.sort!
+    Sequence.where(id: sequences, lca: lca).order(:id).pluck(:id)
   end
 
   # Filters a list of sequences for a given lca
