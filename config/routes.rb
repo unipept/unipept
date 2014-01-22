e4cda711a375d68a2b368347f720ee358978235d
Toon Willems <willemstoon@gmail.com>
2014-01-22 19:32:57 +0100

    add lca api

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,7 @@ UnipeptWeb::Application.routes.draw do
   # API namespace
   namespace :api, path: 'api/v1' do
     get 'single' => "api#single"
+    get 'lca' => "api#lca"
   end
 
   # The priority is based upon order of creation:
