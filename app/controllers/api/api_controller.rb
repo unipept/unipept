1c3a8abda975101cf72f41a1fb7e2f6f20490103
Toon Willems <willemstoon@gmail.com>
2014-03-11 11:02:36 +0100

    fix controller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -5,21 +5,20 @@ class Api::ApiController < ApplicationController
   before_filter :set_params, only: [:single, :lca]
 
   def set_params
-    @sequences = params[:sequences]
+    @sequences = params[:sequences].map(&:chomp)
     @equate_il = (!params[:equate_il].blank? && params[:equate_il] == 'true')
   end
 
   def single
     sequences = @sequences.map { |s| Sequence.single_search(s.upcase, @equate_il) }
-    peptides = sequences.map { |s| s.peptides.map(&:uniprot_entry).map(&:name) }
+    peptides = sequences.reject(&:nil?).map { |s| s.peptides.map(&:uniprot_entry).map(&:name) }
 
     respond_with(:api, peptides)
   end
 
   def lca
-    equate_il = params[:equate_il] == true
-    sequences = params[:sequences].map {|s| Sequence.single_search(s.upcase, equate_il) }
-    if equate_il
+    sequences = @sequences.map {|s| Sequence.single_search(s.upcase, @equate_il) }
+    if @equate_il
       taxons = Taxon.find(sequences.map(&:lca_il))
     else
       taxons = Taxon.find(sequences.map(&:lca))
