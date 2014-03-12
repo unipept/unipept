87f2778b57626f8d8abd47654f628e9ffb21f2eb
Toon Willems <willemstoon@gmail.com>
2014-03-12 19:46:00 +0100

    fix selection of attributes

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -7,21 +7,26 @@ class Api::ApiController < ApplicationController
   def set_params
     @sequences = params[:sequences].map(&:chomp)
     @equate_il = (!params[:equate_il].blank? && params[:equate_il] == 'true')
+    @filter = params[:filter]
   end
 
   def single
     sequences = @sequences.map { |s| Sequence.single_search(s.upcase, @equate_il) }
-    peptides = sequences.reject(&:nil?).map { |s| s.peptides.map(&:uniprot_entry).map(&:name) }
+    peptides = sequences.reject(&:nil?).map { |s| s.peptides.map(&:uniprot_entry).map(&:taxon_id) }
+
+    base_search = @filter.blank? ? Taxon : Taxon.select(@filter)
+    peptides = base_search.find(peptides)
 
     respond_with(:api, peptides)
   end
 
   def lca
     sequences = @sequences.map {|s| Sequence.single_search(s.upcase, @equate_il) }
+    base_search = @filter.blank? ? Taxon : Taxon.select(@filter)
     if @equate_il
-      taxons = Taxon.find(sequences.map(&:lca_il))
+      taxons = base_search.find(sequences.map(&:lca_il))
     else
-      taxons = Taxon.find(sequences.map(&:lca))
+      taxons = base_search.find(sequences.map(&:lca))
     end
 
     respond_with(:api, taxons)
