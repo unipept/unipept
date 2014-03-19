231c224ae3ad529779d7618f1bd46753f0175344
Toon Willems <willemstoon@gmail.com>
2014-03-19 09:59:17 +0100

    filtering will be done client-side

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -7,27 +7,22 @@ class Api::ApiController < ApplicationController
   def set_params
     @sequences = params[:sequences].map(&:chomp)
     @equate_il = (!params[:equate_il].blank? && params[:equate_il] == 'true')
-    @filter = params[:filter]
   end
 
   def single
     sequences = @sequences.map { |s| Sequence.single_search(s.upcase, @equate_il) }
     peptides = sequences.reject(&:nil?).map { |s| s.peptides.map(&:uniprot_entry).map(&:taxon_id) }
 
-    base_search = @filter.blank? ? Taxon : Taxon.select(@filter)
-    peptides = base_search.find(peptides)
+    peptides = Taxon.find(peptides)
 
     respond_with(:api, peptides)
   end
 
   def lca
     sequences = @sequences.map {|s| Sequence.single_search(s.upcase, @equate_il) }
-    base_search = @filter.blank? ? Taxon : Taxon.select(@filter)
-    if @equate_il
-      taxons = base_search.find(sequences.map(&:lca_il))
-    else
-      taxons = base_search.find(sequences.map(&:lca))
-    end
+    name = @equate_il ? :lca_il : :lca
+
+    taxons = Taxon.find(sequences.map(&name))
 
     respond_with(:api, taxons)
   end
