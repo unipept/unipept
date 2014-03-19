b969332109ba3cb99480b8c1d3cffc8b70c1d7ba
Toon Willems <willemstoon@gmail.com>
2014-03-19 10:33:32 +0100

    rename fiels in output, valid_taxon is filtered. Fixes #221 and #226

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -13,18 +13,18 @@ class Api::ApiController < ApplicationController
     sequences = @sequences.map { |s| Sequence.single_search(s.upcase, @equate_il) }
     peptides = sequences.reject(&:nil?).map { |s| s.peptides.map(&:uniprot_entry).map(&:taxon_id) }
 
-    peptides = Taxon.find(peptides)
+    @peptides = Taxon.find(peptides)
 
-    respond_with(:api, peptides)
+    respond_with(:api, @peptides)
   end
 
   def lca
     sequences = @sequences.map {|s| Sequence.single_search(s.upcase, @equate_il) }
+
     name = @equate_il ? :lca_il : :lca
+    @taxons = Taxon.find(sequences.map(&name))
 
-    taxons = Taxon.find(sequences.map(&name))
-
-    respond_with(:api, taxons)
+    respond_with(:api, @taxons)
   end
 
 end
