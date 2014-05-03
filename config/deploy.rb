b7a6e186ccbb3310266ca895a65682e5e83f8548
Toon Willems <willemstoon@gmail.com>
2014-05-03 18:06:42 +0200

    migrate to capistrano 3.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,59 +1,25 @@
-require 'bundler/capistrano'
-
 set :application, "unipept-web"
-set :repository,  "ssh://git@github.ugent.be/bmesuere/unipept.git"
+set :repo_url,  "ssh://git@github.ugent.be/bmesuere/unipept.git"
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_to, "/home/bmesuere/rails"
 
-task :feat do
-  set :deploy_to, "/home/bmesuere/rails"
-  set :branch, "feature/thesis"
-  set :user, "bmesuere"
-  set :use_sudo, false
-  set :port, 4840
-  set :deploy_via, :remote_cache
+# set :deploy_to, '/var/www/my_app'
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
 
-  role :web, "scruffy.ugent.be"                          # Your HTTP server, Apache/etc
-  role :app, "scruffy.ugent.be"                          # This may be the same as your `Web` server
-  role :db,  "scruffy.ugent.be", :primary => true # This is where Rails migrations will run
-end
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-task :dev do
-  set :deploy_to, "/home/bmesuere/rails"
-  set :branch, "develop"
-  set :user, "bmesuere"
-  set :use_sudo, false
-  set :port, 4840
-  set :deploy_via, :remote_cache
-
-  role :web, "scruffy.ugent.be"                          # Your HTTP server, Apache/etc
-  role :app, "scruffy.ugent.be"                          # This may be the same as your `Web` server
-  role :db,  "scruffy.ugent.be", :primary => true # This is where Rails migrations will run
-end
-
-task :prod do
-  load 'deploy/assets'
-  set :deploy_to, "/home/bmesuere/rails"
-  set :branch, "master"
-  set :user, "bmesuere"
-  set :use_sudo, false
-  set :port, 4840
-  set :deploy_via, :remote_cache
-
-  role :web, "sherlock.ugent.be"                          # Your HTTP server, Apache/etc
-  role :app, "sherlock.ugent.be"                          # This may be the same as your `Web` server
-  role :db,  "sherlock.ugent.be", :primary => true # This is where Rails migrations will run
-end
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+    execte :touch, release_path.join('tmp','restart.txt'
   end
+
 end
---
+++ config/deploy/prod.rb
@@ -0,0 +1,9 @@
+set :stage, :dev
+
+# don't specify db as it's not needed for unipept
+server "scruffy.ugent.be" user: 'bmesuere', roles: %{web app}, ssh_options: {
+  port: 4840,
+}
+
+set :branch, 'develop'
+set :rails_env, :development
