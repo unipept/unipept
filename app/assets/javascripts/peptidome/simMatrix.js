c7e3a42834c6d3c94da6e023a5e5c0a87bc76013
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-05-15 18:23:24 +0200

    larger modal

--- app/assets/javascripts/peptidome/simMatrix.js
+++ app/assets/javascripts/peptidome/simMatrix.js
@@ -555,7 +555,7 @@ var constructSimMatrix = function constructSimMatrix(args) {
     that.handleSaveData = function handleSaveData() {
         $("#downloadModal").remove();
         $("body").append("<div id='downloadModal' class='modal fade' tabindex='-1' role='dialog'>" +
-          "<div class='modal-dialog modal-sm'>" +
+          "<div class='modal-dialog'>" +
             "<div class='modal-content'>" +
               "<div class='modal-header'>" +
                 "<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>" +
