2a52cca491534be74a994aca7e0a48997813d7cd
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-05-27 11:20:09 +0200

    tableless users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ActiveRecord::Base
   devise :cas_authenticatable
 
+  def is_admin
+    return true
+  end
+
+  # go tableless
+  def self.columns
+    @columns ||= [];
+  end
+
+  def self.column(name, sql_type = nil, default = nil, null = true)
+    columns << ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default,
+      sql_type.to_s, null)
+  end
+
+  # Override the save method to prevent exceptions.
+  def save(validate = true)
+    validate ? valid? : true
+  end
 end
