0d174e8bd6d339348dd69fbc483c899a718030eb
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-05-27 11:22:32 +0200

    remove some more old code

--- config/routes.rb
+++ config/routes.rb
@@ -49,11 +49,6 @@ UnipeptWeb::Application.routes.draw do
   # load pride dataset from webservice
   get '/pride/:id', :to => 'pride#load'
 
-  # verbosity is needed to add namespace to controller
-  get "cas/auth", :to => "cas#auth"
-  get "cas/logout", :to => "cas#logout"
-  get "cas/verify", :to => "cas#verify"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
