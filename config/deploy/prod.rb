5b5db5a7af7bb3b097b158007a4829beefb23731
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-06-07 15:06:52 +0200

    fix workers and fonts

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -8,3 +8,17 @@ server "sherlock.ugent.be", user: 'bmesuere', roles: [:web, :app], ssh_options: 
 
 set :branch, 'master'
 set :rails_env, :production
+
+namespace :deploy do
+
+  after :compile_assets, :asset_stuff do
+    on roles :all do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'assets:nodigest'
+        end
+      end
+    end
+  end
+
+end
