83308268aa52cf4e31229c6a76a1b61d59562aa7
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-06-25 14:08:02 +0200

    update compile script

--- backend/scripts/compile.sh
+++ backend/scripts/compile.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # compiles all java files
 
-rm -rf backend/java/bin
-mkdir backend/java/bin
-javac -cp "backend/java/lib/mysql.jar" -d backend/java/bin/ backend/java/src/*/*.java backend/java/src/*/*/*.java
+currentdir=`pwd`
+
+cd "backend/java"
+mvn clean
+mvn package
+
+cd "${currentdir}"
