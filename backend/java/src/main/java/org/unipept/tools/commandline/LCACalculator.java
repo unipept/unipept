1c6fd8ed515bedd0695aa2553b248d1a626faa2a
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-06-25 13:49:40 +0200

    calculate LCA

--- backend/java/src/main/java/org/unipept/tools/commandline/LCACalculator.java
+++ backend/java/src/main/java/org/unipept/tools/commandline/LCACalculator.java
@@ -1,8 +1,8 @@
 package org.unipept.tools.commandline;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.sql.Timestamp;
+import java.util.*;
 
 /**
  * This script calculates all lowest common ancestors
@@ -21,19 +21,67 @@ public class LCACalculator {
 
         br.lines()
                 .skip(1)
-                .forEach((line) -> {
-                    String[] elements = line.split(",");
+                .forEach(line -> {
+                    String[] elements = SEPARATOR.split(line);
 
                     int key = Integer.parseInt(elements[0]);
                     int[] lineage = Arrays.stream(elements)
-                            .mapToInt((s) -> s.equals("NULL") ? 0 : Integer.parseInt(s))
+                            .mapToInt(s -> s.equals("NULL") ? 0 : Integer.parseInt(s))
                             .toArray();
 
                     taxonomyMap.put(key, lineage);
                 });
 
-        int max = taxonomyMap.keySet().stream().mapToInt(x -> x).max().getAsInt();
+        int max = taxonomyMap.keySet().stream().max(Integer::compare).get();
         taxonomy = new int[max + 1][];
         taxonomyMap.keySet().stream().forEach(key -> taxonomy[key] = taxonomyMap.get(key));
+    }
+
+    public static void calculateLCAs(String file) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(file));
+        sc.nextLine(); // skip header
+
+        int count = 0;
+
+        int currentSequence = -1;
+        Collection<Integer> taxa = new ArrayList<>();
+        while (sc.hasNext()) {
+            count++;
+            if (count % 1000000 == 0) {
+                System.err.println(new Timestamp(System.currentTimeMillis()) + ": " + count);
+            }
+            String[] split = SEPARATOR.split(sc.nextLine());
+            int sequenceId = Integer.parseInt(split[0]);
+            int taxonId = Integer.parseInt(split[1]);
+
+            if (sequenceId != currentSequence) {
+                if (currentSequence != -1) {
+                    int lca = 1;
+                    int[][] lineages = taxa.stream()
+                            .map(t -> taxonomy[t])
+                            .filter(l -> l != null)
+                            .toArray(int[][]::new);
+                    for (int rank = 0; rank < lineages[0].length; rank++) {
+                        final int finalRank = rank;
+                        int[] current = Arrays.stream(lineages)
+                                .mapToInt(l -> l[finalRank])
+                                .filter(i -> i > 0)
+                                .toArray();
+                        if (current.length > 1) {
+                            break;
+                        }
+                        if (current.length > 0 && current[0] != 0) {
+                            lca = current[0];
+                        }
+                    }
+                    //System.out.println(currentSequence + "," + lca);
+                }
+
+                currentSequence = sequenceId;
+                taxa.clear();
+            }
+
+            taxa.add(taxonId);
+        }
     }
 }
