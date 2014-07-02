4c16bfefd2ddbe13fad990ead92c0ae564cfcc0a
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-07-02 17:42:37 +0200

    remove jit treeview

--- app/assets/javascripts/multi_search.js
+++ app/assets/javascripts/multi_search.js
@@ -33,15 +33,7 @@ function init_multi(data, data2, equate_il) {
 
     // treeview
     try {
-        initTreeView(data);
-        $("#jitTreeViewWrapper").removeClass("active");
-    } catch (err) {
-        error(err.message, "Loading the Treeview visualization failed. Please use Google Chrome, Firefox or Internet Explorer 9 or higher.");
-    }
-
-    // treeview2
-    try {
-        initTreeView2(JSON.parse(JSON.stringify(data)));
+        initTreeView(JSON.parse(JSON.stringify(data)));
     } catch (err) {
         error(err.message, "Loading the Treeview visualization failed. Please use Google Chrome, Firefox or Internet Explorer 9 or higher.");
     }
@@ -185,141 +177,12 @@ function initTreeMap(jsonData) {
     }
 }
 
-function initTreeView(jsonData) {
-    var st = new $jit.ST({
-        injectInto: 'jitTreeView',
-        // id of viz container element
-        duration: 800,
-        // set duration for the animation
-        transition: $jit.Trans.Quart.easeInOut,
-        // set animation transition type
-        levelDistance: 50,
-        // set distance between node and its children
-        levelsToShow: 4,
-        // offsetY: 170,
-        // orientation: 'top',
-        offsetX: 350,
-
-        // enable panning
-        Navigation: {
-            enable: true,
-            panning: true
-        },
-
-        // set node and edge styles
-        Node: {
-            autoHeight: true,
-            // autoWidth: true,
-            width: 100,
-            // also change the CSS .node property if you change this!
-            type: 'rectangle',
-            color: '#DCDFE4',
-            overridable: true,
-            align: 'center'
-        },
-
-        Label: {
-            type: 'Native'
-        },
-
-        Edge: {
-            type: 'bezier',
-            color: '#DCDFE4',
-            overridable: true
-        },
-
-        Events: {
-            enable: true,
-            onClick: function (node) {
-                console.log(node);
-                /*if (node) {
-                    logToGoogle("Multi Peptide", "Zoom", "Treemap", "In");
-                    tm.enter(node);
-                    treeSearch(node.name, 500);
-                }*/
-            }
-        },
-
-        // This method is called on DOM label creation.
-        // Use this method to add event handlers and styles to
-        // your node.
-        /*onCreateLabel: function (label, node) {
-            console.log(node);
-            label.id = node.id;
-            label.innerHTML = node.name;
-            label.onclick = function () {
-                st.onClick(node.id);
-                // st.setRoot(node.id, 'animate');
-            };
-            // set label styles => TODO: fix the labels with these settings instead of CSS
-            var style = label.style;
-            style.width = '60px';
-            style.height = '17px';
-            style.cursor = 'pointer';
-            style.color = '#333';
-            style.fontSize = '0.8em';
-            style.textAlign = 'center';
-            style.paddingTop = '3px';
-        },*/
-
-        // This method is called right before plotting
-        // a node. It's useful for changing an individual node
-        // style properties before plotting it.
-        // The data properties prefixed with a dollar
-        // sign will override the global node style properties.
-        onBeforePlotNode: function (node) {
-            // add some color to the nodes in the path between the
-            // root node and the selected node.
-            if (node.selected) {
-                node.data.$color = "#bbb";
-            }
-            else {
-                delete node.data.$color;
-                // if the node belongs to the last plotted level
-                /*if(!node.anySubnode("exist")) {
-                    // count children number
-                    var count = 0;
-                    node.eachSubnode(function(n) { count++; });
-                    // assign a node color based on
-                    // how many children it has
-                    node.data.$color = ['#aaa', '#baa', '#caa', '#daa', '#eaa', '#faa'][count];
-                }*/
-            }
-        }
-
-        // This method is called right before plotting
-        // an edge. It's useful for changing an individual edge
-        // style properties before plotting it.
-        // Edge data proprties prefixed with a dollar sign will
-        // override the Edge global style properties.
-        /*onBeforePlotLine: function (adj) {
-            if (adj.nodeFrom.selected && adj.nodeTo.selected) {
-                adj.data.$color = "#eed";
-                adj.data.$lineWidth = 3;
-            } else {
-                delete adj.data.$color;
-                delete adj.data.$lineWidth;
-            }
-        }*/
-    });
-    // load json data
-    st.loadJSON(jsonData);
-
-    // compute node positions and layout
-    st.compute();
-
-    // optional: make a translation of the tree
-    st.geom.translate(new $jit.Complex(-200, 0), "current");
-
-    st.onClick(st.root);
-}
-
 /**
  * Zoomable treeview based on
  * - http://bl.ocks.org/mbostock/4339083
  * - https://gist.github.com/robschmuecker/7880033
  */
-function initTreeView2(jsonData) {
+function initTreeView(jsonData) {
     var margin = {top: 5, right: 5, bottom: 5, left: 60},
         width = 916 - margin.right - margin.left,
         height = 600 - margin.top - margin.bottom;
