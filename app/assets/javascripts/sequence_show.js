fd30abf0fe9d590817078f4aed9e5862f47e24f4
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-08-08 14:12:30 +0200

    remove the old tree

--- app/assets/javascripts/sequence_show.js
+++ app/assets/javascripts/sequence_show.js
@@ -1,10 +1,7 @@
 function init_sequence_show(data, lcaId) {
 
-    // set up the tree
-    var st = setUpTree(data);
-
     // set up the fancy tree
-    initFancyTree(data);
+    initLineageTree(data);
 
     // set up the fullscreen stuff
     setUpFullScreen();
@@ -99,139 +96,9 @@ function init_sequence_show(data, lcaId) {
     }
 
     /**
-     * Draws the lineage tree
+     * Inits the lineage tree
      */
-    function setUpTree(data) {
-        // Create a new SpaceTree instance
-        st = new $jit.ST({
-            injectInto: 'lineageTree',
-            // id of viz container element
-            duration: 800,
-            // set duration for the animation
-            transition: $jit.Trans.Quart.easeInOut,
-            // set animation transition type
-            levelDistance: 50,
-            // set distance between node and its children
-            levelsToShow: 4,
-            // offsetY: 170,
-            // orientation: 'top',
-            offsetX: 350,
-
-            // enable panning
-            Navigation: {
-                enable: true,
-                panning: true
-            },
-
-            // set node and edge styles
-            Node: {
-                autoHeight: true,
-                // autoWidth: true,
-                width: 100,
-                // also change the CSS .node property if you change this!
-                type: 'rectangle',
-                color: '#DCDFE4',
-                overridable: true,
-                align: 'center'
-            },
-
-            Edge: {
-                type: 'bezier',
-                color: '#DCDFE4',
-                overridable: true
-            },
-
-            // This method is called on DOM label creation.
-            // Use this method to add event handlers and styles to
-            // your node.
-            onCreateLabel: function (label, node) {
-                label.id = node.id;
-                label.innerHTML = node.name;
-                label.onclick = function () {
-                    st.onClick(node.id);
-                    // st.setRoot(node.id, 'animate');
-                };
-                // set label styles => TODO: fix the labels with these settings instead of CSS
-                var style = label.style;
-                style.width = '60px';
-                style.height = '17px';
-                style.cursor = 'pointer';
-                style.color = '#333';
-                style.fontSize = '0.8em';
-                style.textAlign = 'center';
-                style.paddingTop = '3px';
-            },
-
-            // This method is called right before plotting
-            // a node. It's useful for changing an individual node
-            // style properties before plotting it.
-            // The data properties prefixed with a dollar
-            // sign will override the global node style properties.
-            onBeforePlotNode: function (node) {
-                // add some color to the nodes in the path between the
-                // root node and the selected node.
-                if (node.selected) {
-                    node.data.$color = "#bbb";
-                }
-                else {
-                    delete node.data.$color;
-                    // if the node belongs to the last plotted level
-                    /*if(!node.anySubnode("exist")) {
-                        // count children number
-                        var count = 0;
-                        node.eachSubnode(function(n) { count++; });
-                        // assign a node color based on
-                        // how many children it has
-                        node.data.$color = ['#aaa', '#baa', '#caa', '#daa', '#eaa', '#faa'][count];
-                    }*/
-                }
-            }
-
-            // This method is called right before plotting
-            // an edge. It's useful for changing an individual edge
-            // style properties before plotting it.
-            // Edge data proprties prefixed with a dollar sign will
-            // override the Edge global style properties.
-            /*onBeforePlotLine: function (adj) {
-                if (adj.nodeFrom.selected && adj.nodeTo.selected) {
-                    adj.data.$color = "#eed";
-                    adj.data.$lineWidth = 3;
-                } else {
-                    delete adj.data.$color;
-                    delete adj.data.$lineWidth;
-                }
-            }*/
-        });
-        // load json data
-        st.loadJSON(data);
-
-        // compute node positions and layout
-        st.compute();
-
-        // optional: make a translation of the tree
-        st.geom.translate(new $jit.Complex(-200, 0), "current");
-
-        // emulate a click on the root node.
-        try {
-            if (lcaId == 1) {
-                lcaId = data.id;
-            }
-            st.onClick(lcaId);
-        }
-        catch (err) {
-            error(err.message, "Something went wrong while loading the lineage tree.");
-        }
-
-        // disable the text selection of tree nodes
-        $("#lineageTree").disableSelection();
-
-        return st;
-    }
-
-    /**
-     * Inits the fancy tree
-     */
-    function initFancyTree(jsonData) {
+    function initLineageTree(jsonData) {
         var margin = {top: 5, right: 5, bottom: 5, left: 60},
             width = 916 - margin.right - margin.left,
             height = 600 - margin.top - margin.bottom;
@@ -257,7 +124,7 @@ function init_sequence_show(data, lcaId) {
             .scaleExtent([0.1, 3])
             .on("zoom", zoom);
 
-        var svg = d3.select("#fancyTree").append("svg")
+        var svg = d3.select("#lineageTree").append("svg")
             .attr("version", "1.1")
             .attr("xmlns", "http://www.w3.org/2000/svg")
             .attr("viewBox", "0 0 " + (width + margin.right + margin.left) + " " + (height + margin.top + margin.bottom))
