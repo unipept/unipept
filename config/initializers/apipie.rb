c5eec431ce6305f362962388d476ca8d7b32f32b
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-08-15 20:06:35 +0200

    apply unipept layout

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -6,4 +6,5 @@ Apipie.configure do |config|
   config.validate                = false
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/*.rb"
+  config.layout                  = "application"
 end
