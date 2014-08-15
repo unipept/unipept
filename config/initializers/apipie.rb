b61c53da584d3068ee183cfc8acde7a7f3ef6d6c
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-08-15 20:19:41 +0200

    add a few extra settings

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -7,4 +7,7 @@ Apipie.configure do |config|
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/*.rb"
   config.layout                  = "application"
+  config.default_version         = "v1"
+  config.app_info                = "This page gives an overview of the Unipept API."
+  config.markup                  = Apipie::Markup::Markdown.new
 end
