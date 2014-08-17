821d81887347124e103300954b02658d7a18886e
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-08-17 16:35:29 +0200

    consistent summaries

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -58,107 +58,7 @@ class Api::ApiController < ApplicationController
   end
 
 
-  api :POST, '/v1/pept2taxa', "Returns all the taxa in which input peptides are present"
-  param :input, Array, required: true, description: "List of input peptides"
-  param :equate_il, ['true','false'], description: "Indicate if you want to equate I and L"
-  param :extra, ['true','false'], description: "Request extra information such as the entire lineage"
-  param :names, ['true','false'], description: "Include the lineage names"
-
-  description <<-EOS
-  The api is strictly on a per peptide basis, there is no aggregation. However due to speed concerns there is a possibility to pass
-  multiple peptides.  It will lookup all the proteins in which a given input peptide is present, of these proteins it will find all
-  the organisms and return its taxon_id, taxon_name, ...
-  EOS
-  example <<-EOS
-  $ curl -X POST -H "Accept: application/json" scruffy.ugent.be/api/v1/pept2taxa -d 'input[0]=SASLGR'
-  [{"sequence":"SASLGR","taxon_id":3055,"taxon_name":"Chlamydomonas reinhardtii","taxon_parent_id":3052,"taxon_rank":"species"},...]
-  EOS
-  def pept2taxa
-    @result = {}
-    lookup = Hash.new { |h,k| h[k] = Set.new }
-    ids = []
-    @sequences.pluck(:sequence, :taxon_id).each do |sequence,taxon_id|
-      lookup[taxon_id] << sequence
-      ids.append(taxon_id)
-      @result[sequence] = Set.new
-    end
-
-    ids = ids.uniq.reject(&:nil?).sort
-
-    @query.where(id: ids).find_in_batches do |group|
-      group.each do |t|
-        lookup[t.id].each {|s| @result[s] << t}
-      end
-    end
-
-    filter_input_order
-
-    respond_with(@result)
-  end
-
-  api :POST, '/v1/pept2lca', "Returns the lowest common ancestor per input peptide"
-  param :input, Array, required: true, description: "List of input peptides"
-  param :equate_il, ['true','false'], description: "Indicate if you want to equate I and L"
-  param :extra, ['true','false'], description: "Request extra information such as the entire lineage"
-  param :names, ['true','false'], description: "Include the lineage names"
-  description <<-EOS
-  For each input peptide, return the lowest common ancestor as found in the unipept database.
-  EOS
-  example <<-EOS
-  $ curl -X POST -H "Accept: application/json" scruffy.ugent.be/api/v1/pept2lca -d 'input[0]=ASFHLECIK' -d 'equate_il=false'
-  []
-
-  $ curl -X POST -H "Accept: application/json" scruffy.ugent.be/api/v1/pept2lca -d 'input[0]=ASFHLECIK' -d 'equate_il=true'
-  [{"sequence":"ASFHLECIK","taxon_id":7776,"taxon_name":"Gnathostomata","taxon_parent_id":7742,"taxon_rank":"superclass"}]
-  EOS
-  def pept2lca
-    @result = {}
-    lookup = Hash.new { |h,k| h[k] = Set.new }
-    ids = []
-    @sequences = Sequence.where(sequence: @input)
-    lca_field = @equate_il ? :lca_il : :lca
-    @sequences.pluck(:sequence, lca_field).each do |sequence, lca_il|
-      ids.append lca_il
-      lookup[lca_il] << sequence
-    end
-
-    ids = ids.uniq.reject(&:nil?).sort
-
-    @query.where(id: ids).find_in_batches do |group|
-      group.each do |t|
-        lookup[t.id].each { |s| @result[s] = t }
-      end
-    end
-
-    filter_input_order
-
-    respond_with(@result)
-  end
-
-  api :POST, '/v1/taxa2lca', "calculates the lowest common ancestor of given taxon ids"
-  param :input, Array, required: true, description: "List of input taxon ids"
-  param :extra, ['true','false'], description: "Request extra information such as the entire lineage"
-  param :names, ['true','false'], description: "Include the lineage names"
-  description <<-EOS
-  This will actually calculate the lowest common ancestor of all the lineages of all the taxon ids passed via the input paramater.
-  EOS
-  example <<-EOS
-  $ curl -X POST -H "Accept: application/json" scruffy.ugent.be/api/v1/taxa2lca -d 'input[0]=816' -d 'input[1]=124'
-  {"taxon_id":2,"taxon_name":"Bacteria","taxon_parent_id":131567,"taxon_rank":"superkingdom"}
-  EOS
-  def taxa2lca
-    # handle case where 1 is provided
-    if @input.include? "1"
-      @result = Taxon.find(1)
-    else
-      lineages = Lineage.includes(Lineage::ORDER_T).where(taxon_id: @input)
-      @result = Lineage.calculate_lca_taxon(lineages)
-    end
-
-    respond_with(@result)
-  end
-
-  api :POST, '/v1/pept2prot', "Returns uniprot ids for given input peptides"
+  api :POST, '/v1/pept2prot', "Returns a list of Uniprot entries containing a given tryptic peptide"
   param :input, Array, required: true, description: "List of input peptides"
   param :equate_il, ['true','false'], description: "Indicate if you want to equate I and L"
   param :extra, ['true','false'], description: "Request extra information such as the entire lineage"
@@ -199,6 +99,106 @@ class Api::ApiController < ApplicationController
     end
 
     filter_input_order
+    respond_with(@result)
+  end
+
+  api :POST, '/v1/pept2taxa', "Returns a list of taxa retrieved from the Uniprot entries containing a given tryptic peptide"
+  param :input, Array, required: true, description: "List of input peptides"
+  param :equate_il, ['true','false'], description: "Indicate if you want to equate I and L"
+  param :extra, ['true','false'], description: "Request extra information such as the entire lineage"
+  param :names, ['true','false'], description: "Include the lineage names"
+
+  description <<-EOS
+  The api is strictly on a per peptide basis, there is no aggregation. However due to speed concerns there is a possibility to pass
+  multiple peptides.  It will lookup all the proteins in which a given input peptide is present, of these proteins it will find all
+  the organisms and return its taxon_id, taxon_name, ...
+  EOS
+  example <<-EOS
+  $ curl -X POST -H "Accept: application/json" scruffy.ugent.be/api/v1/pept2taxa -d 'input[0]=SASLGR'
+  [{"sequence":"SASLGR","taxon_id":3055,"taxon_name":"Chlamydomonas reinhardtii","taxon_parent_id":3052,"taxon_rank":"species"},...]
+  EOS
+  def pept2taxa
+    @result = {}
+    lookup = Hash.new { |h,k| h[k] = Set.new }
+    ids = []
+    @sequences.pluck(:sequence, :taxon_id).each do |sequence,taxon_id|
+      lookup[taxon_id] << sequence
+      ids.append(taxon_id)
+      @result[sequence] = Set.new
+    end
+
+    ids = ids.uniq.reject(&:nil?).sort
+
+    @query.where(id: ids).find_in_batches do |group|
+      group.each do |t|
+        lookup[t.id].each {|s| @result[s] << t}
+      end
+    end
+
+    filter_input_order
+
+    respond_with(@result)
+  end
+
+  api :POST, '/v1/pept2lca', "Returns the taxonomic lowest common ancestor for a given tryptic peptide"
+  param :input, Array, required: true, description: "List of input peptides"
+  param :equate_il, ['true','false'], description: "Indicate if you want to equate I and L"
+  param :extra, ['true','false'], description: "Request extra information such as the entire lineage"
+  param :names, ['true','false'], description: "Include the lineage names"
+  description <<-EOS
+  For each input peptide, return the lowest common ancestor as found in the unipept database.
+  EOS
+  example <<-EOS
+  $ curl -X POST -H "Accept: application/json" scruffy.ugent.be/api/v1/pept2lca -d 'input[0]=ASFHLECIK' -d 'equate_il=false'
+  []
+
+  $ curl -X POST -H "Accept: application/json" scruffy.ugent.be/api/v1/pept2lca -d 'input[0]=ASFHLECIK' -d 'equate_il=true'
+  [{"sequence":"ASFHLECIK","taxon_id":7776,"taxon_name":"Gnathostomata","taxon_parent_id":7742,"taxon_rank":"superclass"}]
+  EOS
+  def pept2lca
+    @result = {}
+    lookup = Hash.new { |h,k| h[k] = Set.new }
+    ids = []
+    @sequences = Sequence.where(sequence: @input)
+    lca_field = @equate_il ? :lca_il : :lca
+    @sequences.pluck(:sequence, lca_field).each do |sequence, lca_il|
+      ids.append lca_il
+      lookup[lca_il] << sequence
+    end
+
+    ids = ids.uniq.reject(&:nil?).sort
+
+    @query.where(id: ids).find_in_batches do |group|
+      group.each do |t|
+        lookup[t.id].each { |s| @result[s] = t }
+      end
+    end
+
+    filter_input_order
+
+    respond_with(@result)
+  end
+
+  api :POST, '/v1/taxa2lca', "Returns the lowest common ancestor for a given list of taxon id's"
+  param :input, Array, required: true, description: "List of input taxon ids"
+  param :extra, ['true','false'], description: "Request extra information such as the entire lineage"
+  param :names, ['true','false'], description: "Include the lineage names"
+  description <<-EOS
+  This will actually calculate the lowest common ancestor of all the lineages of all the taxon ids passed via the input paramater.
+  EOS
+  example <<-EOS
+  $ curl -X POST -H "Accept: application/json" scruffy.ugent.be/api/v1/taxa2lca -d 'input[0]=816' -d 'input[1]=124'
+  {"taxon_id":2,"taxon_name":"Bacteria","taxon_parent_id":131567,"taxon_rank":"superkingdom"}
+  EOS
+  def taxa2lca
+    # handle case where 1 is provided
+    if @input.include? "1"
+      @result = Taxon.find(1)
+    else
+      lineages = Lineage.includes(Lineage::ORDER_T).where(taxon_id: @input)
+      @result = Lineage.calculate_lca_taxon(lineages)
+    end
+
     respond_with(@result)
   end
 
