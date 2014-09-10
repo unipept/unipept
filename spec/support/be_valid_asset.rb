a1e8573a7d8b04942a869e592c5cda72ac1eb863
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-09-10 15:47:49 +0200

    remove spec dir

--- spec/support/be_valid_asset.rb
+++
@@ -1,5 +0,0 @@
-include BeValidAsset
-
-# BeValidAsset::Configuration.display_invalid_content = true
-BeValidAsset::Configuration.enable_caching = true
-BeValidAsset::Configuration.cache_path = File.join(RAILS_ROOT, %w(tmp be_valid_asset_cache))
