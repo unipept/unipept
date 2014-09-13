916db47a3ca6ab665ff52f0ca05a76ea3a553743
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-09-13 14:33:40 +0200

    add tests for embl_cross_references model

--- app/models/embl_cross_reference.rb
+++ app/models/embl_cross_reference.rb
@@ -9,15 +9,16 @@
 #
 
 class EmblCrossReference < ActiveRecord::Base
+  include ReadOnlyModel
   attr_accessible nil
 
   belongs_to :uniprot_entry
 
   # Returns a set af sequence_ids for a given refseq_id
   def self.get_sequence_ids(insdc_id)
-    return connection.select_values("SELECT original_sequence_id 
-      FROM peptides 
-      LEFT JOIN  embl_cross_references ON peptides.uniprot_entry_id = embl_cross_references.uniprot_entry_id 
+    return connection.select_values("SELECT original_sequence_id
+      FROM peptides
+      LEFT JOIN  embl_cross_references ON peptides.uniprot_entry_id = embl_cross_references.uniprot_entry_id
       WHERE embl_cross_references.sequence_id = '#{insdc_id}'").to_set
   end
 end
