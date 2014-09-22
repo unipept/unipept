69862010b1ec49e4533fcc3fda6d72c199e791f0
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-09-22 13:43:21 +0200

    add function tests for posts controller

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,3 +19,9 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionController::TestCase
+
+  # Devise helper
+  include Devise::TestHelpers
+end
