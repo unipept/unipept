5a8a3c71c0c70082392f4826a59164c776982eea
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-09-23 15:36:37 +0200

    complete function tests for peptidome controller

--- test/controllers/peptidome_controller_test.rb
+++ test/controllers/peptidome_controller_test.rb
@@ -29,4 +29,55 @@ class PeptidomeControllerTest < ActionController::TestCase
     assert_equal "peptidomeclustering", assigns(:tab)
   end
 
+  test "should get get_sequence_ids_for_bioproject" do
+    genome = genome_caches(:genomecache1)
+    get :get_sequence_ids_for_bioproject, bioproject_id: "1", format: "json"
+    assert_response :success
+    assert_equal genome.json_sequences, @response.body
+  end
+
+  test "should get get_genomes" do
+    get :get_genomes, species_id: "1", format: "json"
+    assert_response :success
+    assert @response.body.start_with? "[{"
+    assert @response.body.end_with? "}]"
+  end
+
+  test "should get get_unique_sequences" do
+    get :get_unique_sequences, sequences: "[1, 2, 3]", bioprojects: "1", format: "json"
+    assert_response :success
+    assert_equal '["species1",[3]]', @response.body
+  end
+
+  test "should return undefined for get_unique_sequences without bioprojects" do
+    get :get_unique_sequences, sequences: "[1, 2, 3]", bioprojects: "", format: "json"
+    assert_response :success
+    assert_equal '["undefined",[]]', @response.body
+  end
+
+  test "should get get_sequences" do
+    get :get_sequences, sequence_ids: "[1, 2]"
+    assert_response :success
+    assert_equal '"AAIER\nAALER"', @response.body
+  end
+
+  test "should get convert_peptides" do
+    get :convert_peptides, peptides: '["AALER", "AAIER", "FOO"]'
+    assert_response :success
+    assert_equal "[2,1]", @response.body
+  end
+
+  test "should get get_lca" do
+    species = taxons(:taxon1)
+    get :get_lca, bioprojects: "1"
+    assert_response :success
+    assert_equal species.to_json, @response.body
+  end
+
+  test "should return undefined without bioprojects" do
+    get :get_lca, bioprojects: ""
+    assert_response :success
+    assert_equal '{"name":"undefined"}', @response.body
+  end
+
 end
