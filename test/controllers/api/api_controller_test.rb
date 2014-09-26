d07294cd5ccda08d21a65d949727ffad03888e7a
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-09-26 17:10:50 +0200

    add all functional tests for api controller

--- test/controllers/api/api_controller_test.rb
+++ test/controllers/api/api_controller_test.rb
@@ -40,11 +40,349 @@ class Api::ApiControllerTest < ActionController::TestCase
     skip
   end
 
+
   test "should get pept2prot" do
-    skip
-    get :pept2prot
-    assert_resonse :success
+    get :pept2prot, input: ["AAIER", "AAILER"], format: "json"
+    assert_response :success
     assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "uniprot_id"
+    assert @response.body.include? "taxon_id"
+    assert_not @response.body.include? "taxon_name"
+    assert_not @response.body.include? '"uniprot_id":"nr2"'
+  end
+
+  test "should get pept2prot with il" do
+    get :pept2prot, input: ["AAIER", "AAILER"], format: "json", equate_il: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "uniprot_id"
+    assert @response.body.include? "taxon_id"
+    assert_not @response.body.include? "taxon_name"
+    assert @response.body.include? '"uniprot_id":"nr2"'
+  end
+
+  test "should get pept2prot with extra" do
+    get :pept2prot, input: ["AAIER", "AAILER"], format: "json", extra: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "uniprot_id"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "ec_references"
+    assert @response.body.include? "go_references"
+    assert_not @response.body.include? '"uniprot_id":"nr2"'
+  end
+
+  test "should get pept2prot with extra and il" do
+    get :pept2prot, input: ["AAIER", "AAILER"], format: "json", equate_il: "true", extra: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "uniprot_id"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "ec_references"
+    assert @response.body.include? "go_references"
+    assert @response.body.include? '"uniprot_id":"nr2"'
+  end
+
+
+  test "should get pept2taxa" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert_not @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+    assert_not @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+  test "should get pept2taxa with il" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json", equate_il: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert_not @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+    assert @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+  test "should get pept2taxa with extra" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json", extra: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+    assert_not @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+  test "should get pept2taxa with names" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json", names: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert_not @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+    assert_not @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+  test "should get pept2taxa with extra and names" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json", extra: "true", names: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert @response.body.include? "kingdom_id"
+    assert @response.body.include? "kingdom_name"
+    assert_not @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+  test "should get pept2taxa with extra and names and il" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json", equate_il: "true", extra: "true", names: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert @response.body.include? "kingdom_id"
+    assert @response.body.include? "kingdom_name"
+    assert @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+
+  test "should get pept2lca" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert_not @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+    assert_not @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+  test "should get pept2lca with il" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json", equate_il: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert_not @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+    assert @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+  test "should get pept2lca with extra" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json", extra: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+    assert_not @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+  test "should get pept2lca with names" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json", names: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert_not @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+    assert_not @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+  test "should get pept2lca with extra and names" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json", extra: "true", names: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert @response.body.include? "kingdom_id"
+    assert @response.body.include? "kingdom_name"
+    assert_not @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+  test "should get pept2lca with extra and names and il" do
+    get :pept2taxa, input: ["AAIER", "AAILER"], format: "json", equate_il: "true", extra: "true", names: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "AAIER"
+    assert @response.body.include? "AAILER"
+    assert_not @response.body.include? "AALLER"
+    assert_not @response.body.include? "AALER"
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert @response.body.include? "kingdom_id"
+    assert @response.body.include? "kingdom_name"
+    assert @response.body.include? 'AAIER","taxon_id":2'
+  end
+
+
+  test "should get taxa2lca" do
+    get :taxa2lca, input: ["1", "2"], format: "json"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert_not @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+  end
+
+  test "should get taxa2lca with extra" do
+    get :taxa2lca, input: ["1", "2"], format: "json", extra: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+  end
+
+  test "should get taxa2lca with names" do
+    get :taxa2lca, input: ["1", "2"], format: "json", names: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert_not @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+  end
+
+  test "should get taxa2lca with extra and names" do
+    get :taxa2lca, input: ["1", "2"], format: "json", names: "true", extra: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert @response.body.include? "kingdom_id"
+    assert @response.body.include? "kingdom_name"
+  end
+
+
+  test "should get taxonomy" do
+    get :taxonomy, input: ["1", "2"], format: "json"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert_not @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+  end
+
+  test "should get taxonomy with extra" do
+    get :taxonomy, input: ["1", "2"], format: "json", extra: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+  end
+
+  test "should get taxonomy with names" do
+    get :taxonomy, input: ["1", "2"], format: "json", names: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert_not @response.body.include? "kingdom_id"
+    assert_not @response.body.include? "kingdom_name"
+  end
+
+  test "should get taxonomy with extra and names" do
+    get :taxonomy, input: ["1", "2"], format: "json", names: "true", extra: "true"
+    assert_response :success
+    assert_equal "*", @response.headers["Access-Control-Allow-Origin"]
+    assert @response.body.include? "taxon_id"
+    assert @response.body.include? "taxon_name"
+    assert @response.body.include? "taxon_rank"
+    assert @response.body.include? "kingdom_id"
+    assert @response.body.include? "kingdom_name"
   end
 
 end
