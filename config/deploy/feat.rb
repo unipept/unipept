21cb1196018f14f158d4c7b79f49fbf2c9934a8d
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-10-20 14:16:16 +0200

    update deploy branch

--- config/deploy/feat.rb
+++ config/deploy/feat.rb
@@ -5,5 +5,5 @@ server "scruffy.ugent.be", user: 'bmesuere', roles: [:web, :app], ssh_options: {
   port: 4840,
 }
 
-set :branch, 'develop'
+set :branch, 'feature/d3-treemap'
 set :rails_env, :development
