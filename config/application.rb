162b3a40393461609586cb311d50e2f76418fc54
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-11-11 12:37:44 +0100

    bump version

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,7 @@ module UnipeptWeb
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
     config.versions = {
-      :unipept => "2.4",
+      :unipept => "2.5 beta",
       :gem => "0.5.7",
       :uniprot => "2014.05"
     }
