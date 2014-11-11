01e042cb9b974e35f32cff587fd1a1552c612d2e
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-11-11 15:54:40 +0100

    use condensed font for linux in navigation. Closes #430

--- config/deploy/feat.rb
+++ config/deploy/feat.rb
@@ -5,5 +5,5 @@ server "scruffy.ugent.be", user: 'bmesuere', roles: [:web, :app], ssh_options: {
   port: 4840,
 }
 
-set :branch, 'feature/d3-treemap'
+set :branch, 'develop'
 set :rails_env, :development
