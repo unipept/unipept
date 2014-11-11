366d4c88cca45c6035e11d6f5971a31cadbad17e
Bart Mesuere <Bart.Mesuere@UGent.be>
2014-11-11 17:49:08 +0100

    jshint application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,7 @@ function addCopy(selector, textFunction) {
     });
     // Copy to clipboard
     copyMissed.on('copy', function (client) {
-      copyMissed.setText(textFunction.call());
+        copyMissed.setText(textFunction.call());
     });
     // Notify copy success and reset tooltip title
     copyMissed.on('aftercopy', function () {
@@ -167,7 +167,7 @@ function downloadDataByForm(data, fileName, callback) {
     $downloadForm.submit();
     if (callback) {
         downloadTimer = setInterval(function checkCookie() {
-            if (document.cookie.indexOf(nonce) != -1) {
+            if (document.cookie.indexOf(nonce) !== -1) {
                 callback();
                 clearInterval(downloadTimer);
             }
@@ -200,7 +200,7 @@ function downloadDataByLink(dataURL, fileName) {
 function error(errorMessage, userMessage) {
     if (errorMessage !== null) {
         logErrorToGoogle(errorMessage);
-        if (typeof console != "undefined") {
+        if (typeof console !== "undefined") {
             console.error(errorMessage);
         }
     }
@@ -257,8 +257,11 @@ var delay = (function () {
  * Hash function for strings from
  * http://stackoverflow.com/a/15710692/865696
  */
-function stringHash(s){
-  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
+function stringHash(s) {
+    return s.split("").reduce(function (a, b) {
+        a = ((a << 5) - a) + b.charCodeAt(0);
+        return a & a;
+    }, 0);
 }
 
 /**
@@ -268,32 +271,32 @@ function stringHash(s){
  */
 (function () {
     if (!Array.prototype.map) {
-      Array.prototype.map = function(callback, thisArg) {
-        var T, A, k;
-        if (this == null) {
-          throw new TypeError(' this is null or not defined');
-        }
-        var O = Object(this);
-        var len = O.length >>> 0;
-        if (typeof callback !== 'function') {
-          throw new TypeError(callback + ' is not a function');
-        }
-        if (arguments.length > 1) {
-          T = thisArg;
-        }
-        A = new Array(len);
-        k = 0;
-        while (k < len) {
-          var kValue, mappedValue;
-          if (k in O) {
-            kValue = O[k];
-            mappedValue = callback.call(T, kValue, k, O);
-            A[k] = mappedValue;
-          }
-          k++;
-        }
-        return A;
-      };
+        Array.prototype.map = function (callback, thisArg) {
+            var T, A, k;
+            if (this == null) {
+                throw new TypeError(' this is null or not defined');
+            }
+            var O = Object(this);
+            var len = O.length >>> 0;
+            if (typeof callback !== 'function') {
+                throw new TypeError(callback + ' is not a function');
+            }
+            if (arguments.length > 1) {
+                T = thisArg;
+            }
+            A = new Array(len);
+            k = 0;
+            while (k < len) {
+                var kValue, mappedValue;
+                if (k in O) {
+                    kValue = O[k];
+                    mappedValue = callback.call(T, kValue, k, O);
+                    A[k] = mappedValue;
+                }
+                k++;
+            }
+            return A;
+        };
     }
 })();
 
@@ -303,28 +306,30 @@ function stringHash(s){
  * Only needed for IE8
  */
 (function () {
-    if ( 'function' !== typeof Array.prototype.reduce ) {
-        Array.prototype.reduce = function( callback /*, initialValue*/ ) {
+    if ('function' !== typeof Array.prototype.reduce) {
+        Array.prototype.reduce = function (callback /*, initialValue*/) {
             'use strict';
-            if ( null === this || 'undefined' === typeof this ) {
-                throw new TypeError('Array.prototype.reduce called on null or undefined' );
+            if (null === this || 'undefined' === typeof this) {
+                throw new TypeError('Array.prototype.reduce called on null or undefined');
             }
-            if ( 'function' !== typeof callback ) {
-                throw new TypeError( callback + ' is not a function' );
+            if ('function' !== typeof callback) {
+                throw new TypeError(callback + ' is not a function');
             }
-            var t = Object( this ), len = t.length >>> 0, k = 0, value;
-            if ( arguments.length >= 2 ) {
+            var t = Object(this), len = t.length >>> 0, k = 0, value;
+            if (arguments.length >= 2) {
                 value = arguments[1];
             } else {
-                while ( k < len && ! k in t ) k++;
-                if ( k >= len ) {
+                while (k < len && !(k in t)) {
+                    k++;
+                }
+                if (k >= len) {
                     throw new TypeError('Reduce of empty array with no initial value');
                 }
-                value = t[ k++ ];
+                value = t[k++];
             }
-            for ( ; k < len ; k++ ) {
-                if ( k in t ) {
-                    value = callback( value, t[k], k, t );
+            for (; k < len ; k++) {
+                if (k in t) {
+                    value = callback(value, t[k], k, t);
                 }
             }
             return value;
@@ -348,21 +353,22 @@ function stringHash(s){
             prefix: ''
         },
         browserPrefixes = 'webkit moz o ms khtml'.split(' '),
-        i;
+        i,
+        il;
 
     // check for native support
-    if (typeof document.exitFullScreen != 'undefined') {
+    if (typeof document.exitFullScreen !== 'undefined') {
         fullScreenApi.supportsFullScreen = true;
     } else {
         // check for fullscreen support by vendor prefix
         for (i = 0, il = browserPrefixes.length; i < il; i++) {
             fullScreenApi.prefix = browserPrefixes[i];
 
-            if (typeof document[fullScreenApi.prefix + 'CancelFullScreen'] != 'undefined') {
+            if (typeof document[fullScreenApi.prefix + 'CancelFullScreen'] !== 'undefined') {
                 fullScreenApi.supportsFullScreen = true;
                 break;
             }
-            if (typeof document[fullScreenApi.prefix + 'ExitFullscreen'] != 'undefined') {
+            if (typeof document[fullScreenApi.prefix + 'ExitFullscreen'] !== 'undefined') {
                 fullScreenApi.supportsFullScreen = true;
                 break;
             }
@@ -378,42 +384,42 @@ function stringHash(s){
 
         fullScreenApi.isFullScreen = function () {
             switch (this.prefix) {
-                case '':
-                    return document.fullscreenElement !== null;
-                case 'moz':
-                    return document.mozFullScreenElement !== null;
-                default:
-                    return document[this.prefix + 'FullscreenElement'] !== null;
+            case '':
+                return document.fullscreenElement !== null;
+            case 'moz':
+                return document.mozFullScreenElement !== null;
+            default:
+                return document[this.prefix + 'FullscreenElement'] !== null;
             }
         };
         fullScreenApi.requestFullScreen = function (el) {
             switch (this.prefix) {
-                case '':
-                    return el.requestFullscreen();
-                case 'webkit':
-                    return el.webkitRequestFullscreen();
-                case 'ms':
-                    return el.msRequestFullscreen();
-                case 'moz':
-                    return el.mozRequestFullScreen();
-                case 'default':
-                    return el[this.prefix + "RequestFullscreen"]();
+            case '':
+                return el.requestFullscreen();
+            case 'webkit':
+                return el.webkitRequestFullscreen();
+            case 'ms':
+                return el.msRequestFullscreen();
+            case 'moz':
+                return el.mozRequestFullScreen();
+            case 'default':
+                return el[this.prefix + "RequestFullscreen"]();
             }
         };
         fullScreenApi.cancelFullScreen = function (el) {
             switch (this.prefix) {
-                case '':
-                    return document.exitFullscreen();
-                case 'moz':
-                    return document.mozCancelFullScreen();
-                case 'default':
-                    return document[this.prefix + "ExitFullscreen"]();
+            case '':
+                return document.exitFullscreen();
+            case 'moz':
+                return document.mozCancelFullScreen();
+            case 'default':
+                return document[this.prefix + "ExitFullscreen"]();
             }
         };
     }
 
     // jQuery plugin
-    if (typeof jQuery != 'undefined') {
+    if (typeof jQuery !== 'undefined') {
         jQuery.fn.requestFullScreen = function () {
             return this.each(function () {
                 if (fullScreenApi.supportsFullScreen) {
@@ -431,11 +437,11 @@ function stringHash(s){
  * requestAnimationFrame shim
  * source: http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
  */
-window.requestAnimFrame = (function(){
+window.requestAnimFrame = (function () {
     return  window.requestAnimationFrame       ||
             window.webkitRequestAnimationFrame ||
             window.mozRequestAnimationFrame    ||
-            function( callback ){
+            function (callback) {
                 window.setTimeout(callback, 1000 / 60);
             };
 })();
@@ -444,9 +450,9 @@ window.requestAnimFrame = (function(){
  * Catches all errors, displays them in console and
  * logs them to Google Analytics
  */
-window.onerror = function(message, file, line) {
+window.onerror = function (message, file, line) {
     var e = file + '(' + line + '): ' + message;
-    if (typeof console != "undefined") {
+    if (typeof console !== "undefined") {
         console.error(e);
     }
     logErrorToGoogle(e);
