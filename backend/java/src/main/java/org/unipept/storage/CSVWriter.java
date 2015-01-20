82282d7f8d1004dd5a8a5dcdd1875254135c471a
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2015-01-20 12:12:03 +0100

    no tsv headers + \N for null (#451)

--- backend/java/src/main/java/org/unipept/storage/CSVWriter.java
+++ backend/java/src/main/java/org/unipept/storage/CSVWriter.java
@@ -9,19 +9,14 @@ public class CSVWriter {
     private BufferedWriter buffer;
     private int index = 0;
 
-    public CSVWriter(String file, String... titles) throws IOException {
+    public CSVWriter(String file) throws IOException {
         buffer = new BufferedWriter(new FileWriter(file));
-        buffer.write("id");
-        for(int i = 0; i < titles.length; i++) {
-            buffer.write("	" + titles[i].toString());
-        }
-        buffer.newLine();
     }
 
     public int write(String... values) throws IOException {
         buffer.write(Integer.toString(++index));
         for(int i = 0; i < values.length; i++) {
-            buffer.write("	" + values[i]);
+            buffer.write("	" + (values[i] == null ? "\\N" : values[i]));
         }
         buffer.newLine();
         return index;
