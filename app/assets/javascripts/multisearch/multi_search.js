fac16bdcc2e06869dc2440db2ae99d433405c0d9
Bart Mesuere <Bart.Mesuere@UGent.be>
2015-01-28 10:49:04 +0100

    move missed peptides

--- app/assets/javascripts/multisearch/multi_search.js
+++ app/assets/javascripts/multisearch/multi_search.js
@@ -1,6 +1,6 @@
 function init_multi(data, sequences, missed, equate_il) {
 
-    constructMultisearch({data : data, equateIL : equate_il});
+    constructMultisearch({data : data, equateIL : equate_il, missed : missed});
 
     $("#downloadDataset").click(function () {
         // Track the download button
@@ -17,9 +17,6 @@ function init_multi(data, sequences, missed, equate_il) {
         }, 1000);
         return true;
     });
-
-    // copy to clipboard for missed peptides
-    addCopy($("#copy-missed span").first(), function () {return $(".mismatches").text(); });
 
     // set up the fullscreen stuff
     if (fullScreenApi.supportsFullScreen) {
@@ -93,16 +90,6 @@ function init_multi(data, sequences, missed, equate_il) {
         }
     });
 
-    // set up missed
-    addMissed();
-
-    function addMissed() {
-        var misses = "";
-        for (var i = 0; i < missed.length; i++) {
-            misses += "<li><a href='http://blast.ncbi.nlm.nih.gov/Blast.cgi?PAGE_TYPE=BlastSearch&amp;SET_SAVED_SEARCH=on&amp;USER_FORMAT_DEFAULTS=on&amp;PAGE=Proteins&amp;PROGRAM=blastp&amp;QUERY=" + missed[i] + "&amp;GAPCOSTS=11%201&amp;EQ_MENU=Enter%20organism%20name%20or%20id--completions%20will%20be%20suggested&amp;DATABASE=nr&amp;BLAST_PROGRAMS=blastp&amp;MAX_NUM_SEQ=100&amp;SHORT_QUERY_ADJUST=on&amp;EXPECT=10&amp;WORD_SIZE=3&amp;MATRIX_NAME=BLOSUM62&amp;COMPOSITION_BASED_STATISTICS=2&amp;SHOW_OVERVIEW=on&amp;SHOW_LINKOUT=on&amp;ALIGNMENT_VIEW=Pairwise&amp;MASK_CHAR=2&amp;MASK_COLOR=1&amp;GET_SEQUENCE=on&amp;NEW_VIEW=on&amp;NUM_OVERVIEW=100&amp;DESCRIPTIONS=100&amp;ALIGNMENTS=100&amp;FORMAT_OBJECT=Alignment&amp;FORMAT_TYPE=HTML&amp;OLD_BLAST=false' target='_blank'>" + missed[i] + "</a> <span class='glyphicon glyphicon-share-alt'></span></li>";
-        }
-        $(".mismatches").html(misses);
-    }
 
 
     // tooltip functions
--- app/assets/javascripts/multisearch/multisearch.js
+++ app/assets/javascripts/multisearch/multisearch.js
@@ -10,9 +10,11 @@ var constructMultisearch = function constructMultisearch(args) {
     var that = {},
         data = args.data,
         equateIL = args.equateIL,
+        missed = args.missed,
         sunburst,
         treemap,
-        treeview;
+        treeview
+        searchtree;
 
     /*************** Private methods ***************/
 
@@ -48,6 +50,22 @@ var constructMultisearch = function constructMultisearch(args) {
             error(err.message, "Loading the Hierarchical outline failed. Please use Google Chrome, Firefox or Internet Explorer 9 or higher.");
         }
 
+        // set up missed
+        addMissed();
+        // copy to clipboard for missed peptides
+        addCopy($("#copy-missed span").first(), function () {return $(".mismatches").text(); });
+
+    }
+
+    /**
+     * Adds the list of missed peptides
+     */
+    function addMissed() {
+        var misses = "";
+        for (var i = 0; i < missed.length; i++) {
+            misses += "<li><a href='http://blast.ncbi.nlm.nih.gov/Blast.cgi?PAGE_TYPE=BlastSearch&amp;SET_SAVED_SEARCH=on&amp;USER_FORMAT_DEFAULTS=on&amp;PAGE=Proteins&amp;PROGRAM=blastp&amp;QUERY=" + missed[i] + "&amp;GAPCOSTS=11%201&amp;EQ_MENU=Enter%20organism%20name%20or%20id--completions%20will%20be%20suggested&amp;DATABASE=nr&amp;BLAST_PROGRAMS=blastp&amp;MAX_NUM_SEQ=100&amp;SHORT_QUERY_ADJUST=on&amp;EXPECT=10&amp;WORD_SIZE=3&amp;MATRIX_NAME=BLOSUM62&amp;COMPOSITION_BASED_STATISTICS=2&amp;SHOW_OVERVIEW=on&amp;SHOW_LINKOUT=on&amp;ALIGNMENT_VIEW=Pairwise&amp;MASK_CHAR=2&amp;MASK_COLOR=1&amp;GET_SEQUENCE=on&amp;NEW_VIEW=on&amp;NUM_OVERVIEW=100&amp;DESCRIPTIONS=100&amp;ALIGNMENTS=100&amp;FORMAT_OBJECT=Alignment&amp;FORMAT_TYPE=HTML&amp;OLD_BLAST=false' target='_blank'>" + missed[i] + "</a> <span class='glyphicon glyphicon-share-alt'></span></li>";
+        }
+        $(".mismatches").html(misses);
     }
 
     /*************** Public methods ***************/
