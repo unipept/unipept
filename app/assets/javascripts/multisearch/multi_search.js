a16b7c6d3c661f312974beb66a3268c485fd2235
Bart Mesuere <Bart.Mesuere@UGent.be>
2015-01-28 11:49:38 +0100

    remove old multi search file

--- app/assets/javascripts/multisearch/multi_search.js
+++
@@ -1,8 +0,0 @@
-function init_multi(data, sequences, missed, equate_il) {
-
-    constructMultisearch({
-        data : data,
-        equateIL : equate_il,
-        missed : missed,
-        sequences : sequences});
-}
