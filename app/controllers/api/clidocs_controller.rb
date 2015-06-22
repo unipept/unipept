74c33f5374ce6c53c6104555318f40c344988504
Bart Mesuere <Bart.Mesuere@UGent.be>
2015-06-22 21:49:06 +0200

    add sidebar links

--- app/controllers/api/clidocs_controller.rb
+++ app/controllers/api/clidocs_controller.rb
@@ -1,18 +1,25 @@
 class Api::ClidocsController < ApplicationController
-
   before_filter :set_sidebar_nav
   before_filter :set_sidebar_subnav, only: []
 
   def index
-    @title = "Unipept command line interface"
-    @sidebar_name = "Overview"
-    @sidebar_subnav = ["Installation", "Configuration"]
+    @title = 'Unipept command line interface'
+    @sidebar_name = 'Overview'
+    @sidebar_subnav = %w(Installation Configuration)
   end
 
   private
+
   def set_sidebar_nav
     @sidebar_nav = [
-      {:name => "Overview",  :path => api_clidocs_path}
+      { name: 'Overview',  path: api_clidocs_path },
+      { name: 'uniprot',  path: api_clidocs_path },
+      { name: 'peptfilter',  path: api_clidocs_path },
+      { name: 'unipept pept2lca',  path: api_clidocs_path },
+      { name: 'unipept pept2prot',  path: api_clidocs_path },
+      { name: 'unipept pept2taxa',  path: api_clidocs_path },
+      { name: 'unipept taxa2lca',  path: api_clidocs_path },
+      { name: 'unipept taxonomy',  path: api_clidocs_path }
     ]
   end
 
