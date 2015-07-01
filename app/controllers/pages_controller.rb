2d5819c472445d368a3f6ee300d3fd6cc5b25a88
Bart Mesuere <Bart.Mesuere@UGent.be>
2015-07-01 17:27:49 +0200

    clean up pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,9 +19,9 @@ class PagesController < ApplicationController
 
     # progressbar stuff
     file = Rails.root.join('public', 'progress')
-    if FileTest.exists?(file)
-      file = File.open(file, 'r')
-      @progress = file.readlines.to_a.map { |line| line.strip.gsub(/#/, "\n").lines.to_a }
-    end
+    return unless FileTest.exists?(file)
+
+    file = File.open(file, 'r')
+    @progress = file.readlines.to_a.map { |line| line.strip.gsub(/#/, "\n").lines.to_a }
   end
 end
