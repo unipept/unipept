bc1c5e0a5128349b16ce8925a4a18f5fcf3cfd66
Bart Mesuere <Bart.Mesuere@UGent.be>
2015-07-01 17:26:15 +0200

    clean up imagemagick controller

--- app/controllers/imagemagick_controller.rb
+++ app/controllers/imagemagick_controller.rb
@@ -4,7 +4,10 @@ class ImagemagickController < ApplicationController
   # converts an svg to a png
   def convert
     i = params[:image].sub("'Helvetica Neue'", 'Helvetica Neue')
-    img = Magick::Image.from_blob(i) { self.density = '144.0x144.0'; self.format = 'SVG'; }.first
+    img = Magick::Image.from_blob(i) do
+      self.density = '144.0x144.0'
+      self.format = 'SVG'
+    end.first
     img.format = 'png'
     image = Base64.encode64(img.to_blob)
     render body: "data:image/png;base64,#{image}"
