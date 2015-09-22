b6c8f2b5e2045dc23dcf32008d4c55c8fa52b3e9
Cizar Almalak <cizaralmalak@we02c031.ugent.be>
2015-09-22 17:40:11 +0200

    added the loadEcNumbers.sh script

---
+++ backend/scripts/loadEcNumbers.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Downloads the enzymes.dat file and loads them into the ec_numbers table
+
+datadir="../data"
+currentdir=`pwd`
+
+# prepare the sql file
+cd "${datadir}"
+rm -f enzyme.dat
+rm -f enzyme.sql
+wget ftp://ftp.expasy.org/databases/enzyme/enzyme.dat
+echo "TRUNCATE TABLE ec_numbers;" > enzyme.sql
+awk 'BEGIN {
+  FS = "   "
+}
+/^ID/{
+  if (id != "") {
+    printf("INSERT INTO ec_numbers (number, name) VALUES (\"%s\", \"%s\");\n", id, name)
+  }
+  name = ""
+  id = $2
+}
+/^DE/{
+  gsub(/.$/, "", $2)
+  name = name $2
+}
+END {
+  printf("INSERT INTO ec_numbers (number, name) VALUES (\"%s\", \"%s\");\n", id, name)
+}' enzyme.dat >> enzyme.sql
+
+# load the file into the database
+echo "loading the database"
+mysql -u unipept -punipept unipept < "enzyme.sql"
+
+cd "${currentdir}"
