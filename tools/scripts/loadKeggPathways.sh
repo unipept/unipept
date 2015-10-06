b8f789b372c44f5e2336c367e54fec60ce013f23
Paretje <info@Online-Urbanus.be>
2015-10-06 18:06:57 +0200

    Use REST API to fetch kegg pathways

--- tools/scripts/loadKeggPathways.sh
+++ tools/scripts/loadKeggPathways.sh
@@ -1,11 +1,29 @@
-#!/bin/sh
-tempfile=$(mktemp)
-echo "TRUNCATE TABLE kegg_pathways;" > pathways.sql
-mysql -p -u unipept -B -e 'SELECT number FROM ec_numbers' unipept | tail -n +2 | while read number; do
-	wget "http://www.genome.jp/dbget-bin/get_linkdb?-t+pathway+ec:$number" -q -O - | fgrep 'kegg-bin/show_pathway' > $tempfile
-	
-	sed 's/^.*">\(.*\)<\/a>   *\([^ ].*\)$/INSERT INTO kegg_pathways (long_id, name) VALUES ("\1", "\2");/' $tempfile >> pathways.sql
-	sed 's/^.*">\(.*\)<\/a>   *\([^ ].*\)$/INSERT INTO kegg_pathways_mappings (long_id, ec_number) VALUES ("\1", "'"$number"'");/' $tempfile >> pathways.sql
-done
+#!/bin/sh -e
+paths=$(mktemp)
+mapping=$(mktemp)
+sql=$(mktemp)
+numbers=$(mktemp)
 
-rm $tempfile
+wget -nv -O $paths http://rest.kegg.jp/list/path
+echo "TRUNCATE TABLE kegg_pathways;" > $sql
+
+sed 's/^path:\([^\t]*\)\t\([^\t]*\)/INSERT INTO kegg_pathways (long_id, name) VALUES ("\1", "\2");/' $paths >> $sql
+
+echo "loading the database"
+mysql -u unipept -punipept unipept < $sql
+
+wget -nv -O - http://rest.kegg.jp/link/path/ec | fgrep 'path:map' > $mapping
+echo "TRUNCATE TABLE kegg_pathway_mappings;" > $sql
+
+mysql -punipept -u unipept -B -e 'SELECT * FROM ec_numbers' unipept | tail -n +2 > $numbers
+mysql -punipept -u unipept -B -e 'SELECT * FROM kegg_pathways' unipept | tail -n +2 > $paths
+
+sed 's/^ec:\([^\t]*\)\tpath:\([^\t]*\)/SELECT id FROM kegg_pathways WHERE long_id="\2";SELECT id FROM ec_numbers WHERE number="\1";/' $mapping | mysql -punipept -u unipept unipept -B | grep -v '^id$' | sed 'N;s/^\(.*\)\n\(.*\)$/INSERT INTO kegg_pathway_mappings (kegg_pathway_id, ec_number_id) VALUES ("\1", "\2");/' >> $sql
+
+echo "loading the database"
+mysql -u unipept -punipept unipept < $sql
+
+rm $paths
+rm $mapping
+rm $numbers
+rm $sql
