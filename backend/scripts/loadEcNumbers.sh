15dd47ef760b39c69660b69f678b327bb1159d25
Cizar Almalak <cizaralmalak@we02c031.ugent.be>
2015-10-14 10:10:06 +0200

    added the enzyme classes to the database

--- backend/scripts/loadEcNumbers.sh
+++ backend/scripts/loadEcNumbers.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # Downloads the enzymes.dat file and loads them into the ec_numbers table
 
-datadir="../data"
+mkdir ../../../data/ecnumbers
+datadir="../../../data/ecnumbers"
 currentdir=`pwd`
 
 # prepare the sql file
 cd "${datadir}"
 rm -f enzyme.dat
+rm -f enzclass.txt
 rm -f enzyme.sql
 wget ftp://ftp.expasy.org/databases/enzyme/enzyme.dat
+wget ftp://ftp.expasy.org/databases/enzyme/enzclass.txt
+
 echo "TRUNCATE TABLE ec_numbers;" > enzyme.sql
 awk 'BEGIN {
   FS = "   "
@@ -28,8 +32,20 @@ END {
   printf("INSERT INTO ec_numbers (number, name) VALUES (\"%s\", \"%s\");\n", id, name)
 }' enzyme.dat >> enzyme.sql
 
+cat enzclass.txt| sed -e "s/- /-    /" | awk 'BEGIN {
+        FS = "    "
+}
+/^[1-9]/ {
+
+        gsub(/ /, "", $1)
+        gsub(/^ */, "", $2)
+        printf("INSERT INTO ec_numbers (number, name) VALUES (\"%s\", \"%s\");\n", $1, $2)
+}
+END {
+}' >> enzyme.sql
+
 # load the file into the database
-echo "loading the database"
-mysql -u unipept -punipept unipept < "enzyme.sql"
+#echo "loading the database"
+#mysql -u unipept -punipept unipept < "enzyme.sql"
 
 cd "${currentdir}"
