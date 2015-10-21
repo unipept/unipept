d730a7f6d7b421f5278d709e8b7342f2f35fed0f
Cizar Almalak <cizaralmalak@we02c031.ugent.be>
2015-10-21 17:05:43 +0200

    changed variable name column_name to ec_column_name

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -59,7 +59,7 @@ class SequencesController < ApplicationController
     # get all items from table EcNumber
     ecdb = EcNumber.all
     # add column names list
-    @column_name = ["name", "digit 1", "digit 2", "digit 3", "digit 4", "count"]
+    @ec_column_name = ["name", "digit 1", "digit 2", "digit 3", "digit 4", "count"]
     # nested list in which each row is stored
     tp_ec_lca_table = []
     # create a hash that contails all required data for visualization
