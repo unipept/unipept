47da889896fcec866dd152d0a1349aaa71bcfee2
Bart Mesuere <Bart.Mesuere@UGent.be>
2015-12-11 16:31:53 +0100

    add database switch queries

---
+++ backend/database/swap_databases.sql
@@ -0,0 +1,24 @@
+drop table
+    ec_cross_references,
+    embl_cross_references,
+    go_cross_references,
+    lineages,
+    peptides,
+    refseq_cross_references,
+    sequences,
+    taxons,
+    uniprot_entries,
+    proteomes,
+    proteome_caches;
+
+rename table unipept2.ec_cross_references to unipept.ec_cross_references;
+rename table unipept2.embl_cross_references to unipept.embl_cross_references;
+rename table unipept2.go_cross_references to unipept.go_cross_references;
+rename table unipept2.lineages to unipept.lineages;
+rename table unipept2.peptides to unipept.peptides;
+rename table unipept2.refseq_cross_references to unipept.refseq_cross_references;
+rename table unipept2.sequences to unipept.sequences;
+rename table unipept2.taxons to unipept.taxons;
+rename table unipept2.uniprot_entries to unipept.uniprot_entries;
+rename table unipept2.proteomes to unipept.proteomes;
+rename table unipept2.proteome_caches to unipept.proteome_caches;
