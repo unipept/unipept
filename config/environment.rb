7bd395085fd4c544d02d46de7f69b16d142a27d8
Paretje <kevin@paretje.be>
2015-12-27 15:25:01 +0100

    Use the biological process ontology for now

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ require 'scbi_go'
 # Initialize GO graph
 file = Rails.root.join('go-basic.obo')
 if !File.exist?(file)
-  IO.copy_stream(open('http://purl.obolibrary.org/obo/go/go-basic.obo'), file)
+  IO.copy_stream(open('http://purl.obolibrary.org/obo/go/go-basic-process.obo'), file)
 end
 GO_GRAPH = ScbiGo::GeneOntology.new(filename=file.to_s)
 
