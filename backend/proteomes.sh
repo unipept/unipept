14748bfb15ef447b540bf4c54441cff7cb1373c9
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-01-28 10:49:54 +0100

    run curls to uniprot in parallel

--- backend/proteomes.sh
+++ backend/proteomes.sh
@@ -7,13 +7,22 @@ proteome_gz_file="$1"
 type_strains="$2"
 outfile="$3"
 
-tmp="$(mktemp)"
+set -e
 
-zcat $proteome_gz_file | while read id accession; do
+tmp="$(mktemp)"
+echo "$tmp"
+
+get_proteome() {
+    local id="$1"
+    local accession="$2"
     curl -s http://www.uniprot.org/proteomes/$accession \
         | html2text -nobs -width 1000 \
         | awk -f proteomes.awk -v id=$id -v accession=$accession
-done > "$tmp"
+}
+# to reach it with the bash subprocess of xargs
+export -f get_proteome
+
+zcat $proteome_gz_file | xargs -P 10 -I{} bash -c 'get_proteome {}' > "$tmp"
 
 join -1 8 -2 1 -a 1 -t '	'                                         \
         <(sort -t'	' -k 8 "$tmp")                                    \
