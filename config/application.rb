db822f2a1dcac6c8ca89819deb02ac28550803d7
Cizar Almalak <cizar.almalak@ugent.be>
2016-02-12 17:30:53 +0100

    new branch for database management

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,7 @@ module UnipeptWeb
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
     config.versions = {
-      unipept: '3.1.2',
+      unipept: '3.1.2 functional-database',
       gem: '1.1.0',
       uniprot: '2016.02'
     }
