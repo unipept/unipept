5487a06d38ed98ed6f46bfa65b5bbf93806faa89
Cizar Almalak <cizar.almalak@ugent.be>
2016-02-16 13:17:00 +0100

    added relation between ec number and pathway mapping

--- app/models/ec_number.rb
+++ app/models/ec_number.rb
@@ -3,4 +3,5 @@ class EcNumber < ActiveRecord::Base
   attr_accessible nil
 
   has_many :ec_cross_references, foreign_key: 'ec_number_id', primary_key: 'id', class_name: 'ec_cross_reference'
+  has_many :kegg_pathway_mappings, foreign_key: 'ec_number_id', primary_key: 'id', class_name: 'kegg_pathway_mapping'
 end
