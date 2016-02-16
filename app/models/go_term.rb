ca0db61cbfae5546cbd831d58b71c5146624ed0f
Cizar Almalak <cizar.almalak@ugent.be>
2016-02-16 11:30:43 +0100

    added model for go terms

---
+++ app/models/go_term.rb
@@ -0,0 +1,6 @@
+class Goterm < ActiveRecord::Base
+  include ReadOnlyModel
+  attr_accessible nil
+
+  has_many :go_cross_references, foreign_key: 'go_term_id', primary_key: 'id', class_name: 'go_cross_reference'
+end
