e830ef205d5b30bae07de0f6ebfc9fe47bc4ed92
Cizar Almalak <cizar.almalak@ugent.be>
2016-02-22 15:18:46 +0100

    changed variable name

--- backend/interprotable.py
+++ backend/interprotable.py
@@ -6,16 +6,16 @@ import xml.etree.ElementTree as ET
 tree = ET.parse(gzip.open(sys.argv[1]))
 root = tree.getroot()
 
-xindex=0
+pkey=0
 for child in root.getchildren():
 	if child.tag == "interpro":
 		attr=child.attrib
-		xindex+=1
+		pkey+=1
 		parent=attr.get("id")
 		intid=attr.get("id")
 		name=attr.get("short_name")
 		types=" ".join(attr.get("type").split("_"))
 		for nchild in child.iter("parent_list"):
 			parent=nchild.getchildren()[0].get("ipr_ref")
-		print("{}\t{}\t{}\t{}\t{}".format(xindex, parent, intid, name, types))
+		print("{}\t{}\t{}\t{}\t{}".format(pkey, parent, intid, name, types))
 
