de5a4cbc58843670f426f1d565b2d711b29d7198
Cizar Almalak <cizar.almalak@ugent.be>
2016-03-25 15:51:25 +0100

    added lca calculation

--- app/models/ec_cross_reference.rb
+++ app/models/ec_cross_reference.rb
@@ -13,4 +13,28 @@ class EcCrossReference < ActiveRecord::Base
 
   belongs_to :uniprot_entry
   belongs_to :ec_number_info, foreign_key: 'ec_number', primary_key: 'ec_number', class_name: 'ec_number'
+
+
+  # adds some sort of LCA calculation for EC numbers.
+  # the input is an array of ec numbers represented as strings.
+  def self.calculate_lca(ecs)
+    return "nil" if ecs.empty?
+    lca = ""
+    done = false
+    splits = ecs.map{|e| e.split(".")}
+    for level in (0..3).to_a do
+      if done
+        lca += "-."
+      else
+        current = splits.map{|r| r[level] == "-" ? nil : r[level]}.uniq.compact
+        if current.length == 1
+          lca += current[0] + "."
+        else
+          lca += "-."
+          done = true
+        end
+      end
+    end
+    return lca.chop
+  end
 end
