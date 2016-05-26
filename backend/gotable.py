a657e136a0de0f026822205e8ebf06281108a146
Cizar Almalak <cizar.almalak@ugent.be>
2016-05-26 15:19:49 +0200

    added argparse command and alt_id parsing

--- backend/gotable.py
+++ backend/gotable.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python
 
 import sys
+import os
+import gzip
+import argparse
 
-seg, pkey, go, name, namespace, deff=False, 1, "", "", "", ""
-for line in open(sys.argv[1]):
+parser = argparse.ArgumentParser(description='Get GO terms.')
+parser.add_argument('-g', metavar="Obo file", nargs='?', type=str,
+                   help='input Obo file')
+parser.add_argument('-o', metavar="Output go file", nargs='?', type=str, default="",
+                   help='output go file')
+parser.add_argument('-a', metavar="Output alt ids file", nargs='?', type=str, default="",
+                   help='output alt ids file')
+args = parser.parse_args()
+
+
+seg, pkey, go, name, namespace, deff, alt_id=False, 1, "", "", "", "", []
+save_go=gzip.open(args.o, "w")
+save_alt=gzip.open(args.a, "w")
+for line in open(args.g, "r"):
 	line=line.strip()
 	if line[0:6] == "[Term]" or seg:
 		seg=True
 		if line == "":
-			print("{}\t{}\t{}\t{}".format(pkey, go, name, namespace))
+			#print("{}\t{}\t{}\t{}".format(pkey, go, name, namespace))
+			save_go.write("{}\t{}\t{}\t{}\n".format(pkey, go, name, namespace))
+			if alt_id != []:
+				save_alt.write("{}\t{}\n".format(go, "\t".join(alt_id)))
+				#print("{}\t{}".format(go, "\t".join(alt_id)))
+				alt_id = []
 			pkey+=1
 			seg, go, name, namespace, deff=False, "", "", "", ""
 		elif line[0:3] == "id:":
 			go=line.split("d: ")[-1]
+		elif line[0:6] == "alt_id":
+			alt_id.append(line.split("alt_id: ")[-1])
 		elif line[0:5] == "name:":
 			name=line.split("ame: ")[-1]
 		elif line[0:10] == "namespace:":
