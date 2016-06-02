931fdd14ba88885153356cdaea447393161d33a8
Cizar Almalak <cizar.almalak@ugent.be>
2016-06-02 17:15:16 +0200

    moved code for preprocessing of ec data to model

--- app/models/ec_number.rb
+++ app/models/ec_number.rb
@@ -12,4 +12,53 @@ class EcNumber < ActiveRecord::Base
 
   has_many :ec_cross_references, foreign_key: 'ec_number_id', primary_key: 'id', class_name: 'ec_cross_reference'
   has_many :kegg_pathway_mappings, foreign_key: 'ec_number_id', primary_key: 'id', class_name: 'kegg_pathway_mapping'
+
+  def self.get_ontology(ec)
+  	lineage_list = []
+  	mem = ""
+  	count = 3
+  	ec.split(".").each do |ec_rank|
+  	  if ec_rank != "-"
+  		lineage_list.append(mem+ec_rank+".-"*count)
+  	  else
+  	  	return lineage_list
+  	  end
+  	  mem += ec_rank+"."
+  	  count -= 1
+  	end
+  	return lineage_list
+  end
+
+  def self.count_ontology(ec_self_count)
+  	count_dic = {}
+  	ec_self_count.each do |key, val|
+  	  ontology = self.get_ontology(key)
+  	  ontology.each do |ec|
+	  	count_dic[ec] = count_dic.has_key?(ec) ? count_dic[ec]+val : val
+	  end
+  	end
+  	return count_dic
+  end
+
+  def self.get_ec_function(ec_ontology, ecnumber_db)
+    ec_ontology_functions = {}
+    # get all ec numbers from the ec_ontology
+    ec_all_ontology = ec_ontology.keys.flatten(1).uniq
+    # get all functions
+    tmp_ec_ontology_functions = ecnumber_db.select("ec_number, name").where(ec_number: ec_all_ontology)
+    # put functions in hash
+    tmp_ec_ontology_functions.each do |ec_func|
+      ec_ontology_functions[ec_func[:ec_number]] = ec_func[:name]
+    end
+    return ec_ontology_functions
+  end
+  
+  def self.calc_ec_lca(ec_hash, ec_root, common_ec_lineage)
+    if ec_hash["children"].nil? or ec_hash["children"].size > 1 or ec_hash["children"] == []
+      return ec_root, common_ec_lineage
+    end
+    ec_root = ec_hash["children"][0]["id"]
+    common_ec_lineage.push(ec_root)
+    calc_ec_lca(ec_hash["children"][0], ec_root, common_ec_lineage)
+  end
 end
