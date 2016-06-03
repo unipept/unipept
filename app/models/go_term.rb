1b1b0546965965cec603b3417dc94075e7de3392
Cizar Almalak <cizar.almalak@ugent.be>
2016-06-03 17:35:20 +0200

    updated relation with go_lca

--- app/models/go_term.rb
+++ app/models/go_term.rb
@@ -3,4 +3,5 @@ class Goterm < ActiveRecord::Base
   attr_accessible nil
 
   has_many :go_cross_references, foreign_key: 'go_term_id', primary_key: 'id', class_name: 'go_cross_reference'
+  has_many :go_lcas
 end
