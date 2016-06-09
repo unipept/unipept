b41d83bd632449c28520a0349f47bbdab639fdef
Cizar Almalak <cizar.almalak@ugent.be>
2016-06-09 12:12:34 +0200

    fixed go relation

--- app/models/go_lca.rb
+++ app/models/go_lca.rb
@@ -2,6 +2,7 @@ class Go_lca < ActiveRecord::Base
 	include ReadOnlyModel
 	attr_accessible nil
 
-	belongs_to :go_term
 	belongs_to :sequence
+	belongs_to :go_lca_g, foreign_key: 'go_lca', primary_key: 'id', class_name: 'Go_term'
+	belongs_to :go_lca_il_g, foreign_key: 'go_lca_il', primary_key: 'id', class_name: 'Go_term'
 end
