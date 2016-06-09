54df5683ae229ef34888be9d12078806ed89bd1e
Cizar Almalak <cizar.almalak@ugent.be>
2016-06-09 11:17:58 +0200

    corrected reference to linked tables

--- app/models/ec_number.rb
+++ app/models/ec_number.rb
@@ -10,8 +10,8 @@ class EcNumber < ActiveRecord::Base
   include ReadOnlyModel
   attr_accessible nil
 
-  has_many :ec_cross_references, foreign_key: 'ec_number_id', primary_key: 'id', class_name: 'ec_cross_reference'
-  has_many :kegg_pathway_mappings, foreign_key: 'ec_number_id', primary_key: 'id', class_name: 'kegg_pathway_mapping'
+  has_many :ec_cross_references
+  has_many :kegg_pathway_mappings
 
   def self.get_ontology(ec)
   	lineage_list = []
--- app/models/interpro_entry.rb
+++ app/models/interpro_entry.rb
@@ -2,5 +2,5 @@ class InterproEntry < ActiveRecord::Base
   include ReadOnlyModel
   attr_accessible nil
 
-  has_many :interpro_cross_references, foreign_key: 'interpro_entry_id', primary_key: 'id', class_name: 'interpro_cross_reference'
+  has_many :interpro_cross_references
 end
