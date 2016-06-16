6aee6ec28ab434943edece5b9a46f26f9338b1ef
Cizar Almalak <cizar.almalak@ugent.be>
2016-06-16 14:54:08 +0200

    annotate

--- app/models/ec_number.rb
+++ app/models/ec_number.rb
@@ -2,9 +2,9 @@
 #
 # Table name: ec_numbers
 #
-#  id        :integer          not null, primary key
-#  ec_number :string(15)       not null
-#  name      :string(140)      not null
+#  id   :integer          not null, primary key
+#  code :string(15)       not null
+#  name :string(140)      not null
 #
 
 class EcNumber < ActiveRecord::Base
