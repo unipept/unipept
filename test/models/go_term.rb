4c91922ff9178fd4681efca15c7352c757728f25
Cizar Almalak <cizar.almalak@ugent.be>
2016-06-16 14:51:57 +0200

    update test for ec/go/interpro

--- test/models/go_term.rb
+++ test/models/go_term.rb
@@ -1,1 +1,37 @@
 require 'test_helper'
+
+class GoTermTest < ActiveSupport::TestCase
+  test 'should rails error on create new GoTerm' do
+    assert_raises(ActiveRecord::ReadOnlyRecord) { GoTerm.new.save }
+  end
+
+  test 'should raise error on save' do
+    go_term = go_terms(:goterm1)
+    assert_raises(ActiveRecord::ReadOnlyRecord) { go_term.save }
+  end
+
+  test 'should raise error on code change' do
+    go_term = go_terms(:goterm1)
+    assert_raises(ActiveRecord::ActiveRecordError) { go_term.update_attribute(:code, '35') }
+  end
+
+  test 'should raise error on namespace change' do
+    go_term = go_terms(:goterm1)
+    assert_raises(ActiveRecord::ActiveRecordError) { go_term.update_attribute(:namespace, '35') }
+  end
+
+  test 'should raise error on name change' do
+    go_term = go_terms(:goterm1)
+    assert_raises(ActiveRecord::ActiveRecordError) { go_term.update_attribute(:name, '35') }
+  end
+
+  test 'should raise error on delete' do
+    go_term = go_terms(:goterm1)
+    assert_raises(ActiveRecord::ReadOnlyRecord) { go_term.delete }
+  end
+
+  test 'should raise error on destroy' do
+    go_term = go_terms(:goterm1)
+    assert_raises(ActiveRecord::ReadOnlyRecord) { go_term.destroy }
+  end
+end
--- test/models/interpro_cross_reference.rb
+++ test/models/interpro_cross_reference.rb
@@ -1,1 +1,32 @@
 require 'test_helper'
+
+class InterproCrossReferenceTest < ActiveSupport::TestCase
+  test 'should rails error on create new interprocrossreference' do
+    assert_raises(ActiveRecord::ReadOnlyRecord) { interprocrossreference.new.save }
+  end
+
+  test 'should raise error on save' do
+    interprocrossreference = interpro_cross_references(:interprocrossreference1)
+    assert_raises(ActiveRecord::ReadOnlyRecord) { interprocrossreference.save }
+  end
+
+  test 'should raise error on uniprot_entry_id change' do
+    interprocrossreference = interpro_cross_references(:interprocrossreference1)
+    assert_raises(ActiveRecord::ActiveRecordError) { interprocrossreference.update_attribute(:uniprot_entry_id, 35) }
+  end
+
+  test 'should raise error on go_id change' do
+    interprocrossreference = interpro_cross_references(:interprocrossreference1)
+    assert_raises(ActiveRecord::ActiveRecordError) { interprocrossreference.update_attribute(:interpro_entry_code, '35') }
+  end
+
+  test 'should raise error on delete' do
+    interprocrossreference = interpro_cross_references(:interprocrossreference1)
+    assert_raises(ActiveRecord::ReadOnlyRecord) { interprocrossreference.delete }
+  end
+
+  test 'should raise error on destroy' do
+    interprocrossreference = interpro_cross_references(:interprocrossreference1)
+    assert_raises(ActiveRecord::ReadOnlyRecord) { interprocrossreference.destroy }
+  end
+end
