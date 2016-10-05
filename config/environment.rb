7ee661cf559b7d3b5cca64037f885d8cc99cdd32
Cizar Almalak <cizar.almalak@ugent.be>
2016-10-05 19:33:44 +0200

    added gem for GO parser

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 require 'csv'
+require 'scbi_go'
+
+# Initialize GO graph
+file = Rails.root.join('../data/goterms/goinfo.obo')
+GO_GRAPH = ScbiGo::GeneOntology.new(filename=file.to_s)
 
 # Initialize the rails application
 UnipeptWeb::Application.initialize!
