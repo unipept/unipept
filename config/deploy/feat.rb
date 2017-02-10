79f0e0f2245961156e5f21b452d09ec25d198818
Cizar Almalak <cizar.almalak@ugent.be>
2017-02-10 15:54:21 +0100

    fixed conflict

--- config/deploy/feat.rb
+++ config/deploy/feat.rb
@@ -5,9 +5,5 @@ server 'adler.ugent.be', user: 'bmesuere', roles: [:web, :app], ssh_options: {
   port: 4840
 }
 
-<<<<<<< HEAD
 set :branch, 'feature/adler-test'
-=======
-set :branch, 'feature/develop-functional-analysis-tpa'
->>>>>>> 07ba73990ca0cbfb929a5b0466516888f300644c
 set :rails_env, :development
