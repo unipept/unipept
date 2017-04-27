14ff32a8f53ce5f27620eb2b3a835db1061f0ee7
Cizar Almalak <cizar.almalak@ugent.be>
2017-04-27 11:38:42 +0200

    deploy

--- config/deploy/feat.rb
+++ config/deploy/feat.rb
@@ -5,5 +5,5 @@ server 'adler.ugent.be', user: 'bmesuere', roles: [:web, :app], ssh_options: {
   port: 4840
 }
 
-set :branch, 'feature/uniprot-proteomes'
+set :branch, 'add-functional-analysis-mpa-to-develop'
 set :rails_env, :development
