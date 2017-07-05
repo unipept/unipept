8b99ce001c35de31e17853a2d29b2c1e7cea62d2
Bart Mesuere <Bart.Mesuere@UGent.be>
2017-07-05 10:41:43 +0200

    get rid of capistrano deprecation error

--- config/deploy/feat.rb
+++ config/deploy/feat.rb
@@ -1,9 +1,9 @@
 set :stage, :feat
 
 # don't specify db as it's not needed for unipept
-server 'adler.ugent.be', user: 'bmesuere', roles: %i[web app], ssh_options: {
+server 'morty.ugent.be', user: 'unipept', roles: %i[web app], ssh_options: {
   port: 4840
 }
 
-set :branch, 'feature/uniprot-proteomes'
+set :branch, 'feature/rails-5.1'
 set :rails_env, :development
