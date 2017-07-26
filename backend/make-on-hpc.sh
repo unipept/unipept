f96d9e6250da83f35b59ac1df86e2a8161075e23
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2017-07-26 15:57:08 +0200

    generic makefile and LineagesSequencesTaxons2LCAs
    - add K to configuration
    - add HPC (phanpy) configuration
    - add a script to qsub on HPC
    - modify LineagesSequencesTaxons2LCAs to take strings as the first column
    - adjust makefile to buffer above change

---
+++ backend/make-on-hpc.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#PBS -N unipept-backend
+#PBS -q long
+#PBS -m abe
+#PBS -l nodes=1:ppn=24
+#PBS -l walltime=72:00:00
+#PBS -l vmem=500gb
+
+# Running:
+# $ module swap cluster/phanpy
+# $ qsub make-on-hpc.sh
+
+# Loading the required modules
+module load Java
+module load Maven
+
+# Check the current directory
+pushd "$PBS_O_WORKDIR"
+
+# Running the makefile
+make --debug=v sequences
+
+# Reset the directory
+popd
+
