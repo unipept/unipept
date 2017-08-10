d032c553ba6dd6d8bd22792d18ad380710aff021
Bart Mesuere <Bart.Mesuere@UGent.be>
2017-08-10 15:53:01 +0200

    enable common chunks

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -41,6 +41,19 @@ module.exports = {
     new ManifestPlugin({
       publicPath: output.publicPath,
       writeToFileEmit: true
+    }),
+    // Creates a common vendor.js with all shared modules
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: (module) => {
+        // this assumes your vendor imports exist in the node_modules directory
+        return module.context && module.context.indexOf('node_modules') !== -1;
+      }
+    }),
+    // Webpack code chunk - manifest.js
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      minChunks: Infinity
     })
   ],
 
