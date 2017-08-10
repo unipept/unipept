81ba17af098dfc902f96537dd2b3669bc2cb5bed
Bart Mesuere <Bart.Mesuere@UGent.be>
2017-08-10 13:59:01 +0200

    remove polyfills

--- vendor/assets/javascripts/vendor.js
+++ vendor/assets/javascripts/vendor.js
@@ -1,26 +1,15 @@
-//  es6 shims
-//  https://github.com/jakearchibald/es6-promise/
-//  used by IE, SF 6/7.0
-//  2.0.1
-//= require es6-shims/es6-promise.min.js
-//
-//  https://github.com/WebReflection/es6-collections
-//  used by IE, SF 6/7.0
-//  0.5.2
-//= require es6-shims/es6-collections.min.js
-
 //  global
 //  3.3.5
-//= require bootstrap/bootstrap.min.js
+// = require bootstrap/bootstrap.min.js
 
 //  pancore
 //  http://sliptree.github.io/bootstrap-tokenfield/
-//= require bootstrap-tokenfield/bootstrap-tokenfield.min.js
+// = require bootstrap-tokenfield/bootstrap-tokenfield.min.js
 
 //  export treemap to png
 //  0.4.1
-//= require html2canvas/html2canvas.js
+// = require html2canvas/html2canvas.js
 
 // used for clustering page
 //  newick.js
-//= require newick/newick.js
+// = require newick/newick.js
