dae4814fdb52f42306814da8b741ca9f2edac490
Bart Mesuere <Bart.Mesuere@UGent.be>
2017-08-10 14:14:05 +0200

    add babel polyfills

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import "babel-polyfill";
 import jQuery from "jquery";
 import d3 from "d3";
 import dragula from "dragula";
