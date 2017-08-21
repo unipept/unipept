5ef5b493cd353b109f9c4d86d9f7e4139435c9cf
Bart Mesuere <Bart.Mesuere@UGent.be>
2017-08-21 18:32:32 +0200

    call api from js in batches

--- app/assets/javascripts/mpa/dataset.js
+++ app/assets/javascripts/mpa/dataset.js
@@ -1,10 +1,28 @@
+const BATCH_SIZE = 1;
+
 class Dataset {
     constructor(peptides = []) {
         this.originalPeptides = Dataset.cleanPeptides(peptides);
+        this.length = this.originalPeptides.length;
     }
 
     process() {
-        console.log(this.originalPeptides);
+        const startIds = Array.from({length: Math.ceil(this.length/BATCH_SIZE)}, (v, k) => k * BATCH_SIZE);
+        startIds.reduce((sequence, startId) => {
+            return sequence.then(() => {
+                const data = JSON.stringify({
+                    peptides: this.originalPeptides.slice(startId, startId + BATCH_SIZE),
+                });
+                return fetch("/mpa/pept2lca", {
+                    method: "POST",
+                    headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: data,
+                }).then(res => res.json()).then(res => console.log(res));
+            });
+        }, Promise.resolve());
     }
 
     static cleanPeptides(peptides) {
--- app/controllers/mpa_controller.rb
+++ app/controllers/mpa_controller.rb
@@ -1,9 +1,11 @@
 class MpaController < ApplicationController
   before_action :default_format_json, except: ['analyze']
+  skip_before_action :verify_authenticity_token, except: ['analyze']
 
   def analyze
     @header_class = 'MPA'
-    @peptides = (params[:peptides] || '').lines.to_json
+    #@peptides = (params[:peptides] || '').lines.to_json TODO
+    @peptides = ["aalter", "ENDLVVK", "AAAADLAPLPR"].to_json
   end
 
   def pept2lca
