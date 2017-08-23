1e1fcd612f29de53652d1f253c09563998fd0c6c
Bart Mesuere <Bart.Mesuere@UGent.be>
2017-08-23 14:46:46 +0200

    add settings form

--- app/assets/javascripts/mpa/index.js
+++ app/assets/javascripts/mpa/index.js
@@ -2,9 +2,10 @@ import {Dataset} from "./dataset.js";
 import TreeView from "unipept-visualizations/src/treeview/treeview.js";
 
 class MPA {
-    constructor(peptides = []) {
+    constructor(peptides = [], il = true, dupes = true, missed = false) {
         this.datasets = [];
         this.addDataset(peptides);
+        this.setUpForm(peptides, il, dupes, missed);
     }
 
     async addDataset(peptides) {
@@ -12,7 +13,14 @@ class MPA {
         this.datasets.push(dataset);
         let tree = await dataset.process();
         $("#treeview").treeview(tree.getRoot());
-        console.log(tree);
+        return dataset;
+    }
+
+    setUpForm(peptides, il, dupes, missed) {
+        $("#qs").text(peptides.join("\n"));
+        $("#il").prop("checked", il);
+        $("#dupes").prop("checked", dupes);
+        $("#missed").prop("checked", missed);
     }
 }
 
