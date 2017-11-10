db2c5fdd6eece88d7c98a58e6b77814623ae21ea
Bart Mesuere <Bart.Mesuere@UGent.be>
2017-11-10 11:28:34 +0100

    drop tables and load data

--- backend/database/index.sh
+++ backend/database/index.sh
@@ -5,50 +5,52 @@ function print {
     echo $(date -u) $1
 }
 
+database=$1
+
 
 # print "adding index to taxons"
-# echo "ALTER TABLE taxons ADD INDEX fk_taxon_taxon (parent_id ASC);" | mysql -u unipept -punipept unipept
+# echo "ALTER TABLE taxons ADD INDEX fk_taxon_taxon (parent_id ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to uniprot_entries"
-echo "ALTER TABLE uniprot_entries ADD INDEX fk_uniprot_entries_taxons (taxon_id ASC);" | mysql -u unipept -punipept unipept
-echo "ALTER TABLE uniprot_entries ADD UNIQUE INDEX idx_uniprot_entries_accession (uniprot_accession_number ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE uniprot_entries ADD INDEX fk_uniprot_entries_taxons (taxon_id ASC);" | mysql -u unipept -punipept $database
+echo "ALTER TABLE uniprot_entries ADD UNIQUE INDEX idx_uniprot_entries_accession (uniprot_accession_number ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to ec_numbers"
-echo "ALTER TABLE ec_numbers ADD UNIQUE INDEX idx_ec_numbers (code ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE ec_numbers ADD UNIQUE INDEX idx_ec_numbers (code ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to go_terms"
-echo "ALTER TABLE go_terms ADD UNIQUE INDEX idx_go_code (code ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE go_terms ADD UNIQUE INDEX idx_go_code (code ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to sequences"
-echo "ALTER TABLE sequences ADD UNIQUE INDEX idx_sequences (sequence ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE sequences ADD UNIQUE INDEX idx_sequences (sequence ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to sequences"
-echo "ALTER TABLE sequences ADD INDEX fk_sequences_taxons (lca ASC);" | mysql -u unipept -punipept unipept
-echo "ALTER TABLE sequences ADD INDEX fk_sequences_taxons_2 (lca_il ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE sequences ADD INDEX fk_sequences_taxons (lca ASC);" | mysql -u unipept -punipept $database
+echo "ALTER TABLE sequences ADD INDEX fk_sequences_taxons_2 (lca_il ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to peptides"
-echo "ALTER TABLE peptides ADD INDEX fk_peptides_sequences (sequence_id ASC);" | mysql -u unipept -punipept unipept
-echo "ALTER TABLE peptides ADD INDEX fk_peptides_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept unipept
-echo "ALTER TABLE peptides ADD INDEX fk_peptides_original_sequences (original_sequence_id ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE peptides ADD INDEX fk_peptides_sequences (sequence_id ASC);" | mysql -u unipept -punipept $database
+echo "ALTER TABLE peptides ADD INDEX fk_peptides_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept $database
+echo "ALTER TABLE peptides ADD INDEX fk_peptides_original_sequences (original_sequence_id ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to embl_cross_references"
-echo "ALTER TABLE embl_cross_references ADD INDEX fk_embl_reference_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept unipept
-# echo "ALTER TABLE embl_cross_references ADD INDEX idx_sequence_id (sequence_id ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE embl_cross_references ADD INDEX fk_embl_reference_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept $database
+# echo "ALTER TABLE embl_cross_references ADD INDEX idx_sequence_id (sequence_id ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to refseq_cross_references"
-echo "ALTER TABLE refseq_cross_references ADD INDEX fk_refseq_reference_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE refseq_cross_references ADD INDEX fk_refseq_reference_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to go_cross_references"
-echo "ALTER TABLE go_cross_references ADD INDEX fk_go_reference_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept unipept
-# echo "ALTER TABLE go_cross_references ADD INDEX fk_go_cross_reference_go_terms_idx (go_term_code ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE go_cross_references ADD INDEX fk_go_reference_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept $database
+# echo "ALTER TABLE go_cross_references ADD INDEX fk_go_cross_reference_go_terms_idx (go_term_code ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to ec_cross_references"
-echo "ALTER TABLE ec_cross_references ADD INDEX fk_ec_reference_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept unipept
-# echo "ALTER TABLE ec_cross_references ADD INDEX fk_ec_terms_reference_go_terms_idx (ec_number_code ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE ec_cross_references ADD INDEX fk_ec_reference_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept $database
+# echo "ALTER TABLE ec_cross_references ADD INDEX fk_ec_terms_reference_go_terms_idx (ec_number_code ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to proteomes"
-echo "ALTER TABLE proteomes ADD INDEX fk_taxons_proteomes (taxon_id ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE proteomes ADD INDEX fk_taxons_proteomes (taxon_id ASC);" | mysql -u unipept -punipept $database
 
 print "adding index to proteome_cross_references"
-echo "ALTER TABLE proteome_cross_references ADD INDEX fk_proteome_cross_references_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept unipept
-echo "ALTER TABLE proteome_cross_references ADD INDEX fk_proteome_cross_references (proteome_id ASC);" | mysql -u unipept -punipept unipept
+echo "ALTER TABLE proteome_cross_references ADD INDEX fk_proteome_cross_references_uniprot_entries (uniprot_entry_id ASC);" | mysql -u unipept -punipept $database
+echo "ALTER TABLE proteome_cross_references ADD INDEX fk_proteome_cross_references (proteome_id ASC);" | mysql -u unipept -punipept $database
--- backend/database/update_uniprot.sh
+++ backend/database/update_uniprot.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 database="unipept2"
-workdir=`find . -name "tables-*" | sort | tail -1 | sed "s/^..//"`
+tabledir=`find . -name "tables-*" | sort | tail -1 | sed "s/^..//"`
 
-echo "This script will load the files from $workdir into $database."
+echo "This script will load the files from $tabledir into $database."
 read -p "Do you want to continue? (y/n) " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # do dangerous stuff
+    cp load.sh $tabledir
+    cp index.sh $tabledir
+    cd $tabledir
+    echo "dropping tables"
+    find . -name "*.tsv.gz" | sed "s/..\(.*\)\.tsv\.gz/\1/" | tr "\n" "," | sed "s/,/, /g" | sed "s/^/drop table /" | sed "s/, $/;/" | mysql -u unipept -punipept $database
+
+    echo "creating database"
+    cat ../structure_no_index.sql | sed "s/unipept/$database/" | mysql -u unipept -punipept $database
+
+    echo "loading data"
+    ./load.sh $database
+
+    echo "indexing data"
+    ./index.sh $database
 fi
