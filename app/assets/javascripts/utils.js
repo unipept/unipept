796dff1011f4275d0a06257fa163b8cbf05193c5
beardhatcode <mail.garonn@gmail.com>
2017-11-27 14:39:58 +0100

    feat(js utils): add to CSV utility function

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -218,6 +218,34 @@ function stringTitleize(s){
 }
 
 /**
+ * Converts a javascript array of array to RFC 4180 complient CSV
+ *
+ * Each record is located on a separate line, delimited by a line break (CRLF)
+ *
+ * Fields containing line breaks (CRLF), double quotes, and commas should be
+ * enclosed in double-quotes; If double-quotes are used to enclose fields, then
+ * a double-quote appearing inside a field must be escaped by preceding it with
+ * another double quote.
+ *
+ * Prepending `data:text/csv,` makes a valid data url
+ * @param  {[[string]]} grid [description]
+ * @return {string}      The csv string
+ */
+function toCSVString(grid) {
+    return grid.map(line =>
+        line.map(cell => {
+            let content = cell.toString();
+            if (content.includes(",") || content.includes("\"") ||
+                content.includes("\n") || content.includes("\r")) {
+                return `"${content.replace(/"/g, "\"\"")}"`;
+            } else {
+                return content;
+            }
+        }).join(",")
+    ).join("\n\r");
+}
+
+/**
  * Triggers the image export modal.
  *
  * If an svgSelector is present, sends the SVG-code to the server to convert
