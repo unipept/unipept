fa8f9be827fd250e03fbb3f804911d64cc9d06be
beardhatcode <mail.garonn@gmail.com>
2017-11-27 14:58:42 +0100

    feat(download): add filetype to download

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -3,8 +3,10 @@ class DownloadController < ApplicationController
   def download
     data = params[:data]
     filename = params[:filename]
+    filetype = params[:filetype] || "text/plain"
+    charset = params[:charset] || "iso-8859-1"
     cookies['nonce'] = params[:nonce]
     response.headers['Set-Cookie'] = 'nonce=' + params[:nonce]
-    send_data data, type: 'text/plain; charset=iso-8859-1; header=present', disposition: 'attachment; filename=' + filename
+    send_data data, type: "#{filetype}; charset=#{charset}; header=present", disposition: 'attachment; filename=' + filename
   end
 end
