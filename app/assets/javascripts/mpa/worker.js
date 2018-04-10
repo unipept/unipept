3ead5a898173e027acdaf42e4e6ac745a616e031
beardhatcode <mail.garonn@gmail.com>
2018-04-10 15:56:17 +0200

    doc(mpa worker): write documentation

--- app/assets/javascripts/mpa/worker.js
+++ app/assets/javascripts/mpa/worker.js
@@ -1,35 +1,66 @@
-import "babel-polyfill";
+import "babel-polyfill"; // for async await webpacker support
+// TODO: also include other pollyfills?
 import GOTerms from "../components/goterms.js";
 import ECNumbers from "../components/ecnumbers.js";
 import {postJSON} from "../utils.js";
+
+const BATCH_SIZE = 100;
+const PEPT2DATA_URL = "/mpa/pept2data";
 
 /* Worker global conaining processed peptides
  * Keeping this here gives a huge perforamce boost
  */
 let processedPeptides = new Map();
 
-/**
- * Sends the list of peptides to the server and returns the proccesed results
- *
- * @param {*} preparedPeptides
- * @param {*} il
- * @param {*} missed
- * @return result
- */
-export async function process(originalPeptides, il, dupes, missed) {
-    const preparedPeptides = preparePeptides(originalPeptides, il, dupes, missed);
-    const peptideList = Array.from(preparedPeptides.keys());
-    const BATCH_SIZE = 100,
-        PEPT2LCA_URL = "/mpa/pept2lca",
-        PEPT2FA_URL = "/mpa/pept2fa";
 
-    setProgress(1/peptideList.length);
+/**
+ * @typedef {Object} MPAResult
+ * @property {Map}      processed   A map form pepides to information about them
+ * @property {string[]} missed      The list of peptides that could not be matched
+ * @property {number}   numMatched  Number of peptides that were matched
+ * @property {number}   numSearched Number of peptides that were matched
+ */
+
+/**
+ * @typedef {Object} MPAConfig
+ * @property  {boolean} il     should we equate I and L
+ * @property  {boolean} dupes  should we filter duplicates?
+ * @property  {boolean} missed will we perform advancedMissedCleavageHandling
+ */
+
+
+/**
+ * @typedef {Object} MPAFAResult
+ * @property  {number} weightedValue
+ *  The sum of the relative weights of the GOTerm in each sequence it occurs in
+ * @property  {number} absoluteCount
+ *   The number of occurences of this annotation in a protein that was matched by a
+ *   sequence taking dupes into account (only if il=false)
+ * @property  {number} absoluteCountFiltered
+ *   `absoluteCount` not taking dupes into account
+ * @property  {number} numberOfPepts
+ *   Number of peptides this annotiation occurs at least once in (taking dupes into account)
+ * @property  {number} value
+ *   weightedValue / sumWeightedValue
+ */
+
+/**
+ * Fetches inforamtaion of the list of
+ *
+ * @param  {string[]} originalPeptides The list of peptides to procces
+ * @param {MPAConfig} config The configuration of the search
+ * @return {MPAResult} The result of MPA analysis
+ */
+export async function process(originalPeptides, config) {
+    const preparedPeptides = preparePeptides(originalPeptides, config);
+    const peptideList = Array.from(preparedPeptides.keys());
+
 
     for (let i = 0; i < peptideList.length; i += BATCH_SIZE) {
         const data = JSON.stringify({
             peptides: peptideList.slice(i, i + BATCH_SIZE),
-            equate_il: il,
-            missed: missed,
+            equate_il: config.il,
+            missed: config.missed,
         });
         const lcaQuery = postJSON(PEPT2LCA_URL, data);
         const faQuery = postJSON(PEPT2FA_URL, data);
@@ -65,17 +96,14 @@ export async function process(originalPeptides, il, dupes, missed) {
  * cleaving, filtering, equating IL and finally generating a frequency table
  *
  * @param  {string[]} originalPeptides A list of peptides
- * @param  {boolean} il should we equate I and L
- * @param  {boolean} dupes should we filter duplicates?
- * @param  {boolean} missed will we perform advancedMissedCleavageHandling
- * @return {Map.<string, number>} A frequency table of the cleaned up
- *   peptides
+ * @param {MPAConfig} config The configuration of the search
+ * @return {Map.<string, number>} A frequency table of the cleaned up peptides
  */
-function preparePeptides(originalPeptides, il, dupes, missed) {
-    let peptides = cleavePeptides(originalPeptides, missed);
+function preparePeptides(originalPeptides, config) {
+    let peptides = cleavePeptides(originalPeptides, config.missed);
     peptides = filterShortPeptides(peptides);
-    peptides = equateIL(peptides, il);
-    peptides = indexPeptides(peptides, dupes);
+    peptides = equateIL(peptides, config.il);
+    peptides = indexPeptides(peptides, config.dupes);
     return peptides;
 }
 
@@ -85,9 +113,8 @@ function preparePeptides(originalPeptides, il, dupes, missed) {
  *
  * @param  {string[]} peptides A list of peptides
  * @param  {boolean} dupes Filter duplicates
- * @return {Map.<string, number>} A map containing the frequency of
- *   each peptide
- * @onlyhere
+ * @return {Map.<string, number>} A map containing the frequency of each peptide
+ * @access private
  */
 function indexPeptides(peptides, dupes) {
     const peptideMap = new Map();
@@ -108,8 +135,9 @@ function indexPeptides(peptides, dupes) {
  *
  * @param  {string[]} peptides The list of peptides
  * @param  {boolean} advancedMissedCleavageHandling Should we do
- *   advancedMissedCleavageHandling?
+ *      advancedMissedCleavageHandling?
  * @return {string[]} The list of cleaved peptides
+ * @access private
  */
 function cleavePeptides(peptides, advancedMissedCleavageHandling) {
     if (!advancedMissedCleavageHandling) {
@@ -126,7 +154,7 @@ function cleavePeptides(peptides, advancedMissedCleavageHandling) {
  *
  * @param  {string[]} peptides A list of peptides
  * @return {string[]} A filtered list of peptides
- * @onlyhere
+ * @access private
  */
 function filterShortPeptides(peptides) {
     return peptides.filter(p => p.length >= 5);
@@ -138,7 +166,7 @@ function filterShortPeptides(peptides) {
  * @param  {string[]} peptides An array of peptides in upper case
  * @param  {boolean}  equateIL Only makes the replacement if this is true
  * @return {string[]} The peptides where the replacements are made
- * @onlyhere
+ * @access private
  */
 function equateIL(peptides, equateIL) {
     if (equateIL) {
@@ -149,14 +177,18 @@ function equateIL(peptides, equateIL) {
 
 
 /**
-     * Fill `this.go` with a Map<string,GoInfo[]> per namespace. The values of the
-     * maps have an additional `value` field that indicated the weight of that
-     * GO number.
-     * @param {[Peptides]} processedPeptides
-     * @param {number} [percent=50] ignore data weighing less (to be removed)
-     * @param {string[]} [sequences=null] subset of sequences to take into account,
-     *                                    null to consider all
-     */
+ * Creates a `GOTerms` summary of the go terms avalible in the dataset.
+ * Optinally limited to a list of sequences and/or a tresshold of acceptance
+ *
+ * After using this function you should synchronise the contents of the GOTerms
+ * data as static inforamtin about these terms is not shared over threads.
+ * (see getGoData).
+ *
+ * @param {number} [percent=50] ignore data weighing less (to be removed)
+ * @param {string[]} [sequences=null] subset of sequences to take into account,
+ *                                    null to consider all
+ * @return {GOTerms} GoTerms summary
+ */
 export async function summarizeGo(percent = 50, sequences=null) {
     // Find used go term and fetch data about them
     let usedGoTerms = new Set();
@@ -181,14 +213,18 @@ export async function summarizeGo(percent = 50, sequences=null) {
 }
 
 /**
-     * Fill `this.ec` with a Map<string,EcInfo[]>. The values of the map
-     * have an additional `value` field that indicated the weight of that
-     * EC number.
-     * @param {[Peptides]} processedPeptides
-     * @param {number} [percent=50] ignore data weighing less (to be removed)
-     * @param {string[]} [sequences=null] subset of sequences to take into account,
-     *                                    null to consider all
-     */
+ * Creates a `ECNumbers` summary of the go terms avalible in the dataset.
+ * Optinally limited to a list of sequences and/or a tresshold of acceptance
+ *
+ * After using this function you should synchronise the contents of the ECNumbers
+ * data as static inforamtin about these terms is not shared over threads.
+ * (see getEcNames).
+ *
+ * @param {number} [percent=50] ignore data weighing less (to be removed)
+ * @param {string[]} [sequences=null] subset of sequences to take into account,
+ *                                    null to consider all
+ * @return {ECNumbers} ECNumbers summary
+ */
 export async function summarizeEc(percent = 50, sequences=null) {
     // Filter FA' staring with EC (+ remove "EC:")
     const dataExtractor = pept =>
@@ -205,36 +241,37 @@ export async function summarizeEc(percent = 50, sequences=null) {
 }
 
 /**
-     * Create a mapping of functional analysis codes to a weight by aggregating
-     * the counts of all peptides that have functional analysis tags.
-     *
-     * @example this.summarizeFa(pept => pept.fa.ec || [])
-     * @param {[Peptides]} processedPeptides
-     * @param {function(pept:Peptide): {code, value}} extract
-     *            function extracting the FAInfo form a peptide
-     * @param {function(pept:Peptide): int} countExtractor
-     *            function extracting the the number of annotated (full)peptides
-     *            form a peptide
-     * @param {number} cutoff  data with strictly lower weight is ignored
-     *                         value should be given as percentage in [0,100]
-     * @return {Map<string,number>} map  map to store the results in
-     * @param {Iterable.<string>} [sequences=null] subset of sequences to take into account,
-     *                                    null to consider all
-     * @todo  remove the cutoff
-     */
+ * Create a mapping of functional analysis codes to a weight by aggregating
+ * the counts of all peptides that have functional analysis tags.
+ *
+ * @param {function(pept:Peptide): {code, value}} extract
+ *            function extracting the FAInfo form a peptide
+ * @param {function(pept:Peptide): int} countExtractor
+ *            function extracting the the number of annotated (full)peptides
+ *            form a peptide
+ * @param {number} cutoff  data with strictly lower weight is ignored
+ *                         value should be given as percentage in [0,100]
+ * @param {Iterable.<string>} [sequences=null] subset of sequences to take into account,
+ *                                    null to consider all
+ * @return {MPAFAResult[]} an array of MPAFAResult to be stored
+ * @todo  remove the cutoff
+ */
 function summarizeFa(extract, countExtractor, cutoff = 50, sequences=null) {
     let iteratableOfSequences = sequences || processedPeptides.keys();
+
     const map = new Map();
     const fraction = cutoff / 100;
-    let numAnnotations = 0;
+    let sumWeight = 0;
+
     for (let sequence of iteratableOfSequences) {
         const pept = processedPeptides.get(sequence);
-        const divisor = countExtractor(pept);
-        if (divisor !== 0) {
-            const counts = extract(pept);
-            for (const {code, value} of counts) {
-                const weight = value / divisor;
-                if (weight < fraction) continue; // skip if insignificant TODO: remove
+        const totalNumAnnotations = countExtractor(pept);
+
+        if (totalNumAnnotations > 0) {
+            for (const {code, value} of extract(pept)) {
+                const weight = value / totalNumAnnotations;
+                if (weight < fraction) continue; // skip if insignificant weight TODO: remove
+
                 const count = map.get(code) || [0, 0, 0, 0];
                 const scaledWeight = weight * (pept.count);
                 map.set(code, [
@@ -243,7 +280,7 @@ function summarizeFa(extract, countExtractor, cutoff = 50, sequences=null) {
                     count[2] + value * pept.count,
                     count[3] + pept.count,
                 ]);
-                numAnnotations += scaledWeight;
+                sumWeight += scaledWeight;
             }
         }
     }
@@ -254,28 +291,30 @@ function summarizeFa(extract, countExtractor, cutoff = 50, sequences=null) {
         absoluteCountFiltered: x[1][1],
         absoluteCount: x[1][2],
         numberOfPepts: x[1][3],
-        value: x[1][0] / numAnnotations,
+        value: x[1][0] / sumWeight,
     }));
 }
 
 /**
- * dsd
+ * @return {object} GOTerms.goData to perform `GOTerms.ingestData`
  */
 export function getGoData() {
     return GOTerms.goData;
 }
 
+/**
+ * @return {object} ECNumbers.ecNames to perform `ECNumbers.ingestNames`
+ */
 export function getEcNames() {
     return ECNumbers.ecNames;
 }
 
-let progress = 0;
 
 /**
- * dsdd
- * @param {*} value
+ * Send out a message to the calling procces that that the progress
+ * has changed
+ * @param {number} value progrress in [0,1]
  */
 function setProgress(value) {
-    progress = value;
     self.postMessage({type: "progress", value: value});
 }
