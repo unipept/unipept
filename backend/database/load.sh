013b87a0d7e3cd35dab3a4ce2b57a1d59b01779b
beardhatcode <mail.garonn@gmail.com>
2018-05-09 16:53:27 +0200

    feat(backend): load from stdin

--- backend/database/load.sh
+++ backend/database/load.sh
@@ -2,6 +2,8 @@
 # extracts all .tsv.gz files, imports them into the unipept database
 # and removes the files
 
+db=unipept
+
 function print {
     echo $(date -u) $1
 }
@@ -10,9 +12,8 @@ function print {
 for file in *.tsv.gz
 do
     print $file
-    gunzip $file
-    file=`echo $file | sed "s/.gz//"`
-    print $file
-    mysqlimport -u unipept -punipept --local unipept $file
-    rm $file
+    tbl=`echo $file | sed "s/.tsv.gz//"`
+    echo "zcatting - LOAD DATA LOCAL INFILE '$file' INTO TABLE $tbl"
+    zcat $file | mysql  --local-infile=1 -uunipept -punipept $db -e "LOAD DATA LOCAL INFILE '/dev/stdin' INTO TABLE $tbl;SHOW WARNINGS" 2>&1
 done
+print "done"
