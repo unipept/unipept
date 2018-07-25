0fd0045c6fba69a18f11387347b4e03bac0abbb0
beardhatcode <mail.garonn@gmail.com>
2018-07-25 15:32:45 +0200

    feat(proteoms): use batches for the API

--- backend/src/main/java/org/unipept/tools/MakeProteoms.java
+++ backend/src/main/java/org/unipept/tools/MakeProteoms.java
@@ -4,12 +4,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 
 import org.unipept.storage.CSV;
+import org.unipept.storage.CSV.Writer;
 
 /**
  * Get proteom data from ebi
@@ -24,30 +29,49 @@ public class MakeProteoms {
     CSV.Reader reader = new CSV.Reader(args[0]);
     CSV.Writer writer = new CSV.Writer(args[1]);
     String[] row = null;
+
+    Map<String, Long> proteomIdMap = new HashMap<>();
+
     while ((row = reader.read()) != null) {
-      getProteom(Long.parseLong(row[0], 10), row[1], writer);
+      proteomIdMap.put(row[1], Long.parseLong(row[0]));
+      if (proteomIdMap.size() >= 95) {
+        performLookup(proteomIdMap, writer);
+        proteomIdMap.clear();
+      }
     }
+    performLookup(proteomIdMap, writer);
     writer.close();
   }
 
-  private static void getProteom(long id, String accession, CSV.Writer writer) throws IOException {
-    try (InputStream is = (new URL("https://www.ebi.ac.uk/proteins/api/proteomes/" + accession)).openStream();
-        JsonReader rdr = Json.createReader(is)) {
+  private static void performLookup(Map<String, Long> proteomIdMap, Writer writer) throws IOException {
+    if (!proteomIdMap.isEmpty()) {
+      String requested = proteomIdMap.keySet().stream().collect(Collectors.joining("%2C"/* comma */));
+      try (
+        InputStream is = (new URL("https://www.ebi.ac.uk/proteins/api/proteomes?offset=0&size=-1&upid=" + requested)).openStream(); 
+        JsonReader rdr = Json.createReader(is)
+      ) {
 
-      JsonObject obj = rdr.readObject();
+        JsonArray results = rdr.readArray();
 
-      if ("viruses".equals(obj.getString("superregnum", null))) {
-        return; // Skip viruses
+        for (JsonObject obj : results.getValuesAs(JsonObject.class)) {
+          String accession = obj.getString("upid", "");
+          long origId = proteomIdMap.get(accession);
+          parseAndWrite(obj, origId, accession, writer);
+        }
+
+      } catch (FileNotFoundException e) {
+        // error without stopping
+        e.printStackTrace();
       }
-
-      parseAndWrite(obj, id, accession, writer);
-
-    } catch (FileNotFoundException e) {
-      System.err.println("Not found:" + accession);
     }
   }
 
   private static void parseAndWrite(JsonObject obj, long id, String accession, CSV.Writer writer) throws IOException {
+
+    if ("viruses".equals(obj.getString("superregnum", null))) {
+      return; // Skip viruses
+    }
+
     JsonArray dbRfs = obj.getJsonArray("dbReference");
     String assembly = null;
     if (dbRfs != null) {
