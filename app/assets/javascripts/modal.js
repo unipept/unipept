0ccb4a1cf4110fadadc11a13f400408ffdf3e525
beardhatcode <mail.garonn@gmail.com>
2018-07-27 13:24:36 +0200

    feat(modal): add an × to close the modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -8,11 +8,12 @@ function showInfoModal(title, content, options = {wide: false}) {
     if (options.wide) {
         $("#info-modal .modal-dialog").addClass("modal-lg");
     }
+    const $title = $("#info-modal .modal-title");
     const $body = $("#info-modal .modal-body");
     const $content = $("<p></p>").append(content);
+    $title.empty().append(title);
     $body
         .empty()
-        .append("<h2>" + title + "</h2>")
         .append($content);
     $("#info-modal").modal("show");
     return $("#info-modal");
