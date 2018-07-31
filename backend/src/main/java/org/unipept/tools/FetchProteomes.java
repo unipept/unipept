643cca2545286ac960e5921430c04208c380ecdb
beardhatcode <mail.garonn@gmail.com>
2018-07-31 14:32:48 +0200

    doc(proteomes): document

--- backend/src/main/java/org/unipept/tools/FetchProteomes.java
+++ backend/src/main/java/org/unipept/tools/FetchProteomes.java
@@ -17,21 +17,31 @@ import org.unipept.storage.CSV;
 import org.unipept.storage.CSV.Writer;
 
 /**
- * Get proteomes data from ebi
+ * Get proteomes data from the ebi API 
+ * https://www.ebi.ac.uk/proteins/api/doc/#!/proteomes/search
+ * 
+ * We only keep "Representative" proteomes and mark "Reference" proteomes.
+ * 
  */
 public class FetchProteomes {
 
+  /**
+   * Reads lines lines of the form (internal_proteom_id \t upid).
+   * Send then in batched of 95 to the server (max 100). When the results return,
+   * Use a Map to look up the origional internal id. And write out statistics.
+   */
   public static void main(String[] args) throws IOException {
     if (args.length != 2) {
-      throw new RuntimeException("Please provide the parameters.");
+      throw new RuntimeException("Please provide 2 parameters. (inputfile outputfile)");
     }
 
     CSV.Reader reader = new CSV.Reader(args[0]);
     CSV.Writer writer = new CSV.Writer(args[1]);
+
+    // take in lines till we have 95 proteomes (max is 100)
+
     String[] row = null;
-
     Map<String, Long> proteomIdMap = new HashMap<>();
-
     while ((row = reader.read()) != null) {
       proteomIdMap.put(row[1], Long.parseLong(row[0]));
       if (proteomIdMap.size() >= 95) {
@@ -43,6 +53,9 @@ public class FetchProteomes {
     writer.close();
   }
 
+  /**
+   * Look up a batch of upid's (the keys of the input map)
+   */
   private static void performLookup(Map<String, Long> proteomIdMap, Writer writer) throws IOException {
     if (!proteomIdMap.isEmpty()) {
       String requested = proteomIdMap.keySet().stream().collect(Collectors.joining("%2C"/* comma */));
@@ -67,6 +80,11 @@ public class FetchProteomes {
     }
   }
 
+  /**
+   * Write out Representative Proteomes as
+   * internal-id, accession, name, strain, isReferenceProt, assembly, fullName
+   * (with \t as sepparator)
+   */
   private static void parseAndWrite(JsonObject obj, long id, String accession, CSV.Writer writer) throws IOException {
     // Only Representative proteomes
     if (obj.getBoolean("isRepresentativeProteome")) {
