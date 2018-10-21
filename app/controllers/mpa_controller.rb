2da8f213ee7aac19ff9e7d15255c43caa21a6b4a
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-10-21 14:01:01 +0200

    Very early version of primitive dataset comparison

--- app/controllers/mpa_controller.rb
+++ app/controllers/mpa_controller.rb
@@ -5,7 +5,13 @@ class MpaController < ApplicationController
   def analyze
     @header_class = 'MPA'
     @title = 'Metaproteomics analysis result'
-    @selected_datasets = params[:data]
+    @selected_datasets = JSON.parse!(params[:data])
+    # Sanitize data here before embedding it in the HTML-template to avoid XSS
+    @selected_datasets.map do |value|
+      ActionController::Base.helpers.sanitize(value)
+    end
+
+
   end
 
   def pept2data
