8fc65e17d70b09aa11e0409b7415039cfb55e6ab
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-11-14 19:52:55 +0100

    Abstracting textarea component

---
+++ app/javascript/vue/components/material-textarea.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="form-group" v-bind:class="[valid ? '' : 'has-error']">
+        <label class="control-label" :for="name">{{ label }}</label>
+        <textarea v-model="content" class="form-control" :name="name" :id="name" :rows="rows" :spellcheck="spellcheck" :autofocus="autofocus" @input="validate"></textarea>
+        <span class="help-block" v-if="!valid">{{ error }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "material-textarea",
+        data: function() {
+            return {
+                content: '',
+                valid: true
+            }
+        },
+        props: {
+            name: {
+                type: String
+            },
+            label: {
+                type: String
+            },
+            rows: {
+                type: Number,
+                default: 5
+            },
+            spellcheck: {
+                type: Boolean,
+                default: true
+            },
+            autofocus: {
+                type: Boolean,
+                default: false
+            },
+            validation: {
+                type: Function(String),
+                default: function(value) {
+                    return true;
+                }
+            },
+            validationError: {
+                type: String,
+                default: ""
+            }
+        },
+        methods: {
+            validate: function() {
+                this.valid = this.validation(this.content);
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/javascript/vue/mpa/peptide-form.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <material-textarea name="qs" label="Peptide list" :rows="7" :spellcheck="false"></material-textarea>
+
+        <div class="form-group">
+            <label for="search_name">Name this dataset</label>
+            <input type="text" name="search_name" id="search_name" placeholder="e.g. Sample B5" class="form-control js-has-focus-tooltip input-item" title="" data-original-title="This name will be shown on the results page. Handy if you have many open tabs.">
+        </div>
+        <div class="checkbox">
+            <label title="" class="js-has-hover-tooltip" for="save_dataset" data-original-title="Store dataset in local storage and reuse it later on"><input type="checkbox" name="save_dataset" id="save_dataset" value="1" class="input-item" checked="checked"> Store dataset in browser's local storage</label>
+        </div>
+    </div>
+</template>
+
+<script>
+    import MaterialTextarea from "../components/material-textarea";
+    export default {
+        name: "peptide-form",
+        components: {MaterialTextarea}
+    };
+</script>
+
+<style scoped>
+
+</style>
