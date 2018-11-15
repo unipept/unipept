cc4316892478d73406688339c4ce576671bc3720
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-11-15 11:03:28 +0100

    More default Vue components

---
+++ app/javascript/vue/mpa/dataset-form.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <validated-textarea v-model="peptides" name="qs" label="Peptide list" :rows="7" :spellcheck="false" :validation="validate" validation-error="At least one peptide is required"></validated-textarea>
+        <validated-textfield v-model="name" name="search_name" label="Name this dataset" :validation="validate" validation-error="Name is required when the dataset is set to be saved." placeholder="e.g. Sample B5" tooltip="This name will be shown on the results page. Handy if you have many open tabs."></validated-textfield>
+        <checkbox v-model="checked" name="save_dataset" label="Store dataset in browser's local storage" tooltip="Store dataset in local storage and reuse it later on"></checkbox>
+    </div>
+</template>
+
+<script>
+    import ValidatedTextarea from "../components/validated-textarea";
+    import ValidatedTextfield from "../components/validated-textfield"
+    import Checkbox from "../components/checkbox";
+
+    export default {
+        name: "dataset-form",
+        components: {Checkbox, ValidatedTextarea, ValidatedTextfield},
+        data:  function() {
+            return {
+                peptides: "",
+                name: "",
+                checked: true
+            }
+        },
+        methods: {
+            validate: function(content) {
+                return content !== '';
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
