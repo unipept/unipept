a11e0bf926144038c1b6edf902a7a5b838a2a776
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-11-15 09:55:27 +0100

    Input field is now also a Vue component

---
+++ app/javascript/vue/components/validated-textfield.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="form-group" v-bind:class="[valid ? '' : 'has-error']">
+        <label class="control-label" :for="name">{{ label }}</label>
+        <input type="text" v-model="content" class="form-control js-has-focus-tooltip" :data-original-title="tooltip" :name="name" :id="name" :autofocus="autofocus" :placeholder="placeholder" @input="validate"/>
+        <span class="help-block" v-if="!valid">{{ validationError }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "validated-textfield",
+        data: function() {
+            return {
+                content: '',
+                valid: true
+            }
+        },
+        props: {
+            name: {
+                type: String
+            },
+            label: {
+                type: String
+            },
+            autofocus: {
+                type: Boolean,
+                default: false
+            },
+            placeholder: {
+                type: String,
+                deafult: ""
+            },
+            tooltip: {
+                type: String,
+                default: ""
+            },
+            validation: {
+                default: function(value) {
+                    return true;
+                }
+            },
+            validationError: {
+                type: String,
+                default: ""
+            }
+        },
+        methods: {
+            validate: function() {
+                this.valid = this.validation(this.content);
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
--- app/javascript/vue/mpa/peptide-form.vue
+++ app/javascript/vue/mpa/peptide-form.vue
@@ -1,11 +1,7 @@
 <template>
     <div>
-        <material-textarea name="qs" label="Peptide list" :rows="7" :spellcheck="false" :validation="validate" validation-error="At least one peptide is required"></material-textarea>
-
-        <div class="form-group">
-            <label for="search_name">Name this dataset</label>
-            <input type="text" name="search_name" id="search_name" placeholder="e.g. Sample B5" class="form-control js-has-focus-tooltip input-item" title="" data-original-title="This name will be shown on the results page. Handy if you have many open tabs.">
-        </div>
+        <validated-textarea name="qs" label="Peptide list" :rows="7" :spellcheck="false" :validation="validate" validation-error="At least one peptide is required"></validated-textarea>
+        <validated-textfield name="search_name" label="Name this dataset" :validation="validate" validation-error="Name is required when the dataset is set to be saved." placeholder="e.g. Sample B5" tooltip="This name will be shown on the results page. Handy if you have many open tabs."></validated-textfield>
         <div class="checkbox">
             <label title="" class="js-has-hover-tooltip" for="save_dataset" data-original-title="Store dataset in local storage and reuse it later on"><input type="checkbox" name="save_dataset" id="save_dataset" value="1" class="input-item" checked="checked"> Store dataset in browser's local storage</label>
         </div>
@@ -13,10 +9,12 @@
 </template>
 
 <script>
-    import MaterialTextarea from "../components/material-textarea";
+    import ValidatedTextarea from "../components/validated-textarea";
+    import ValidatedTextfield from "../components/validated-textfield"
+
     export default {
         name: "peptide-form",
-        components: {MaterialTextarea},
+        components: {ValidatedTextarea, ValidatedTextfield},
         methods: {
             validate: function(content) {
                 return content !== '';
