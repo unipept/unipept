31128b94dfa41b6480a3bae8478a2f77962d0049
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-11-15 10:00:03 +0100

    Checkbox component implemented

---
+++ app/javascript/vue/components/checkbox.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="checkbox">
+        <label title="" class="js-has-hover-tooltip" :for="name" :data-original-title="tooltip">
+            <input type="checkbox" :name="name" :id="name" checked="checked"> {{ label }}
+        </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "checkbox",
+        data: function() {
+            return {
+                content: '',
+                valid: true
+            }
+        },
+        props: {
+            name: {
+                type: String
+            },
+            label: {
+                type: String
+            },
+            autofocus: {
+                type: Boolean,
+                default: false
+            },
+            placeholder: {
+                type: String,
+                deafult: ""
+            },
+            tooltip: {
+                type: String,
+                default: ""
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
--- app/javascript/vue/mpa/peptide-form.vue
+++ app/javascript/vue/mpa/peptide-form.vue
@@ -2,19 +2,18 @@
     <div>
         <validated-textarea name="qs" label="Peptide list" :rows="7" :spellcheck="false" :validation="validate" validation-error="At least one peptide is required"></validated-textarea>
         <validated-textfield name="search_name" label="Name this dataset" :validation="validate" validation-error="Name is required when the dataset is set to be saved." placeholder="e.g. Sample B5" tooltip="This name will be shown on the results page. Handy if you have many open tabs."></validated-textfield>
-        <div class="checkbox">
-            <label title="" class="js-has-hover-tooltip" for="save_dataset" data-original-title="Store dataset in local storage and reuse it later on"><input type="checkbox" name="save_dataset" id="save_dataset" value="1" class="input-item" checked="checked"> Store dataset in browser's local storage</label>
-        </div>
+        <checkbox name="save_dataset" label="Store dataset in browser's local storage" tooltip="Store dataset in local storage and reuse it later on"></checkbox>
     </div>
 </template>
 
 <script>
     import ValidatedTextarea from "../components/validated-textarea";
     import ValidatedTextfield from "../components/validated-textfield"
+    import Checkbox from "../components/checkbox";
 
     export default {
         name: "peptide-form",
-        components: {ValidatedTextarea, ValidatedTextfield},
+        components: {Checkbox, ValidatedTextarea, ValidatedTextfield},
         methods: {
             validate: function(content) {
                 return content !== '';
