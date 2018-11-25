705c1c1a90754a0c8f4d94beea47ac8d1cc71ed8
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-11-25 14:21:37 +0100

    TreemapVisualization

--- app/assets/javascripts/mpa/ui/visualizations/treemap-visualization.vue
+++ app/assets/javascripts/mpa/ui/visualizations/treemap-visualization.vue
@@ -7,34 +7,37 @@
     import Vue from "vue";
     import Component from "vue-class-component";
     import {Prop, Watch} from "vue-property-decorator";
-    import {Tree} from "../../tree";
     import NewPeptideContainer from "../../NewPeptideContainer";
+    import MpaAnalysisManager from "../../MpaAnalysisManager";
+    import {Tree} from "../../tree";
     import {tooltipContent} from "./VisualizationHelper";
 
     @Component
-    export default class SunburstVisualization extends Vue {
+    export default class TreemapVisualization extends Vue {
         @Prop({default: null}) dataset: NewPeptideContainer | null;
 
         mounted() {
-            this.initTree();
+            this.initTreeMap();
         }
 
         @Watch('dataset') onDatasetChanged() {
-            this.initTree();
+            this.initTreeMap();
         }
 
-        private initTree() {
+        private initTreeMap() {
             if (this.dataset != null && this.dataset.getDataset() != null) {
                 let tree: Tree = this.dataset.getDataset().getTree();
                 const data = JSON.stringify(tree.getRoot());
 
-                $(this.$el).sunburst(JSON.parse(data), {
-                    width: 740,
-                    height: 740,
-                    radius: 740 / 2,
+                $(this.$el).treemap(JSON.parse(data), {
+                    width: 916,
+                    height: 600,
+                    levels: 28,
+                    getBreadcrumbTooltip: d => d.rank,
                     getTooltip: tooltipContent,
-                    getTitleText: d => `${d.name} (${d.rank})`
-                })
+                    getLabel: d => `${d.name} (${d.data.self_count}/${d.data.count})`,
+                    getLevel: d => MpaAnalysisManager.RANKS.indexOf(d.rank)
+                });
             }
         }
     }
