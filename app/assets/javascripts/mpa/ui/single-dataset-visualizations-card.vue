e07e4ddbd027929090adf75ae13bb9220587bae1
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-11-26 11:15:48 +0100

    Fixed placeholders for visualizations

--- app/assets/javascripts/mpa/ui/single-dataset-visualizations-card.vue
+++ app/assets/javascripts/mpa/ui/single-dataset-visualizations-card.vue
@@ -1,45 +1,66 @@
 <template>
     <card-nav>
         <tab label="Sunburst" :active="true" id="sunburstWrapper">
-            <h2 class="ghead">
-                <span class="dir">
-                    <a class="btn btn-xs btn-default btn-animate" id="sunburst-reset" title="reset visualisation"><span class="glyphicon glyphicon-repeat spin"></span></a>
-                </span>
-                <span class="dir">
-                    <div class="btn-group" id="colorswap">
-                        <a class="btn btn-xs btn-default dropdown-toggle" data-toggle="dropdown" id="colorswap-button"><span class="glyphicon glyphicon-cog"></span></a>
-                        <ul class="dropdown-menu dropdown-menu-right dropdown-menu-form">
-                            <li title="Enabling this will assign fixed colors to taxa making it easier to compare samples.">
-                                <div class="checkbox"><label class="checkbox"><input type="checkbox" id="colorswap-checkbox">Use fixed colors</label></div>
-                            </li>
-                        </ul>
-                    </div>
-                </span>
-                <span class="dir text">Click a slice to zoom in and the center node to zoom out</span>
-            </h2>
-            <sunburst-visualization :dataset="$store.getters.activeDataset"></sunburst-visualization>
+            <div v-if="this.$store.getters.activeDataset">
+                <h2 class="ghead">
+                    <span class="dir">
+                        <a class="btn btn-xs btn-default btn-animate" id="sunburst-reset" title="reset visualisation"><span class="glyphicon glyphicon-repeat spin"></span></a>
+                    </span>
+                    <span class="dir">
+                        <div class="btn-group" id="colorswap">
+                            <a class="btn btn-xs btn-default dropdown-toggle" data-toggle="dropdown" id="colorswap-button"><span class="glyphicon glyphicon-cog"></span></a>
+                            <ul class="dropdown-menu dropdown-menu-right dropdown-menu-form">
+                                <li title="Enabling this will assign fixed colors to taxa making it easier to compare samples.">
+                                    <div class="checkbox"><label class="checkbox"><input type="checkbox" id="colorswap-checkbox">Use fixed colors</label></div>
+                                </li>
+                            </ul>
+                        </div>
+                    </span>
+                    <span class="dir text">Click a slice to zoom in and the center node to zoom out</span>
+                </h2>
+                <sunburst-visualization :dataset="$store.getters.activeDataset"></sunburst-visualization>
+            </div>
+            <div v-else>
+                <div class="mpa-waiting">
+                    <img alt="Please wait while we are preparing your data..." class="mpa-placeholder" src="/images/mpa/placeholder_sunburst.svg">
+                </div>
+            </div>
         </tab>
         <tab label="Treemap" id="treemapWrapper">
-            <h2 class="ghead">
-                <span class="dir">
-                    <a class="btn btn-xs btn-default btn-animate" id="treemap-reset" title="reset visualisation">
-                        <span class="glyphicon glyphicon-repeat spin"></span>
-                    </a>
-                </span>
-                <span class="dir text">Click a square to zoom in and right click to zoom out</span>
-            </h2>
-            <treemap-visualization :dataset="$store.getters.activeDataset"></treemap-visualization>
+            <div v-if="this.$store.getters.activeDataset">
+                <h2 class="ghead">
+                    <span class="dir">
+                        <a class="btn btn-xs btn-default btn-animate" id="treemap-reset" title="reset visualisation">
+                            <span class="glyphicon glyphicon-repeat spin"></span>
+                        </a>
+                    </span>
+                    <span class="dir text">Click a square to zoom in and right click to zoom out</span>
+                </h2>
+                <treemap-visualization :dataset="$store.getters.activeDataset"></treemap-visualization>
+            </div>
+            <div v-else>
+                <div class="mpa-waiting">
+                    <img alt="Please wait while we are preparing your data..." class="mpa-placeholder" src="/images/mpa/placeholder_treemap.svg">
+                </div>
+            </div>
         </tab>
         <tab label="Treeview" id="treeviewWrapper">
-            <h2 class="ghead">
-                <span class="dir">
-                    <a class="btn btn-xs btn-default btn-animate" id="treeview-reset" title="reset visualisation">
-                        <span class="glyphicon glyphicon-repeat spin"></span>
-                    </a>
-                </span>
-                <span class="dir text">Scroll to zoom, drag to pan, click a node to expand, right click a node to set as root</span>
-            </h2>
-            <treeview-visualization :dataset="$store.getters.activeDataset"></treeview-visualization>
+            <div v-if="this.$store.getters.activeDataset">
+                <h2 class="ghead">
+                    <span class="dir">
+                        <a class="btn btn-xs btn-default btn-animate" id="treeview-reset" title="reset visualisation">
+                            <span class="glyphicon glyphicon-repeat spin"></span>
+                        </a>
+                    </span>
+                    <span class="dir text">Scroll to zoom, drag to pan, click a node to expand, right click a node to set as root</span>
+                </h2>
+                <treeview-visualization :dataset="$store.getters.activeDataset"></treeview-visualization>
+            </div>
+            <div v-else>
+                <div class="mpa-waiting">
+                    <img alt="Please wait while we are preparing your data..." class="mpa-placeholder" src="/images/mpa/placeholder_treeview.svg">
+                </div>
+            </div>
         </tab>
         <tab label="Hierarchical outline" class="card-supporting-text">
             <hierarchical-outline-visualization :dataset="$store.getters.activeDataset"></hierarchical-outline-visualization>
