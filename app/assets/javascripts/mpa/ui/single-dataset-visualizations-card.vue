24a18be5ecf68f618a80e07aad2f28deac3e5c1e
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-11-26 10:24:14 +0100

    Fixed input-group form-group CSS for ValidatedTextfield

--- app/assets/javascripts/mpa/ui/single-dataset-visualizations-card.vue
+++ app/assets/javascripts/mpa/ui/single-dataset-visualizations-card.vue
@@ -1,6 +1,22 @@
 <template>
     <card-nav>
         <tab label="Sunburst" :active="true">
+            <h2 class="ghead">
+                <span class="dir">
+                    <a class="btn btn-xs btn-default btn-animate" id="sunburst-reset" title="reset visualisation"><span class="glyphicon glyphicon-repeat spin"></span></a>
+                </span>
+                <span class="dir">
+                    <div class="btn-group" id="colorswap">
+                        <a class="btn btn-xs btn-default dropdown-toggle" data-toggle="dropdown" id="colorswap-button"><span class="glyphicon glyphicon-cog"></span></a>
+                        <ul class="dropdown-menu dropdown-menu-right dropdown-menu-form">
+                            <li title="Enabling this will assign fixed colors to taxa making it easier to compare samples.">
+                                <div class="checkbox"><label class="checkbox"><input type="checkbox" id="colorswap-checkbox">Use fixed colors</label></div>
+                            </li>
+                        </ul>
+                    </div>
+                </span>
+                <span class="dir text">Click a slice to zoom in and the center node to zoom out</span>
+            </h2>
             <sunburst-visualization :dataset="$store.getters.activeDataset"></sunburst-visualization>
         </tab>
         <tab label="Treemap">
