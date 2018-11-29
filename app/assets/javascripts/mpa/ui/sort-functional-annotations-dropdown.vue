dd378a2cd26787087d71efc3db743d56f30fe1c9
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-11-29 10:32:31 +0100

    Almost fully working Modal component

---
+++ app/assets/javascripts/mpa/ui/sort-functional-annotations-dropdown.vue
@@ -0,0 +1,18 @@
+<template>
+    $END$
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import Component from "vue-class-component";
+    import {Prop, Watch} from "vue-property-decorator";
+
+    @Component
+    export default class SortFunctionalAnnotationsDropdown extends Vue {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
