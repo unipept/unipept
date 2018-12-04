33b441cd42352dd313a7be1f2dcc107bdc791987
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-12-04 13:32:24 +0100

    Convert SingleDatasetVisualizationCard to new API

--- app/assets/javascripts/mpa/ui/single-dataset-visualizations-card.vue
+++ app/assets/javascripts/mpa/ui/single-dataset-visualizations-card.vue
@@ -1,36 +1,47 @@
 <template>
     <card-nav>
-        <template slot="interactiveTitle">
-            <div style="position: relative; top: 4px;">
-                <button id="zoom-btn" class="btn btn-default btn-xs btn-animate"><span class="glyphicon glyphicon-resize-full grow"></span> Enter full screen</button>
-                <button id="save-btn" class="btn btn-default btn-xs btn-animate"><span class="glyphicon glyphicon-download down"></span> Save as image</button>
+        <card-header class="card-title-interactive">
+            <ul class="nav nav-tabs">
+                <li v-for="tab in tabs" v-if="tab.constructor.name === 'Tab'" v-bind:class="{ active: tab.activated }" @click="changeActiveTab(tab)">
+                    <a>{{ tab.label }}</a>
+                </li>
+            </ul>
+            <div class="nav-right">
+                <div style="position: relative; top: 4px;">
+                    <button id="zoom-btn" class="btn btn-default btn-xs btn-animate"><span class="glyphicon glyphicon-resize-full grow"></span> Enter full screen</button>
+                    <button id="save-btn" class="btn btn-default btn-xs btn-animate"><span class="glyphicon glyphicon-download down"></span> Save as image</button>
+                </div>
             </div>
-        </template>
+        </card-header>
 
-        <tab label="Sunburst" :active="true" id="sunburstWrapper">
-            <sunburst-visualization v-if="$store.getters.activeDataset" :dataset="$store.getters.activeDataset"></sunburst-visualization>
-            <div v-else class="mpa-waiting">
-                <img :alt="waitString" class="mpa-placeholder" src="/images/mpa/placeholder_sunburst.svg">
+        <card-body>
+            <div class="tab-content">
+                <tab label="Sunburst" :active="true" id="sunburstWrapper">
+                    <sunburst-visualization v-if="$store.getters.activeDataset" :dataset="$store.getters.activeDataset"></sunburst-visualization>
+                    <div v-else class="mpa-waiting">
+                        <img :alt="waitString" class="mpa-placeholder" src="/images/mpa/placeholder_sunburst.svg">
+                    </div>
+                </tab>
+                <tab label="Treemap" id="treemapWrapper">
+                    <treemap-visualization v-if="$store.getters.activeDataset" :dataset="$store.getters.activeDataset"></treemap-visualization>
+                    <div v-else class="mpa-waiting">
+                        <img :alt="waitString" class="mpa-placeholder" src="/images/mpa/placeholder_treemap.svg">
+                    </div>
+                </tab>
+                <tab label="Treeview" id="treeviewWrapper">
+                    <treeview-visualization v-if="$store.getters.activeDataset" :dataset="$store.getters.activeDataset"></treeview-visualization>
+                    <div v-else class="mpa-waiting">
+                        <img :alt="waitString" class="mpa-placeholder" src="/images/mpa/placeholder_treeview.svg">
+                    </div>
+                </tab>
+                <tab label="Hierarchical outline" class="card-supporting-text">
+                    <hierarchical-outline-visualization v-if="$store.getters.activeDataset" :dataset="$store.getters.activeDataset"></hierarchical-outline-visualization>
+                    <div v-else>
+                        {{ waitString }}
+                    </div>
+                </tab>
             </div>
-        </tab>
-        <tab label="Treemap" id="treemapWrapper">
-            <treemap-visualization v-if="$store.getters.activeDataset" :dataset="$store.getters.activeDataset"></treemap-visualization>
-            <div v-else class="mpa-waiting">
-                <img :alt="waitString" class="mpa-placeholder" src="/images/mpa/placeholder_treemap.svg">
-            </div>
-        </tab>
-        <tab label="Treeview" id="treeviewWrapper">
-            <treeview-visualization v-if="$store.getters.activeDataset" :dataset="$store.getters.activeDataset"></treeview-visualization>
-            <div v-else class="mpa-waiting">
-                <img :alt="waitString" class="mpa-placeholder" src="/images/mpa/placeholder_treeview.svg">
-            </div>
-        </tab>
-        <tab label="Hierarchical outline" class="card-supporting-text">
-            <hierarchical-outline-visualization v-if="$store.getters.activeDataset" :dataset="$store.getters.activeDataset"></hierarchical-outline-visualization>
-            <div v-else>
-                {{ waitString }}
-            </div>
-        </tab>
+        </card-body>
     </card-nav>
 </template>
 
@@ -45,9 +56,15 @@
     import TreeviewVisualization from "./visualizations/treeview-visualization.vue";
     import SimpleButton from "../../components/button/simple-button.vue";
     import HierarchicalOutlineVisualization from "./visualizations/hierarchical-outline-visualization.vue";
+    import Tabs from "../../components/card/tabs.vue";
+    import CardHeader from "../../components/card/card-header.vue";
+    import CardBody from "../../components/card/card-body.vue";
 
     @Component({
         components: {
+            CardBody,
+            CardHeader,
+            Tabs,
             HierarchicalOutlineVisualization,
             SimpleButton, TreeviewVisualization, TreemapVisualization, SunburstVisualization, CardNav, Tab},
         computed: {
@@ -60,6 +77,11 @@
     })
     export default class SingleDatasetVisualizationsCard extends Vue {
         waitString = "Please wait while we are preparing your data...";
+        tabs: Tab[] = [];
+
+        mounted() {
+            this.tabs = this.$children[0].$children[1].$children as Tab[];
+        }
 
         @Watch('datasetsChosen') onDatasetsChosenChanged(newValue: boolean, oldValue: boolean) {
             if (newValue) {
