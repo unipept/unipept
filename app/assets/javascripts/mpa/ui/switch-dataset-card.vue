fcd04bf773d2841dd5b61876e1e1c203a5aa160f
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-12-04 13:18:55 +0100

    Convert SwitchDatasetCard to new API

--- app/assets/javascripts/mpa/ui/switch-dataset-card.vue
+++ app/assets/javascripts/mpa/ui/switch-dataset-card.vue
@@ -1,16 +1,21 @@
 <template>
     <card title="Metaproteomics Analysis" :interactive="true">
-        <template slot="card-title-action">
-            <span class="glyphicon glyphicon-plus" @click="addDataset()"></span>
-        </template>
-
-        <list class="switch-dataset-list" placeholder="Please add one or more datasets by clicking the plus button above... ">
-            <div class="list-item--two-lines" v-for="dataset of this.$store.getters.selectedDatasets">
+        <card-header class="card-title-interactive">
+            <card-title>
+                Metaproteomics Analysis
+            </card-title>
+            <div class="card-title-action">
+                <span class="glyphicon glyphicon-plus" @click="addDataset()"></span>
+            </div>
+        </card-header>
+        <card-body>
+            <list class="switch-dataset-list" placeholder="Please add one or more datasets by clicking the plus button above... ">
+                <div class="list-item--two-lines" v-for="dataset of this.$store.getters.selectedDatasets">
                 <span class="list-item-primary-action">
                     <input v-if="dataset.getProgress() === 1" v-model="activeDataset" :value="dataset" type="radio" class="input-item select-dataset-radio-button" style="width: 24px;" />
                     <determinate-circular-progress-indicator v-else :progress="dataset.getProgress()" :size="24"></determinate-circular-progress-indicator>
                 </span>
-                <span class="list-item-primary-content">
+                    <span class="list-item-primary-content">
                     {{ dataset.getName() }}
                     <span class="list-item-date">
                         {{ dataset.getDateFormatted() }}
@@ -19,12 +24,13 @@
                         {{ dataset.getAmountOfPeptides() }} peptides
                     </span>
                 </span>
-                <span class="list-item-secondary-action" @click="deselectDataset(dataset)">
+                    <span class="list-item-secondary-action" @click="deselectDataset(dataset)">
                     <span class="glyphicon glyphicon-trash">
                     </span>
                 </span>
-            </div>
-        </list>
+                </div>
+            </list>
+        </card-body>
     </card>
 </template>
 
@@ -36,9 +42,12 @@
     import List from "../../components/list/list.vue";
     import NewPeptideContainer from "../NewPeptideContainer";
     import DeterminateCircularProgressIndicator from "../../components/progress/determinate-circular-progress-indicator.vue";
+    import CardHeader from "../../components/card/card-header.vue";
+    import CardTitle from "../../components/card/card-title.vue";
+    import CardBody from "../../components/card/card-body.vue";
 
     @Component({
-        components: {DeterminateCircularProgressIndicator, Card, List},
+        components: {CardBody, CardTitle, CardHeader, DeterminateCircularProgressIndicator, Card, List},
         computed: {
             activeDataset: {
                 get() {
