07b00b700a29b02b2439b755dd0a0852a9bbbcf7
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-12-15 16:24:04 +0100

    Fixed selection bar in Vue version of code

--- app/javascript/packs/mpa_app.ts
+++ app/javascript/packs/mpa_app.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueClipboard from 'vue-clipboard2'
 import Mpa from '../../assets/javascripts/mpa/ui/mpa.vue'
 import {globalStore} from "../../assets/javascripts/mpa/ui/state/GlobalStore";
 import {analysisStore} from "../../assets/javascripts/mpa/ui/state/AnalysisStore";
 import "unipept-visualizations/dist/unipept-visualizations.es5.js";
 
+Vue.use(VueClipboard);
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
