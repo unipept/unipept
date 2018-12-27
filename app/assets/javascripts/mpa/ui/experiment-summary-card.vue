151f4164e254ea82239b0ea91b898754738fdcf9
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2018-12-27 15:33:20 +0100

    Fixed missing peptides modal

--- app/assets/javascripts/mpa/ui/experiment-summary-card.vue
+++ app/assets/javascripts/mpa/ui/experiment-summary-card.vue
@@ -21,30 +21,11 @@
                 <span v-if="!this.$store.getters.activeDataset">No dataset is selected... Wait for at least one dataset to be loaded or select one.</span>
                 <span v-else>
                     We managed to match {{ this.$store.getters.activeDataset.getDataset().getNumberOfMatchedPeptides() }} of your {{ this.$store.getters.activeDataset.getAmountOfPeptides() }} peptides.
-                    Unfortunately, <a style="cursor: pointer;" @click="notFoundPeptidesModalActive = true">{{ this.$store.getters.activeDataset.getDataset().getNumberOfSearchedForPeptides() - this.$store.getters.activeDataset.getDataset().getNumberOfMatchedPeptides() }}</a> peptides couldn't be found.
+                    Unfortunately, <a style="cursor: pointer;" @click="showNotFoundPeptidesModal">{{ this.$store.getters.activeDataset.getDataset().getNumberOfSearchedForPeptides() - this.$store.getters.activeDataset.getDataset().getNumberOfMatchedPeptides() }}</a> peptides couldn't be found.
                 </span>
 
             </card-body>
         </card>
-        <modal :active="notFoundPeptidesModalActive">
-            <template slot="header" v-if="this.$store.getters.activeDataset">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true" @click="notFoundPeptidesModalActive = false">×</button>
-                <h4 class="modal-title">{{ this.$store.getters.activeDataset.getDataset().getNumberOfSearchedForPeptides() - this.$store.getters.activeDataset.getDataset().getNumberOfMatchedPeptides() }} Missed peptides</h4>
-            </template>
-            <template slot="body" v-if="this.$store.getters.activeDataset">
-                <div class="card-supporting-text">
-                    <simple-button class="pull-right copy-button" type="default" label="Copy to clipboard" glyphicon="copy" tooltip="Copy list to clipboard" v-clipboard:copy="this.$store.getters.activeDataset.getDataset().getMissedPeptides().join('\n')"></simple-button>
-                    Sorry, we didn't manage to find {{ this.$store.getters.activeDataset.getDataset().getNumberOfSearchedForPeptides() - this.$store.getters.activeDataset.getDataset().getNumberOfMatchedPeptides() }} of your peptides. You can BLAST them by clicking the links or copy them by
-                    using the button on the right.
-                </div>
-                <ul>
-                    <li v-for="missedPeptide of this.$store.getters.activeDataset.getDataset().getMissedPeptides()">
-                        <a target="_blank" :href="`http://blast.ncbi.nlm.nih.gov/Blast.cgi?PAGE_TYPE=BlastSearch&SET_SAVED_SEARCH=on&USER_FORMAT_DEFAULTS=on&PAGE=Proteins&PROGRAM=blastp&QUERY=${missedPeptide}&GAPCOSTS=11%201&EQ_MENU=Enter%20organism%20name%20or%20id--completions%20will%20be%20suggested&DATABASE=nr&BLAST_PROGRAMS=blastp&MAX_NUM_SEQ=100&SHORT_QUERY_ADJUST=on&EXPECT=10&WORD_SIZE=3&MATRIX_NAME=BLOSUM62&COMPOSITION_BASED_STATISTICS=2&SHOW_OVERVIEW=on&SHOW_LINKOUT=on&ALIGNMENT_VIEW=Pairwise&MASK_CHAR=2&MASK_COLOR=1&GET_SEQUENCE=on&NEW_VIEW=on&NUM_OVERVIEW=100&DESCRIPTIONS=100&ALIGNMENTS=100&FORMAT_OBJECT=Alignment&FORMAT_TYPE=HTML&OLD_BLAST=false`">{{ missedPeptide }}</a>
-                        <span class="glyphicon glyphicon-share-alt"></span>
-                    </li>
-                </ul>
-            </template>
-        </modal>
     </div>
 </template>
 
@@ -61,6 +42,7 @@
     import SearchSettings from "../SearchSettings";
     import Modal from "../../components/modal/modal.vue";
     import {addCopy} from "../../utils";
+    import {showInfoModal} from "../../modal";
 
     @Component({
         components: {Modal, CardBody, CardTitle, CardHeader, SimpleButton, SearchSettingsForm, Card}
@@ -69,8 +51,6 @@
         equateIl: boolean = true;
         filterDuplicates: boolean = true;
         missingCleavage: boolean = false;
-
-        notFoundPeptidesModalActive: boolean = false;
 
         created() {
             this.equateIl = this.$store.getters.searchSettings.isEquateIl();
@@ -85,6 +65,34 @@
             for (let dataset of this.$store.getters.selectedDatasets) {
                 this.$store.dispatch('processDataset', dataset);
             }
+        }
+
+        showNotFoundPeptidesModal() {
+            let missedPeptidesHTML = '';
+            for (let missedPeptide of this.$store.getters.activeDataset.getDataset().getMissedPeptides()) {
+                missedPeptidesHTML += `
+                    <li>
+                        <a target="_blank" href="http://blast.ncbi.nlm.nih.gov/Blast.cgi?PAGE_TYPE=BlastSearch&SET_SAVED_SEARCH=on&USER_FORMAT_DEFAULTS=on&PAGE=Proteins&PROGRAM=blastp&QUERY=${missedPeptide}&GAPCOSTS=11%201&EQ_MENU=Enter%20organism%20name%20or%20id--completions%20will%20be%20suggested&DATABASE=nr&BLAST_PROGRAMS=blastp&MAX_NUM_SEQ=100&SHORT_QUERY_ADJUST=on&EXPECT=10&WORD_SIZE=3&MATRIX_NAME=BLOSUM62&COMPOSITION_BASED_STATISTICS=2&SHOW_OVERVIEW=on&SHOW_LINKOUT=on&ALIGNMENT_VIEW=Pairwise&MASK_CHAR=2&MASK_COLOR=1&GET_SEQUENCE=on&NEW_VIEW=on&NUM_OVERVIEW=100&DESCRIPTIONS=100&ALIGNMENTS=100&FORMAT_OBJECT=Alignment&FORMAT_TYPE=HTML&OLD_BLAST=false">${missedPeptide}</a>
+                        <span class="glyphicon glyphicon-share-alt"></span>
+                    </li>
+                `;
+            }
+
+            let missedPeptidesCount = this.$store.getters.activeDataset.getDataset().getNumberOfSearchedForPeptides() - this.$store.getters.activeDataset.getDataset().getNumberOfMatchedPeptides();
+
+            let modalContent = `
+                <div class="card-supporting-text">
+                    <button class="btn btn-default pull-right copy-button"><span class="glyphicon glyphicon-copy"></span></button>
+                    Sorry, we didn't manage to find ${missedPeptidesCount} of your peptides. You can BLAST them by clicking the links or copy them by using the button on the right.
+                </div>
+                <ul>
+                    ${missedPeptidesHTML}
+                </ul>
+            `;
+
+            let modal = showInfoModal(missedPeptidesCount + ' missed peptides', modalContent);
+            console.log(modal);
+            addCopy($(".copy-button")[0], () => this.$store.getters.activeDataset.getDataset().getMissedPeptides().join('\n'), "Copy list to clipboard", modal[0]);
         }
     }
 </script>
