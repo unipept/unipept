ce883c7668eae5f0ea5a7df7083ef5858378ec60
Rien Maertens <rien.maertens@posteo.be>
2019-04-03 10:13:39 +0200

    Add ec-index script and makefile target

---
+++ backend/create_kmer_ec.rb
@@ -0,0 +1,62 @@
+#!/bin/env ruby
+
+require 'set'
+
+K = ARGV[0].to_i
+EC_ID_FILE = ARGV[1]
+EC_CROSREF_FILE = ARGV[2]
+UNIPROT_FILE = ARGV[3]
+
+STDERR.puts "Creating {EC number => EC id} hash"
+ec_id = Hash.new
+File.open(EC_ID_FILE)
+  .each_line
+  .map{ |line| line.strip.split("\t") }
+  .each do |id, ec_number, _ec_desciption|
+  ec_id[ec_number] = id.to_i
+end
+
+
+STDERR.puts "Creating {sequence => EC number} hash"
+seq_ecs = Hash.new{ |hash, k| hash[k] = Set.new }
+File.open(EC_CROSREF_FILE)
+  .each_line
+  .map{ |line| line.strip.split("\t") }
+  .each do |_, uniprot_id, ec_number|
+  seq_ecs[uniprot_id.to_i].add ec_id[ec_number]
+end
+
+
+STDERR.puts "Creating {kmer => EC number} hash"
+kmer_ecs = Hash.new{ |hash, k| hash[k] = []  }
+uniprot_lines = File.read(UNIPROT_FILE).lines
+line_count = uniprot_lines.count
+i = 0
+uniprot_lines
+  .map{ |line| line.strip.split("\t") }
+  .each do |id, _uniprot_id,  _version, _taxon, _type, _name, sequence|
+  i += 1
+  STDERR.puts "Progress: #{(i * 100)/line_count}%" if i % 10000 == 0
+
+  ecs = seq_ecs[id.to_i].to_a
+  next if ecs.empty?
+
+  (sequence.length - K + 1).times.map{|s| sequence[s..(s+K-1)]}.each do |kmer|
+    kmer_ecs[kmer].concat(ecs)
+    kmer_ecs[kmer].uniq!
+  end
+end
+
+STDERR.puts "Writing 'kmer_ecs.tsv'"
+kmer_count = kmer_ecs.size
+i = 0
+kmer_ecs.each do |kmer, ecs|
+  if ecs.size > 0
+    puts "#{kmer}\t#{ecs.join(',')}"
+  end
+  i += 1
+  if i % 1000000 == 0
+    STDERR.puts "Progress: #{(i * 100)/kmer_count}%"
+  end
+end
+
