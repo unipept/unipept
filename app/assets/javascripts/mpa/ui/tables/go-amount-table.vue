ea5c9507091c4dc1966d63d121f57bf178b925f7
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-05-04 13:13:47 +0200

    Exposed more of TreeViews settings

--- app/assets/javascripts/mpa/ui/tables/go-amount-table.vue
+++ app/assets/javascripts/mpa/ui/tables/go-amount-table.vue
@@ -33,12 +33,12 @@
                     <td class="glyphicon glyphicon-inline amounttable-chevron">
                     </td>
                 </tr>
-                <tr v-bind:key="goTerm.code + '-2'" class="amounttable-exandrow">
+                <tr v-bind:key="goTerm.code + '-2'" class="amounttable-exandrow" v-if="expandedItems.has(goTerm) && expandedItems.get(goTerm).expanded">
                     <button class="btn btn-default btn-xs btn-animate pull-right" @click="saveImage()">
                         <span class="glyphicon glyphicon-download down"></span>
                         Save as image
                     </button>
-                    
+                    <treeview :height="310" :width="535" :tooltip="tooltip" :colors="highlightColorFunc" :enableAutoExpand="0.3" :linkStrokeColor="linkStrokeColor" :nodeStrokeColor="highlightColorFunc" :nodeFillColor="highlightColorFunc"></treeview>
                 </tr>
             </template>
         </tbody>
@@ -58,7 +58,8 @@
     import Vue from "vue";
     import Component from "vue-class-component";
     import {Prop, Watch} from "vue-property-decorator";
-    	import GoTerm from "../../../fa/GoTerm";
+    import GoTerm from "../../../fa/GoTerm";
+    import { tooltipContent } from "../visualizations/VisualizationHelper";
 
     @Component({
         components: {}
@@ -73,6 +74,15 @@
         private itemsVisible: number = this.initialItemsVisible;
         // The amount of items that are shown extra when expanding the table
         private visibilityStep: number = 10;
+
+        // Keeps track of which rows are expanded and which trees should be shown in that case.
+        private expandedItems: Map<GoTerm, {expanded: boolean, root: Node}> = new Map();
+
+        // All settings for each Treeview that remain the same
+        private tooltip: (d: any) => string = tooltipContent;
+        private highlightColor: string = "#ffc107";
+        private highlightColorFunc: (d: any) => string = d => (d.included ? this.highlightColor : "lightgrey");
+        private linkStrokeColor: (d: any) => string = ({target: d}) => this.highlightColorFunc(d);
 
         private expandView(amount): void {
             if (this.itemsVisible + amount > this.items.length) {
@@ -90,6 +100,7 @@
             }
         }
 
+        // TODO implement
         private saveImage(): void {
 
         }
