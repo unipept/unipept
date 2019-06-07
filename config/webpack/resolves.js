5c67cb97bd00b6e93bc07a4158b51c0ff49029f2
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-06-07 16:25:55 +0200

    Revert "Some changes in packages"
    This reverts commit 41cc4df1c2a9ac5842f895befc1cf85dfae3b23a.

--- config/webpack/resolves.js
+++ config/webpack/resolves.js
@@ -1,10 +1,4 @@
 module.exports = {
-    optimization: {
-      splitChunks: {
-        // include all types of chunks
-        chunks: 'all'
-      }
-    },
     resolve: {
         alias: {
             vue: 'vue/dist/vue.js'
