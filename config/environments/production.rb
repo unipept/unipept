df7140b40f6d25868999d65e8045a91cb4706bf1
Bart Mesuere <Bart.Mesuere@UGent.be>
2019-06-11 11:58:00 +0200

    add harmony compatible uglifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ UnipeptWeb::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
