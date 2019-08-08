2bed0d071195fe77de13cbd18cff8c15aefc5cef
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-08-08 11:48:36 +0000

    Old dockers folder removed

--- dockers/unipept-application/init-application.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Wait for the MySQL-Server container to get online
-# while ! mysqladmin ping -h"mysql" --silent; do
-#     echo "WAITING";
-#     sleep 1
-# done
-
-echo "KLAAR!"
--- dockers/unipept-application/init-database.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE unipept;
-GRANT ALL PRIVILEGES ON *.* TO 'unipept'@'localhost' IDENTIFIED BY 'unipept';
