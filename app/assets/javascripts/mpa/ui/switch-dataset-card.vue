061d1171df9d3be6dd635cdcb218cb6a6d16e17a
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-08-09 12:56:56 +0000

    Updated SwitchDatasetCard

--- app/assets/javascripts/mpa/ui/switch-dataset-card.vue
+++ app/assets/javascripts/mpa/ui/switch-dataset-card.vue
@@ -1,56 +1,61 @@
 <template>
-    <card title="Metaproteomics Analysis" :interactive="true">
-        <card-header class="card-title-interactive">
+    <div>
+    <v-card>
+         <card-header class="card-title-interactive">
             <card-title>
                 Metaproteomics Analysis
             </card-title>
             <div class="card-title-action">
-                <span class="glyphicon glyphicon-equalizer" @click="compareDatasets()" style="margin-right: 5px;"></span>
-                <span class="glyphicon glyphicon-plus" @click="addDataset()"></span>
+                <v-icon @click="compareDatasets()" color="white">mdi-chart-bubble</v-icon>
+                <v-icon @click="addDataset()" color="white">mdi-plus</v-icon>
             </div>
         </card-header>
-        <card-body id="switch-dataset-card-body">
-            <list class="switch-dataset-list" placeholder="Please add one or more datasets by clicking the plus button above... ">
-                <div class="list-item--two-lines" v-for="dataset of this.$store.getters.selectedDatasets" v-bind:key="dataset.id" :class="activeDataset === dataset ? 'selected' : ''">
-                    <span class="list-item-primary-action">
-                        <input v-if="dataset.progress === 1" v-model="activeDataset" :value="dataset" type="radio" class="input-item select-dataset-radio-button" style="width: 24px;" />
-                        <determinate-circular-progress-indicator v-else :progress="dataset.progress" :size="24"></determinate-circular-progress-indicator>
-                    </span>
-                    <span class="list-item-primary-content">
-                        {{ dataset.getName() }}
-                        <span class="list-item-date">
-                            {{ dataset.getDateFormatted() }}
-                        </span>
-                        <span class="list-item-body">
+        <v-card-text v-if="this.$store.getters.selectedDatasets.length === 0">
+            <span>Please add one or more datasets by clicking the plus button above...</span>
+        </v-card-text>
+         <v-list two-line>
+            <template v-for="dataset of this.$store.getters.selectedDatasets">
+                <v-list-tile :key="dataset.id" ripple @click="() => activeDataset = dataset" :class="activeDataset === dataset ? 'selected-list-tile' : ''">
+                    <v-list-tile-action>
+                        <v-radio-group v-if="dataset.progress === 1" v-model="activeDataset"><v-radio :value="dataset"></v-radio></v-radio-group>
+                        <v-progress-circular v-else :rotate="-90" :size="24" :value="dataset.progress * 100" color="primary"></v-progress-circular>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>
+                            {{ dataset.getName() }}
+                        </v-list-tile-title>
+                        <v-list-tile-sub-title>
                             {{ dataset.getAmountOfPeptides() }} peptides
-                        </span>
-                    </span>
-                    <span class="list-item-secondary-action">
-                        <span class="glyphicon glyphicon-trash" @click="deselectDataset(dataset)">
-                        </span>
-                    </span>
-                </div>
-            </list>
+                        </v-list-tile-sub-title>
+                    </v-list-tile-content>
 
-            <v-dialog v-model="dialogOpen" width="1000px">
-                <div style="min-height: 600px; background-color: white;">
-                    <div class="modal-header" >
-                        <button type="button" class="close" @click="dialogOpen = false"><span aria-hidden="true">×</span></button>
-                        <h4 class="modal-title">Heatmap wizard</h4>
-                    </div>
-                    <div class="single-dataset-wizard">
-                        <heatmap-wizard-multi-sample v-if="$store.getters.activeDataset" :dataset="$store.getters.activeDataset"></heatmap-wizard-multi-sample>
-                        <div v-else>
-                            <div class="text-xs-center" style="margin-top: 25px;">
-                                <v-progress-circular indeterminate color="primary"></v-progress-circular>
-                            </div>
+                    <v-list-tile-action>
+                        <v-list-tile-action-text>
+                            {{ dataset.getDateFormatted() }}
+                        </v-list-tile-action-text>
+                        <v-icon @click="deselectDataset(dataset)" v-on:click.stop>mdi-delete-outline</v-icon>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </template>
+        </v-list>
+        <v-dialog v-model="dialogOpen" width="1000px">
+            <div style="min-height: 600px; background-color: white;">
+                <div class="modal-header" >
+                    <button type="button" class="close" @click="dialogOpen = false"><span aria-hidden="true">×</span></button>
+                    <h4 class="modal-title">Heatmap wizard</h4>
+                </div>
+                <div class="single-dataset-wizard">
+                    <heatmap-wizard-multi-sample v-if="$store.getters.activeDataset" :dataset="$store.getters.activeDataset"></heatmap-wizard-multi-sample>
+                    <div v-else>
+                        <div class="text-xs-center" style="margin-top: 25px;">
+                            <v-progress-circular indeterminate color="primary"></v-progress-circular>
                         </div>
                     </div>
                 </div>
-            </v-dialog>
-
-        </card-body>
-    </card>
+            </div>
+        </v-dialog>
+    </v-card>
+    </div>
 </template>
 
 <script lang="ts">
@@ -95,6 +100,18 @@
     }
 </script>
 
-<style scoped>
+<style>
+    .selected-list-tile .v-list__tile:before {
+        content: ' ';
+        background-color: #2196F3;
+        width: 4px;
+        height: 100%;
+        position: relative;
+        left: -16px;
+    }
 
+    /* .selected-list-tile .v-list__tile {
+        position: relative;
+        left: -4px;
+    } */
 </style>
