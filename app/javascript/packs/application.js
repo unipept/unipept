865f29bdea090f96258f8b314c531a10149f235a
Bart Mesuere <Bart.Mesuere@UGent.be>
2019-08-30 15:27:23 +0200

    bleh

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import "babel-polyfill";
+import "@babel/polyfill";
 import "whatwg-fetch";
 import jQuery from "jquery";
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,22 +1,19 @@
 const {environment} = require("@rails/webpacker");
-const resolveConfig = require('./resolves');
+const resolveConfig = require("./resolves");
 
 const webpack = require("webpack");
 const vue = require("./loaders/vue");
 const typescript = require("./loaders/typescript");
 const css = require("./loaders/css");
 
-environment.plugins.append(
-    "CommonsChunkVendor",
-    new webpack.optimize.CommonsChunkPlugin({
-        name: "commons",
-        minChunks: 2,
-    })
-);
+const WebpackAssetsManifest = require("webpack-assets-manifest");
+
+// Enable the default config
+// environment.splitChunks();
 
 environment.config.merge(resolveConfig);
 
-environment.loaders.append('vue', vue);
-environment.loaders.append('typescript', typescript);
-environment.loaders.append('css', css);
+environment.loaders.append("vue", vue);
+environment.loaders.append("typescript", typescript);
+environment.loaders.append("css", css);
 module.exports = environment;
