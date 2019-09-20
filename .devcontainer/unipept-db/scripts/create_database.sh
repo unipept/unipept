2213650a450514acca464c670fefa963aec71f68
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-09-20 10:18:15 +0200

    Cleaned up dockers for generating database files significantly

---
+++ .devcontainer/unipept-db/scripts/create_database.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Move to the backend-folder in which all database-generation files live.
+cd /unipept/backend
+
+# First the database structure should be initialized when the container starts up for the first time
+cp database/structure_no_index.sql /docker-entrypoint-initdb.d/1.sql
+
+# Then, we start processing all data for the database. If the required data files have been generated before, we don't
+# need to redo these! (TODO: we could check if *all* required .tsv.gz files are present in case the database format
+# was changed!)
+if test -f "/tables/peptides.tsv.gz"; then
+    echo "No need to regenerate data. Previously generated data encountered."
+else
+    echo "Data needs to be regenerated."
+    # The generation of umgap-data is not supported at this time by this Docker container.
+    sed -i '/checkdep umgap/d' configure
+    chmod +x script.exp
+    chmod +x configure
+    ./script.exp
+    make
+
+    # Move the data that has been generated to the tables volume. If this file is rerun afterwards, the data does not
+    # need to be regenerated.
+    mv /backend/data/tables/* /tables
+fi
+
+# All necessary data has been generated at this point, and data can be inserted into the database when the container 
+# starts. The generated data can be found in the /tables folder and should be loader into the database by using the
+# load.sh-script that's automatically called when the container starts.
--- .devcontainer/unipept-db/scripts/fill_database.sh
+++ .devcontainer/unipept-db/scripts/fill_database.sh
@@ -5,5 +5,7 @@ result=`mysql $MYSQL_DATABASE -u$MYSQL_USER -p$MYSQL_PASSWORD -N -e "SELECT coun
 if [ $result -lt 1 ]
 then
     cd /tables
+    chmod u+x *.sh
     ./load.sh
+    ./index.sh
 fi
