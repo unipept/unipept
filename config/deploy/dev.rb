a6ae4b9e8d65e32e9093581529f8b91c5ca76d03
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-11-20 16:27:38 +0100

    Fix yarn integrity check

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -8,6 +8,22 @@ server 'morty.ugent.be', user: 'unipept', roles: %i[web app], ssh_options: {
 set :branch, 'develop'
 set :rails_env, :production
 
+# Perform yarn install before precompiling the assets in order to pass the
+# integrity check.
+namespace :deploy do
+  namespace :assets do
+    before :precompile, :yarn_install do
+      on release_roles(fetch(:assets_roles)) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            execute :yarn, "install"
+          end
+        end
+      end
+    end
+  end
+end
+
 namespace :deploy do
   before :publishing, :asset_stuff do
     on roles :all do
