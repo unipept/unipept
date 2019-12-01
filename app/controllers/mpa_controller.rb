987606593224e5fd7bc92bfc02798f7bceed574f
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-12-01 10:07:57 +0100

    Fix integration with MPA

--- app/controllers/mpa_controller.rb
+++ app/controllers/mpa_controller.rb
@@ -7,6 +7,7 @@ class MpaController < ApplicationController
     @title = 'Metaproteomics analysis result'
     @peptides = (params[:qs] || '')
     @name = params[:search_name]
+    puts @name
     @il = params[:il].present?
     @dupes = params[:dupes].present?
     @missed = params[:missed].present?
