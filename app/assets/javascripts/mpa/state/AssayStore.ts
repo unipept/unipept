3180ac04d888c2fcf9a30dc7b2a76f78a58930cb
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-03-06 16:45:37 +0100

    Web app is compatible with newest version of UWC again

--- app/assets/javascripts/mpa/state/AssayStore.ts
+++ app/assets/javascripts/mpa/state/AssayStore.ts
@@ -1,6 +1,9 @@
 import Assay from "unipept-web-components/src/logic/data-management/assay/Assay";
-import { GetterTree, MutationTree, ActionTree, ActionContext, Store } from "vuex";
+import { GetterTree, MutationTree, ActionTree, ActionContext } from "vuex";
 import MpaAnalysisManager from "unipept-web-components/src/logic/data-management/MpaAnalysisManager";
+import MPAConfig from "unipept-web-components/src/logic/data-management/MPAConfig";
+import BrowserStorageDataReader from "unipept-web-components/src/logic/data-management/assay/browser/BrowserStorageDataReader";
+import AssayVisitor from "unipept-web-components/src/logic/data-management/assay/AssayVisitor";
 
 /**
  * The AssayState keeps track of which assays are currently selected by the user for analysis, and which assays are
@@ -28,9 +31,9 @@ const assayState: AssayState = {
     searchSettings: {
         il: true,
         dupes: true,
-        missed: false
-    }
-}
+        missed: false,
+    },
+};
 
 const assayGetters: GetterTree<AssayState, any> = {
     getSelectedAssays(state: AssayState): Assay[] {
@@ -53,7 +56,7 @@ const assayGetters: GetterTree<AssayState, any> = {
 /**
  * Find the index of an assay in the given list. Assay identity is based on ID. Assays with equal ID's are considered
  * to be equal.
- * 
+ *
  * @param item The item for which it's position in the given list should be found.
  * @param list The list in which we look for the index of the given assay.
  * @return Position of the given item, in the given list. -1 if the item was not found.
@@ -71,7 +74,7 @@ const assayMutations: MutationTree<AssayState> = {
     /**
      * Add an assay to the list of selected assays. Assays will only be added if no assay with the same ID is already
      * selected. The given assay will always be added to the end of the selected assay list.
-     * 
+     *
      * @param state Instance of the AssayState to which the given assay should be added.
      * @param assay The assay that should be added to the selection.
      */
@@ -86,7 +89,7 @@ const assayMutations: MutationTree<AssayState> = {
 
     /**
      * Remove an assay from the list of selected assays. No error is thrown when a non-existant assay is being removed.
-     * 
+     *
      * @param state Instance of the AssayState from which the given assay should be removed.
      * @param assay The assay that should be removed from the selection.
      */
@@ -102,7 +105,7 @@ const assayMutations: MutationTree<AssayState> = {
     /**
      * Add a previously stored assay to the list of stored datasets. Note that this mutation does not persistently store
      * the assay itself. This is the responsibility of the caller. The new assay will be added to the end of the list.
-     * 
+     *
      * @param state Instance of the AssayState to which the given assay should be added.
      * @param assay The assay that should be added to the list of stored assays. This assay will not be persistently
      * stored by this function, it will only be marked as stored.
@@ -117,7 +120,7 @@ const assayMutations: MutationTree<AssayState> = {
 
     /**
      * Remove the given assay from the list of stored assays.
-     * 
+     *
      * @param state Instance of the AssayState from which the assay should be removed.
      * @param assay The assay that should be removed from the list of stored assays.
      */
@@ -132,7 +135,7 @@ const assayMutations: MutationTree<AssayState> = {
     /**
      * Set the given assay to be the currently active one. This mutation does not perform any additional checks. Assays
      * previously set to be active, will no longer be marked as active.
-     * 
+     *
      * @param state The state for which the active assay should be updated.
      * @param newActive The assay that should be marked as active. Pass null to select no assay.
      */
@@ -143,8 +146,8 @@ const assayMutations: MutationTree<AssayState> = {
     /**
      * Update the currently active search settings for assay analysis. Reprocessing of assays is not handled by this
      * mutation.
-     * 
-     * @param state The state for which the search settings should be updated. 
+     *
+     * @param state The state for which the search settings should be updated.
      * @param settings A new set of settings that replace the previously set.
      */
     SET_SEARCH_SETTINGS(state: AssayState, settings: MPAConfig) {
@@ -169,7 +172,7 @@ const assayActions: ActionTree<AssayState, any> = {
      * Sets the first fully processed assay to be the active one, only if no active assay is currently set. This
      * action checks whether the currently active assay is indeed a member of the selected assays. If not, another one
      * will be elected to be the active assay.
-     * 
+     *
      * @param store Instance of the store for which the active dataset should be reset.
      */
     resetActiveAssay(store: ActionContext<AssayState, any>) {
@@ -188,57 +191,61 @@ const assayActions: ActionTree<AssayState, any> = {
                 }
             }
 
-            store.commit('SET_ACTIVE_ASSAY', newActive);
+            store.commit("SET_ACTIVE_ASSAY", newActive);
         }
     },
 
     addStoredAssay(store: ActionContext<AssayState, any>, assay: Assay) {
-        store.commit('ADD_STORED_ASSAY', assay);
+        store.commit("ADD_STORED_ASSAY", assay);
     },
 
     /**
      * Remove an assay from the list of stored assays. If the assay was also selected for analysis, it will also be
      * removed from the analysis. This action does not persistently store the assay itself, that is the responsibility
      * of the caller.
-     * 
-     * @param store Instance of the store to which a new stored assay should be added. 
+     *
+     * @param store Instance of the store to which a new stored assay should be added.
      * @param assay The assay that should be added to the list of stored assays.
      */
     removeStoredAssay(store: ActionContext<AssayState, any>, assay: Assay) {
-        store.dispatch('deselectAssay', assay);
-        store.commit('REMOVE_STORED_ASSAY', assay);
+        store.dispatch("deselectAssay", assay);
+        store.commit("REMOVE_STORED_ASSAY", assay);
     },
-    
+
     setActiveAssay(store: ActionContext<AssayState, any>, assay: Assay) {
-        store.commit('SET_ACTIVE_ASSAY', assay);
+        store.commit("SET_ACTIVE_ASSAY", assay);
     },
 
     /**
      * Start the analysis for the given assay. If this assay finishes, and no active assay has been selected at that
      * point, this assay will be set to be the active one.
-     * 
-     * @param store The store for which we are currently processing 
-     * @param assay 
+     *
+     * @param store The store for which we are currently processing
+     * @param assay
      */
     processAssay(store: ActionContext<AssayState, any>, assay: Assay) {
-        let mpaManager = new MpaAnalysisManager();
+        // First read all peptides from storage for this assay, and then process the assay.
+        const dataReader: AssayVisitor = new BrowserStorageDataReader();
+        assay.accept(dataReader).then(() => {
+            const mpaManager = new MpaAnalysisManager();
 
-        mpaManager.processDataset(assay, store.getters.searchSettings, store.getters.baseUrl)
-            .then(() => {
-                store.dispatch("resetActiveAssay");
-            });
+            mpaManager.processDataset(assay, store.getters.searchSettings, store.getters.baseUrl)
+                .then(() => {
+                    store.dispatch("resetActiveAssay");
+                });
+        });
     },
 
     updateSearchSettings(store: ActionContext<AssayState, any>, settings: MPAConfig) {
-        store.commit('SET_SEARCH_SETTINGS', settings);
-        store.commit('SET_ACTIVE_ASSAY', null);
+        store.commit("SET_SEARCH_SETTINGS", settings);
+        store.commit("SET_ACTIVE_ASSAY", null);
 
         // Reprocess all currently selected assays.
-        for (let assay of store.getters.getSelectedAssays) {
-            store.dispatch('processAssay', assay);
+        for (const assay of store.getters.getSelectedAssays) {
+            store.dispatch("processAssay", assay);
         }
-    }
-}
+    },
+};
 
 export const AssayStore = {
     state: assayState,
