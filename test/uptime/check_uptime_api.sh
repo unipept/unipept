9cecf2d8950c4f35eb1cb30f7f77da78163b07af
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-05-07 09:31:15 +0200

    Debug script

--- test/uptime/check_uptime_api.sh
+++ test/uptime/check_uptime_api.sh
@@ -1,6 +1,9 @@
 # This script checks if the API-server returns results as expected, and can thus be used by GitHub actions to
 # periodically verify that our server is still up and running
 
+set -e
+set -o pipefail
+
 # How long should a call to the CLI take before we mark it as slow? (in seconds)
 RUNTIME_TRESHOLD="1.0"
 
@@ -15,6 +18,9 @@ fi
 # Check timing of specific results
 TIMEFORMAT=%R
 RUNTIME=$(time unipept pept2taxa AAAAALTER)
+
+bc --version
+echo "$RUNTIME > $RUNTIME_TRESHOLD"
 
 if [[ $(echo "$RUNTIME > $RUNTIME_TRESHOLD" | bc -l) ]]
 then
