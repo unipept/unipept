69a1f3eaf96a3958fc07cbd03c682d1a0bf226d8
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-08-27 08:33:50 +0200

    fix compilation

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,45 +1,35 @@
-const {environment} = require("@rails/webpacker");
+const { environment } = require('@rails/webpacker');
+const typescript =  require('./loaders/typescript');
+const less = require('./loaders/less');
+const sass = require('./loaders/sass');
+const css = require('./loaders/css');
+const { VueLoaderPlugin } = require('vue-loader');
+const vue = require('./loaders/vue');
+const doc = require('./loaders/doc-loader');
+const workerLoader = require('./loaders/worker-loader');
 const resolveConfig = require("./resolves");
-const optimizeConfig = require("./optimization");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
 const webpack = require("webpack");
-const vue = require("./loaders/vue");
-const typescript = require("./loaders/typescript");
-const css = require("./loaders/css");
-const less = require("./loaders/less");
-const sass = require("./loaders/sass");
-const workerLoader = require("./loaders/worker-loader");
-const docLoader = require("./loaders/doc-loader");
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
-
-// Enable the default config
-// environment.splitChunks();
 
 environment.config.merge(resolveConfig);
-environment.config.merge(optimizeConfig);
 
-
-// Compile TypeScript and Vue
-environment.loaders.append("worker-loader", workerLoader);
-environment.loaders.append("null-loader", docLoader);
-environment.loaders.append("vue", vue);
-environment.loaders.append("typescript", typescript);
-
-// Compile less and css
-environment.loaders.append("css", css);
-environment.loaders.append("less", less);
-environment.loaders.append("sass", sass);
-
-// This plugin is required by the Vue-loader
-environment.plugins.prepend("VueLoaderPlugin", new VueLoaderPlugin());
 environment.plugins.prepend("VuetifyLoader", new VuetifyLoaderPlugin());
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
 
+environment.loaders.prepend('vue', vue);
+environment.loaders.prepend('typescript', typescript);
+environment.loaders.prepend("less", less);
+environment.loaders.prepend("sass", sass);
+// environment.loaders.prepend("css", css);
+environment.loaders.prepend("worker-loader", workerLoader);
+environment.loaders.prepend("doc-loader", doc);
 
 // The unipept-web-components library contains some requires for electron, which are only required when it's being used
 // in an electron-environment. We can thus safely ignore these here.
 environment.plugins.prepend("IgnorePlugin", new webpack.IgnorePlugin({
     resourceRegExp: /^(electron|fs)/,
-    contextRegExp: /.*/ 
+    contextRegExp: /.*/
 }));
+
 module.exports = environment;
--- config/webpack/loaders/sass.js
+++ config/webpack/loaders/sass.js
@@ -5,19 +5,13 @@ module.exports = {
         'css-loader',
         {
             loader: 'sass-loader',
-            // Requires sass-loader@^7.0.0
-            options: {
-            implementation: require('sass'),
-            fiber: require('fibers'),
-            indentedSyntax: true // optional
-            },
             // Requires sass-loader@^8.0.0
             options: {
-            implementation: require('sass'),
-            sassOptions: {
-                fiber: require('fibers'),
-                indentedSyntax: true // optional
-            },
+                implementation: require('sass'),
+                sassOptions: {
+                    fiber: require('fibers'),
+                    indentedSyntax: true // optional
+                },
             },
         },
     ]
