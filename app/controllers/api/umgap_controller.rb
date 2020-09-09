a8432b5b8758b3e932ec9e45e3237074dc6998b4
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2020-09-09 16:02:08 +0200

    Add sidebar markers for the commands

--- app/controllers/api/umgap_controller.rb
+++ app/controllers/api/umgap_controller.rb
@@ -21,6 +21,106 @@ class Api::UmgapController < ApplicationController
     @study = 'Metagenomics'
   end
 
+  def fastq2fasta
+    @title = 'The umgap fastq2fasta command'
+    @sidebar_name = 'fastq2fasta'
+  end
+
+  def translate
+    @title = 'The umgap translate command'
+    @sidebar_name = 'translate'
+  end
+
+  def prot2tryp
+    @title = 'The umgap prot2tryp command'
+    @sidebar_name = 'prot2tryp'
+  end
+
+  def prot2kmer
+    @title = 'The umgap prot2kmer command'
+    @sidebar_name = 'prot2kmer'
+  end
+
+  def filter
+    @title = 'The umgap filter command'
+    @sidebar_name = 'filter'
+  end
+
+  def pept2lca
+    @title = 'The umgap pept2lca command'
+    @sidebar_name = 'pept2lca'
+  end
+
+  def prot2tryp2lca
+    @title = 'The umgap prot2tryp2lca command'
+    @sidebar_name = 'prot2tryp2lca'
+  end
+
+  def prot2kmer2lca
+    @title = 'The umgap prot2kmer2lca command'
+    @sidebar_name = 'prot2kmer2lca'
+  end
+
+  def bestof
+    @title = 'The umgap bestof command'
+    @sidebar_name = 'bestof'
+  end
+
+  def seedextend
+    @title = 'The umgap seedextend command'
+    @sidebar_name = 'seedextend'
+  end
+
+  def uniq
+    @title = 'The umgap uniq command'
+    @sidebar_name = 'uniq'
+  end
+
+  def taxa2agg
+    @title = 'The umgap taxa2agg command'
+    @sidebar_name = 'taxa2agg'
+  end
+
+  def snaptaxon
+    @title = 'The umgap snaptaxon command'
+    @sidebar_name = 'snaptaxon'
+  end
+
+  def taxa2freq
+    @title = 'The umgap taxa2freq command'
+    @sidebar_name = 'taxa2freq'
+  end
+
+  def taxa2tree
+    @title = 'The umgap taxa2tree command'
+    @sidebar_name = 'taxa2tree'
+  end
+
+  def taxonomy
+    @title = 'The umgap taxonomy command'
+    @sidebar_name = 'taxonomy'
+  end
+
+  def splitkmers
+    @title = 'The umgap splitkmers command'
+    @sidebar_name = 'splitkmers'
+  end
+
+  def joinkmers
+    @title = 'The umgap joinkmers command'
+    @sidebar_name = 'joinkmers'
+  end
+
+  def buildindex
+    @title = 'The umgap buildindex command'
+    @sidebar_name = 'buildindex'
+  end
+
+  def printindex
+    @title = 'The umgap printindex command'
+    @sidebar_name = 'printindex'
+  end
+
   private
 
   def set_sidebar_nav
