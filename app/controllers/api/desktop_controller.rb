5ddd568e3b3be87ba8cd5ae4eb91e291157544ab
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-10-29 08:45:00 +0100

    Fix linter issues

--- app/controllers/api/desktop_controller.rb
+++ app/controllers/api/desktop_controller.rb
@@ -2,22 +2,18 @@ class Api::DesktopController < ApplicationController
   before_action :set_header
   before_action :set_sidebar_nav
 
-
   def index
     @title = 'Unipept Desktop - Getting started'
     @sidebar_name = 'Getting started'
-    @sidebar_subnav = [
-        'Installation',
-        'Updates'
-    ]
+    @sidebar_subnav = %w[Installation Updates]
   end
 
   def application_overview
     @title = 'Unipept Desktop - Application overview'
     @sidebar_name = 'Application overview'
     @sidebar_subnav = [
-        'First time use',
-        'Navigation overview'
+      'First time use',
+      'Navigation overview'
     ]
   end
 
@@ -25,8 +21,8 @@ class Api::DesktopController < ApplicationController
     @title = 'Unipept Desktop - Project management'
     @sidebar_name = 'Project management'
     @sidebar_subnav = [
-        'Project structure',
-        'Physical structure'
+      'Project structure',
+      'Physical structure'
     ]
   end
 
@@ -34,18 +30,19 @@ class Api::DesktopController < ApplicationController
     @title = 'Unipept Desktop - Settings'
     @sidebar_name = 'Settings'
     @sidebar_subnav = [
-        'Change endpoint',
-        'Concurrency control'
+      'Change endpoint',
+      'Concurrency control'
     ]
   end
 
   private
+
   def set_sidebar_nav
     @sidebar_nav = [
-        { name: 'Getting started',      path: api_desktop_path },
-        { name: 'Application overview', path: api_desktop_application_overview_path },
-        { name: 'Project management',   path: api_desktop_project_management_path },
-        { name: 'Settings',             path: api_desktop_settings_path }
+      { name: 'Getting started',      path: api_desktop_path },
+      { name: 'Application overview', path: api_desktop_application_overview_path },
+      { name: 'Project management',   path: api_desktop_project_management_path },
+      { name: 'Settings',             path: api_desktop_settings_path }
     ]
   end
 
