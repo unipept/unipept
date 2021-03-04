8907433cedf0592979fcbfbed647a1afcef1a8a1
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2021-03-04 12:56:04 +0100

    increment version numbering

--- config/application.rb
+++ config/application.rb
@@ -38,10 +38,10 @@ module UnipeptWeb
     config.filter_parameters += [:password]
 
     config.versions = {
-      unipept: '4.4.4',
+      unipept: '4.5.0',
       gem: '2.2.1',
       uniprot: '2020.01',
-      desktop: '0.5.0'
+      desktop: '1.1.1'
     }
 
     config.api_host = 'api.unipept.ugent.be'
