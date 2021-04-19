8c96b90bda4aaa003306502fa9b483d279f5544a
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2021-04-19 09:19:50 +0200

    Fix Capistrano task

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -38,19 +38,15 @@ namespace :deploy do
 end
 
 namespace :deploy do
-  desc "Uploads a robots.txt that mandates the site as off-limits to crawlers"
-  task :block_robots do
-    on primary roles :app do
+  before :publishing, :block_robots do
+    on roles :all do
       content = [
         '# This is a staging site. Do not index.',
         'User-agent: *',
         'Disallow: /'
       ].join($/)
 
-      logger.info "Uploading blocking robots.txt"
-      put content, "#{release_path}/public/robots.txt"
+      upload! StringIO.new(content), "#{release_path}/public/robots.txt"
     end
   end
 end
-
-after "deploy:update_code", "deploy:block_robots"
