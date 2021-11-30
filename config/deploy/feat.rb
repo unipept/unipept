660a10ba0eb5bbcfc8381f70b9cff5e441844f8e
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2021-11-30 13:52:10 +0100

    Revert changes to Capistrano config

--- config/deploy/feat.rb
+++ config/deploy/feat.rb
@@ -1,14 +1,30 @@
-set :stage, :prod
+set :stage, :feat
 
-set :deploy_to, '/home/unipept/rails'
+set :deploy_to, '/home/bmesuere/rails'
 
 # don't specify db as it's not needed for unipept
-server 'unipeptweb.ugent.be', user: 'unipept', roles: %i[web app], ssh_options: {
-  port: 22
+server 'morty.ugent.be', user: 'unipept', roles: %i[web app], ssh_options: {
+  port: 4840
 }
 
-set :branch, 'fix/update_api_url'
+set :branch, 'documentation/unipept-desktop'
 set :rails_env, :production
+
+# Perform yarn install before precompiling the assets in order to pass the
+# integrity check.
+namespace :deploy do
+  namespace :assets do
+    before :precompile, :yarn_install do
+      on release_roles(fetch(:assets_roles)) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            execute :yarn, "install"
+          end
+        end
+      end
+    end
+  end
+end
 
 namespace :deploy do
   before :publishing, :asset_stuff do
