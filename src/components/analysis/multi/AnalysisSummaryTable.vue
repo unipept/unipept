5be527f9a7cfdaf7a9475277a2ae8e0d3f941afe
tibvdm <tibovandemoortele@hotmail.com>
2025-02-27 13:21:37 +0100

    Show the functional annotations in the overview table

--- src/components/analysis/multi/AnalysisSummaryTable.vue
+++ src/components/analysis/multi/AnalysisSummaryTable.vue
@@ -25,6 +25,92 @@
             </a>
         </template>
 
+        <template #item.faCounts="{ item }">
+            <v-tooltip v-if="item.faCounts" location="top">
+                <template #activator="{ props }">
+                    <v-avatar
+                        v-bind="props"
+                        size="30"
+                        :color="item.faCounts.go > 0 ? 'amber' : 'amber-lighten-4'"
+                    >
+                        <span
+                            :class="[ item.faCounts.go > 0 ? 'dark--text' : 'gray--text', 'headline']"
+                            style="font-size: 14px !important;"
+                        >
+                            GO
+                        </span>
+                    </v-avatar>
+                </template>
+
+                <span v-if="item.faCounts.go > 0">
+                    This peptide is annotated with {{ item.faCounts.go }}
+                    <span class="font-weight-bold">
+                        {{ item.faCounts.go === 1 ? "GO-term" : "GO-terms" }}.
+                    </span>
+                </span>
+
+                <span v-else>
+                    This peptide is not annotated with GO-terms.
+                </span>
+            </v-tooltip>
+
+            <v-tooltip v-if="item.faCounts" location="top">
+                <template #activator="{ props }">
+                    <v-avatar
+                        v-bind="props"
+                        size="30"
+                        :color="item.faCounts.ec > 0 ? 'indigo' : 'indigo-lighten-4'"
+                    >
+                        <span
+                            class="text-white"
+                            style="font-size: 14px !important;"
+                        >
+                            EC
+                        </span>
+                    </v-avatar>
+                </template>
+
+                <span v-if="item.faCounts.ec > 0">
+                    This peptide is annotated with {{ item.faCounts.ec }}
+                    <span class="font-weight-bold">
+                        {{ item.faCounts.ec === 1 ? "EC-number" : "EC-numbers" }}.
+                    </span>
+                </span>
+
+                <span v-else>
+                    This peptide is not annotated with EC-numbers.
+                </span>
+            </v-tooltip>
+
+            <v-tooltip v-if="item.faCounts" location="top">
+                <template #activator="{ props }">
+                    <v-avatar
+                        v-bind="props"
+                        size="30"
+                        :color="item.faCounts.ipr > 0 ? 'red' : 'red-lighten-4'"
+                    >
+                        <span
+                            class="text-white"
+                            style="font-size: 14px !important;"
+                        >
+                            IPR
+                        </span>
+                    </v-avatar>
+                </template>
+
+                <span v-if="item.faCounts.ipr > 0">
+                    This peptide is annotated with {{ item.faCounts.ipr }}
+                    <span class="font-weight-bold">
+                        {{ item.faCounts.ipr === 1 ? "InterPro-entry" : "InterPro-entries" }}.
+                    </span>
+                </span>
+
+                <span v-else>
+                    This protein is not annotated with InterPro-entries.
+                </span>
+            </v-tooltip>
+        </template>
+
         <template #item.warning="{ item }">
             <v-tooltip
                 v-if="!item.found"
@@ -74,6 +160,12 @@ const headers: any = [
         key: "rank"
     },
     {
+        title: "Annotations",
+        align: "start",
+        key: "faCounts",
+        sortable: false
+    },
+    {
         title: "",
         align: "start",
         key: "warning",
@@ -87,6 +179,7 @@ export interface AnalysisSummaryTableItem {
     lca: string;
     rank: string;
     found: boolean;
+    faCounts: { all: number, ec: number, go: number, ipr: number } | undefined;
 }
 
 const openPeptideAnalysis = (peptide: string) => {
