412fcc41c420c3fb2eeafd931c417eb8c33179cb
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2025-03-20 14:38:02 +0100

    Point to real Unipept API URL

--- src/logic/Constants.ts
+++ src/logic/Constants.ts
@@ -1,5 +1,4 @@
-// export const DEFAULT_API_BASE_URL = "https://api.unipept.ugent.be";
-export const DEFAULT_API_BASE_URL = "http://localhost:51545";
+export const DEFAULT_API_BASE_URL = "https://api.unipept.ugent.be";
 export const DEFAULT_ONTOLOGY_BATCH_SIZE = 100;
 export const DEFAULT_BATCH_SIZE = 200;
 
