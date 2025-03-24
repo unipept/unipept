3f2f8c1ac749b4c083ee9fc50c995c4ba8927f81
tibvdm <tibovandemoortele@hotmail.com>
2025-03-24 14:35:40 +0100

    Set DEFAULT_API_BASE_URL

--- src/logic/Constants.ts
+++ src/logic/Constants.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_API_BASE_URL = "http://127.0.0.1:62176";
+export const DEFAULT_API_BASE_URL = "https://api.unipept.ugent.be";
 export const DEFAULT_ONTOLOGY_BATCH_SIZE = 100;
 export const DEFAULT_BATCH_SIZE = 200;
 
