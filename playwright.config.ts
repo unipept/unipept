f264c377dc21df169572df33bde3e5398a4aa736
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2026-01-19 13:43:14 +0100

    Only use Chrome for now in Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,12 +29,7 @@ export default defineConfig({
         {
             name: 'chromium',
             use: {...devices['Desktop Chrome']},
-        },
-
-        {
-            name: 'firefox',
-            use: {...devices['Desktop Firefox']},
-        },
+        }
     ],
 
     /* Run your local dev server before starting the tests */
